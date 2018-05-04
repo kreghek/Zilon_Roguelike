5da86136e8313d8586913bcb56de6852c1a0b250
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-05-05 00:03:28 +0700

    Подключен тестовый sqlite

--- Zilon.Client/Assets/Zilon/Scripts/GlobeWorldVM.cs
+++ Zilon.Client/Assets/Zilon/Scripts/GlobeWorldVM.cs
@@ -1,19 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine;
 using Zilon.Logic.Services.Client;
 
-public class GlobeWorldVM : MonoBehaviour {
+public class GlobeWorldVM : MonoBehaviour
+{
 
     public Map Map;
     public SchemeLocator SchemeLocator;
 
-    public GlobeWorldVM() {
-        
+    public GlobeWorldVM()
+    {
+
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        string connectionString = "URI=file:" + Application.dataPath + "/db/db.bytes";
+        using (IDbConnection dbcon = (IDbConnection)new SqliteConnection(connectionString))
+        {
+            dbcon.Open();
+
+            // Выбираем нужные нам данные
+            var sql = "SELECT Id, Value FROM Test";
+            using (IDbCommand dbcmd = dbcon.CreateCommand())
+            {
+                dbcmd.CommandText = sql;
+                // Выполняем запрос
+                using (IDataReader reader = dbcmd.ExecuteReader())
+                {
+                    // Читаем и выводим результат
+                    while (reader.Read())
+                    {
+                        Debug.Log($"Id: {reader.GetInt32(0)}, Value: {reader.GetString(1)}");
+                    }
+                }
+            }
+            // Закрываем соединение
+            dbcon.Close();
+        }
+    }
+
 
     private void Awake()
     {
@@ -22,7 +50,8 @@ public class GlobeWorldVM : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+
+    }
 }
