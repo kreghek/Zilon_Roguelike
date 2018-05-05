758ed7685006f39a408cb908b1f39d82aa233783
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-05-05 16:58:08 +0700

    Использование сервиса для генерации боёв

--- Zilon.Client/Assets/Zilon/Scripts/Combat/CombatMapVM.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Combat/CombatMapVM.cs
@@ -3,6 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Zilon.Logic.Persons;
+using Zilon.Logic.Players;
+using Zilon.Logic.Services;
+using Zilon.Logic.Tactics;
+using Zilon.Logic.Tactics.Initialization;
+using Zilon.Logic.Tactics.Map;
 
 public class CombatMapVM : MonoBehaviour
 {
@@ -12,67 +18,101 @@ public class CombatMapVM : MonoBehaviour
     public CombatActorVM ActorPrefab;
     public Canvas Canvas;
 
-    private List<CombatLocationVM> _locations;
-    private List<CombatLocationVM> _teamLocations;
+    private List<CombatLocationVM> locations;
 
-    public object x { get; private set; }
+    private readonly CombatService combatService;
 
     private void Awake()
     {
-        CreateLocations();
-        CreateActors();
+        var initData = GetTwoGroupsData();
+        var combat = combatService.CreateCombat(initData);
+        CreateLocations(combat);
+        CreateActors(combat);
     }
 
-    private void CreateLocations()
+    private void CreateLocations(Combat combat)
     {
-        _locations = new List<CombatLocationVM>();
-        _teamLocations = new List<CombatLocationVM>();
-        const int mapSize = 10;
-        for (var i = 0; i < mapSize; i++)
+        locations = new List<CombatLocationVM>();
+
+        foreach (var mapNode in combat.Map.Nodes)
         {
-            for (var j = 0; j < mapSize; j++)
-            {
-                var location = Instantiate(LocationPrefab, transform);
-                var offset = UnityEngine.Random.insideUnitCircle * 10;
-                location.transform.position = new Vector3(20 * i + offset.x, 20 * j + offset.y);
+            var locationobj = Instantiate(LocationPrefab, transform);
+            locationobj.transform.position = new Vector3(mapNode.Position.X, mapNode.Position.Y);
+            locations.Add(locationobj);
+        }
+    }
 
-                _locations.Add(location);
+    private void CreateActors(Combat combat)
+    {
+        foreach (var actor in combat.Actors)
+        {
+            var actorObj = Instantiate(ActorPrefab, transform);
+            actorObj.transform.position = new Vector3(actor.Position.X, actor.Position.Y);
+        }
+    }
 
-                if (i == 0 || i == mapSize-1)
-                {
-                    if (j == 0 || j == mapSize-1)
-                    {
-                        _teamLocations.Add(location);
+    private static CombatInitData GetTwoGroupsData()
+    {
+        return new CombatInitData
+        {
+            Map = new CombatMap(),
+            Players = new[] {
+                    new PlayerCombatInitData{
+                        Player = CreateFakePlayer(),
+                        Squads =new[]{
+                            CreateSquad(5),
+                            CreateSquad(3),
+                            CreateSquad(4)
+                        }
+                    },
+                    new PlayerCombatInitData{
+                        Player = CreateFakePlayer(),
+                        Squads =new[]{
+                            CreateSquad(5),
+                            CreateSquad(3),
+                            CreateSquad(4),
+                            CreateSquad(6),
+                        }
                     }
                 }
-            }
-        }
+        };
     }
 
-    private void CreateActors()
+    private static Person CreatePerson()
     {
-        for (var teamIndex = 0; teamIndex < 2; teamIndex++)
-        {
-            var teamLocation = _teamLocations[teamIndex];
-            var quadLocations = _locations.Where(x => (x.transform.position - teamLocation.transform.position).magnitude < 40).ToArray();
-            for (var quadIndex = 0; quadIndex < 3; quadIndex++)
-            {
-                var quadLocation = quadLocations[UnityEngine.Random.Range(0, quadLocations.Length)];
+        var person = new Person { };
+        return person;
+    }
 
-                for (var actorIndex = 0; actorIndex < 5 + quadIndex; actorIndex++)
-                {
-                    var actor = Instantiate(ActorPrefab, transform);
-                    var offset = UnityEngine.Random.insideUnitCircle * 2;
-                    actor.transform.position = quadLocation.transform.position + new Vector3(offset.x, offset.y);
-                }
-            }
+    private static IPlayer CreateFakePlayer()
+    {
+        return new HumanPlayer();
+    }
+
+    private static Squad CreateSquad(int count)
+    {
+        var persons = new List<Person>();
+
+        for (var i = 0; i < count; i++)
+        {
+            persons.Add(CreatePerson());
         }
+
+        return new Squad
+        {
+            Persons = persons.ToArray()
+        };
+    }
+
+    public CombatMapVM()
+    {
+        combatService = new CombatService();
     }
 
     // Use this for initialization
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
