f75f9c55c5f4675aa0e6bc17428d83a67b4ac130
Куркутов Павел <kurkutovpa@tomskasu.ru>
2018-05-10 12:42:18 +0700

    Рефакторинг карты.

--- Zilon.Engine/Zilon.Logic.Tests/Tactics/Map/CombatMapTests.cs
+++ Zilon.Engine/Zilon.Logic.Tests/Tactics/Map/CombatMapTests.cs
@@ -1,20 +1,23 @@
-﻿using NUnit.Framework;
-using Zilon.Logic.Tactics.Map;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
-
-namespace Zilon.Logic.Tactics.Map.Tests
+﻿namespace Zilon.Logic.Tests.Tactics.Map
 {
-    [TestFixture()]
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    using Zilon.Logic.Tactics.Map;
+
+    [TestFixture]
     public class CombatMapTests
     {
-        [Test()]
-        public void CombatMapTest()
+        /// <summary>
+        /// 1. В системе есть дефолный набор нод.
+        /// 2. Создаём карту.
+        /// 3. Карта успешно создана. Ноды указаны в объекте карты.
+        /// </summary>
+        [Test]
+        public void Constructor_Default_HasNodes()
         {
+            //TODO Карта должна создаваться на основе указанного набора нодов, передаваемых в конструкторе.
             var map = new CombatMap();
 
             map.Should().NotBeNull();
