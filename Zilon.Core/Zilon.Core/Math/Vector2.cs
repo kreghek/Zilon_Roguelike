870cb80f16d8e4188a9ad8ce1f429da5ab06113a
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-05-12 03:59:13 +0700

    Тесты на менеджер команд по правилам очереди QueueCommandManager.

--- Zilon.Core/Zilon.Core/Math/Vector2.cs
+++ Zilon.Core/Zilon.Core/Math/Vector2.cs
@@ -5,9 +5,15 @@
         public float X { get; set; }
         public float Y { get; set; }
 
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
         public override string ToString()
         {
-            return $"(X: {X:D2}, Y: {Y:D2}";
+            return $"(X: {X:F2}, Y: {Y:F2})";
         }
     }
 }
