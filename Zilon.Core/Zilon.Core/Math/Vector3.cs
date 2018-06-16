00859d4ee45ef226a12986be7b9f140218768b6e
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-06-16 16:56:05 +0700

    Добавлен сравнение объектов для Vector3

--- Zilon.Core/Zilon.Core/Math/Vector3.cs
+++ Zilon.Core/Zilon.Core/Math/Vector3.cs
@@ -18,6 +18,28 @@
             return $"(X: {X:F2}, Y: {Y:F2}, Z: {Z:F2})";
         }
 
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Vector3))
+            {
+                return false;
+            }
+
+            var vector = (Vector3)obj;
+            return X == vector.X &&
+                   Y == vector.Y &&
+                   Z == vector.Z;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -307843816;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            hashCode = hashCode * -1521134295 + Z.GetHashCode();
+            return hashCode;
+        }
+
         public static bool operator ==(Vector3 v, Vector3 v2)
         {
             return v.X == v2.X && v.Y == v2.Y && v.Z == v2.Z;
