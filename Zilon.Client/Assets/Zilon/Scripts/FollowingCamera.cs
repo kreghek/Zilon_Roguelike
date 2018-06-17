294a172ac878d48b742f9f4def3dee27227bf0a7
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-06-17 23:54:25 +0700

    Теперь на клиенте камера следит за игроком вместо РТС-камеры.

---
+++ Zilon.Client/Assets/Zilon/Scripts/FollowingCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+
+	private ActorVM Target;
+	
+	// Use this for initialization
+	void Start () {
+		var actors = FindObjectsOfType<ActorVM>();
+		Target = actors.Single(x => !x.IsEnemy);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Vector3.Lerp(transform.position, 
+			Target.transform.position + new Vector3(0, 0, -10),
+			Time.deltaTime * 3);
+	}
+}
