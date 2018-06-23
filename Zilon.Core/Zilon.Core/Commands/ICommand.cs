b1d2e0d90cb01ef90d0775fe1ce9e77c69e94a46
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-06-23 15:08:41 +0700

    Revert "Удаление неиспользуемых классов."
    This reverts commit ea1970212ef9d6213db1a1b1c900c2945032f5e8.

---
+++ Zilon.Core/Zilon.Core/Commands/ICommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Zilon.Core.Commands
+{
+    /// <summary>
+    /// Интерфейс команды.
+    /// </summary>
+    /// <remarks>
+    /// Изменение состояния модели возможно только через команды.
+    /// </remarks>
+    public interface ICommand
+    {
+        void Execute();
+        bool CanExecute();
+    }
+}
