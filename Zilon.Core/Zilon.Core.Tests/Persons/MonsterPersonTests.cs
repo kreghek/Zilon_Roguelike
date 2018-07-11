6cc6755d90cdb68c11f7f18c0567a8904ec08cd5
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-07-11 22:57:45 +0700

    Добавлен тест на создание персонажа для монстра.

---
+++ Zilon.Core/Zilon.Core.Tests/Persons/MonsterPersonTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+using System;
+
+namespace Zilon.Core.Persons.Tests
+{
+    [TestFixture()]
+    public class MonsterPersonTests
+    {
+        [Test()]
+        public void Constructor_DefaultParams_NoException()
+        {
+            // ACT
+            Action act = () => { var monster = new MonsterPerson(); };
+
+
+
+            // ARRANGE
+            act.Should().NotThrow();
+        }
+    }
+}
