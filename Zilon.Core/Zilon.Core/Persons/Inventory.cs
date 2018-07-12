bf70229e3b09584f3a495f9c90660541528d188b
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-07-13 00:33:44 +0700

    Разработка инвентаря.

---
+++ Zilon.Core/Zilon.Core/Persons/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zilon.Core.Persons
+{
+    public class Inventory
+    {
+        private readonly List<PropBase> _items;
+
+        public Inventory()
+        {
+            _items = new List<PropBase>();
+        }
+
+        public PropBase[] Items => _items.ToArray();
+
+        public void Add(PropBase prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(PropBase prop)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
