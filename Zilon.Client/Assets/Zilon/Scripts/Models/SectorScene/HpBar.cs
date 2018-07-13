d3635fb87ef7722f06fc69055f67bd683c7f2ada
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-07-14 04:17:16 +0700

    Вывод Хп актёра.

---
+++ Zilon.Client/Assets/Zilon/Scripts/Models/SectorScene/HpBar.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Zilon.Scripts.Models.SectorScene;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class HpBar : MonoBehaviour
+{
+	public Image BarImage;
+	
+	[Inject] private IPlayerState _playerState;
+	
+	// Update is called once per frame
+	void Update () {
+		if (_playerState.ActiveActor == null)
+		{
+			return;
+		}
+
+		var actorVm = _playerState.ActiveActor;
+
+		var hpPercentage = CalcPercentage(actorVm.Actor.Hp, actorVm.Actor.Person.Hp);
+
+		BarImage.fillAmount = hpPercentage;
+	}
+
+	private float CalcPercentage(float actorHp, float personHp)
+	{
+		return actorHp/personHp;
+	}
+}
