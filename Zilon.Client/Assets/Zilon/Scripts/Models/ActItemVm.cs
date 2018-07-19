1acf07fe1ce8f7c312a980c62372ede4d4cecaac
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-07-20 00:22:01 +0700

    Добавлено отображение текущих действий активного актёра.

---
+++ Zilon.Client/Assets/Zilon/Scripts/Models/ActItemVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zilon.Core.Persons;
+
+public class ActItemVm : MonoBehaviour
+{
+
+	public event EventHandler Click;
+	public ITacticalAct Act { get; set; }
+	
+	public void Init(ITacticalAct act)
+	{
+		Act = act;
+	}
+
+	public void Click_Handler()
+	{
+		Click?.Invoke(this, new EventArgs());
+	}
+}
---
+++ Zilon.Client/Assets/Zilon/Scripts/Models/ActPanelHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.Zilon.Scripts.Models.SectorScene;
+using UnityEngine;
+using Zenject;
+using Zilon.Core.Persons;
+
+public class ActPanelHandler : MonoBehaviour {
+
+	[Inject] private IPlayerState _playerState;
+
+	public ActItemVm ActVmPrefab;
+	public Transform ActItemParent;
+
+	public void Start()
+	{
+		var actorVm = _playerState.ActiveActor;
+		var actor = actorVm.Actor;
+		
+		var acts = actor.Person.TacticalActCarrier.Acts;
+		UpdateActs(acts);
+	}
+
+	private void UpdateActs(IEnumerable<ITacticalAct> acts)
+	{
+		foreach (Transform item in ActItemParent)
+		{
+			Destroy(item.gameObject);
+		}
+
+		foreach (var act in acts)
+		{
+			var actItemVm = Instantiate(ActVmPrefab, ActItemParent);
+			actItemVm.Init(act);
+			actItemVm.Click += ActClick_Handler;
+		}
+	}
+
+	private void ActClick_Handler(object sender, EventArgs e)
+	{
+		var actItemVm = sender as ActItemVm;
+		Debug.Log(actItemVm.Act.Scheme.Sid);
+	}
+}
