305a184900043d5b651ca89703d5787e35341b7c
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-07-21 15:50:40 +0700

    Инфраструктура таблиц дропа.

---
+++ Zilon.Core/Zilon.Core/Schemes/TrophyTableRecord.cs
@@ -0,0 +1,53 @@
+﻿namespace Zilon.Core.Schemes
+{
+    /// <summary>
+    /// Запись в схеме таблицы дропа.
+    /// </summary>
+    /// <remarks>
+    /// Содержит информацию о том, какой продемет может выпасть,
+    /// количество/качество и с какой вероятностью.
+    /// </remarks>
+    public sealed class TrophyTableRecordSubScheme: SubSchemeBase
+    {
+        /// <summary>
+        /// Схема предмета.
+        /// </summary>
+        public string SchemeSid { get; set; }
+
+        /// <summary>
+        /// Вес записи в таблице дропа.
+        /// </summary>
+        /// <remarks>
+        /// Чем выше, тем веротянее будет выбрана данная запись при разрешении дропа.
+        /// </remarks>
+        public int Weight { get; set; }
+
+        /// <summary>
+        /// Минимальная мощь экипировки.
+        /// </summary>
+        public int MinPower { get; set; }
+
+        /// <summary>
+        /// Максимальная мощь экипировки.
+        /// </summary>
+        public int MaxPower { get; set; }
+
+        /// <summary>
+        /// Минимальное количество ресурса.
+        /// </summary>
+        public int MinCount { get; set; }
+
+        /// <summary>
+        /// Максимальное количество ресурса.
+        /// </summary>
+        public int MaxCount { get; set; }
+
+        /// <summary>
+        /// Концепт какого предмета.
+        /// </summary>
+        /// <remarks>
+        /// См. описание сущности концепта.
+        /// </remarks>
+        public string Concept { get; set; }
+    }
+}
---
+++ Zilon.Core/Zilon.Core/Schemes/TrophyTableScheme.cs
@@ -0,0 +1,11 @@
+﻿namespace Zilon.Core.Schemes
+{
+    /// <summary>
+    /// Схема тарблицы дропа.
+    /// </summary>
+    public sealed class TrophyTableScheme: SchemeBase
+    {
+        public TrophyTableRecordSubScheme[] Records { get; set; }
+        public int Rolls { get; set; }
+    }
+}
