a9989df81e5ef912e225954d8a2a2bcb464d836b
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-07-21 15:51:46 +0700

    Документирование схемы таблицы дропа.

--- Zilon.Core/Zilon.Core/Schemes/TrophyTableScheme.cs
+++ Zilon.Core/Zilon.Core/Schemes/TrophyTableScheme.cs
@@ -5,7 +5,14 @@
     /// </summary>
     public sealed class TrophyTableScheme: SchemeBase
     {
+        /// <summary>
+        /// Записи в таблице дропа.
+        /// </summary>
         public TrophyTableRecordSubScheme[] Records { get; set; }
+
+        /// <summary>
+        /// Количество бросков на проверку выпавшей записи.
+        /// </summary>
         public int Rolls { get; set; }
     }
 }
