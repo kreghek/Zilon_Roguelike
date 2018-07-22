dcd55d0e7bd0b772b6e9c292ad4b4677a78b6fd0
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-07-22 15:22:35 +0700

    Добавлено описание для контейнера с дропом.

--- Zilon.Core/Zilon.Core/Tactics/DropTablePropContainer.cs
+++ Zilon.Core/Zilon.Core/Tactics/DropTablePropContainer.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Zilon.Core.CommonServices.Dices;
-using Zilon.Core.Persons;
+﻿using Zilon.Core.Persons;
 using Zilon.Core.Schemes;
 using Zilon.Core.Tactics.Spatial;
 
 namespace Zilon.Core.Tactics
 {
-//TODO Добавить описание
+    /// <summary>
+    /// Контейнер со дропом, основанном на таблицах дропа.
+    /// </summary>
     public class DropTablePropContainer : IPropContainer
     {
         public IMapNode Node { get; private set; }
