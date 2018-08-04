66f593db4a5ad52af9014af7e6a43c3bfc7bff89
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-08-04 12:27:46 +0700

    Восстановлены тесты для MonsterActorTaskSource.

--- Zilon.Core/Zilon.Core.Tests/Tactics/Behaviour/Bots/MonsterActorTaskSourceTests.cs
+++ Zilon.Core/Zilon.Core.Tests/Tactics/Behaviour/Bots/MonsterActorTaskSourceTests.cs
@@ -1,5 +1,7 @@
 ﻿using FluentAssertions;
+
 using LightInject;
+
 using Moq;
 
 using NUnit.Framework;
@@ -7,6 +9,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using Zilon.Core.Common;
 using Zilon.Core.Persons;
 using Zilon.Core.Players;
@@ -90,91 +93,32 @@ namespace Zilon.Core.Tests.Tactics.Behaviour.Bots
         //TODO Убрать дублирование из тестов
         public void GetActorTasks_PatrolsTryToAttackEnemy_ReturnsAttackTask()
         {
-            //// ARRANGE
+            // ARRANGE
 
-            //// создание актёра бота
-            //var botPlayerMock = new Mock<IPlayer>();
-            //var botPlayer = botPlayerMock.Object;
+            var taskSource = _container.GetInstance<MonsterActorTaskSource>();
+            var actorManager = _container.GetInstance<IActorManager>();
 
-            //var botTacticalActStatsScheme = new TacticalActStatsSubScheme()
-            //{
-            //    Range = new Range<int>(1, 1)
-            //};
-
-            //var botTacticalActMock = new Mock<ITacticalAct>();
-            //botTacticalActMock.SetupGet(x => x.Stats).Returns(botTacticalActStatsScheme);
-            //var botTacticalAct = botTacticalActMock.Object;
-
-            //var botTacticalCarrierMock = new Mock<ITacticalActCarrier>();
-            //botTacticalCarrierMock.SetupProperty(x => x.Acts, new[] { botTacticalAct });
-            //var botTacticalCarrier = botTacticalCarrierMock.Object;
-
-            //var botPersonMock = new Mock<IPerson>();
-            //botPersonMock.SetupGet(x => x.TacticalActCarrier).Returns(botTacticalCarrier);
-            //var botPerson = botPersonMock.Object;
-
-            //var botActorMock = new Mock<IActor>();
-            //botActorMock.SetupGet(x => x.Owner).Returns(botPlayer);
-            //botActorMock.SetupGet(x => x.Person).Returns(botPerson);
-            //var botActor = botActorMock.Object;
-
-            //// Создание актёра игрока
-            //var humanPlayerMock = new Mock<IPlayer>();
-            //var humanPlayer = humanPlayerMock.Object;
-
-            //var humanActorMock = new Mock<IActor>();
-            //humanActorMock.SetupGet(x => x.Owner).Returns(humanPlayer);
-            //humanActorMock.Setup(x => x.CanBeDamaged()).Returns(true);
-            //var humanActor = humanActorMock.Object;
-
-            //var actorListInner = new List<IActor> { humanActor, botActor };
-            //var actorManagerMock = new Mock<IActorManager>();
-            //actorManagerMock.SetupGet(x => x.Actors).Returns(actorListInner);
-            //var actorManager = actorManagerMock.Object;
-
-            //var map = new TestGridGenMap();
-
-            //// Располагаем рядом игрока и бота
-            //var botActorNode = map.Nodes.Cast<HexNode>().SelectBy(1, 1);
-            //humanActorMock.SetupGet(x => x.Node).Returns(map.Nodes.Cast<HexNode>().SelectBy(2, 1));
-            //botActorMock.SetupGet(x => x.Node).Returns(() => botActorNode);
-            //botActorMock.Setup(x => x.MoveToNode(It.IsAny<IMapNode>()))
-            //    .Callback<IMapNode>((node) => botActorNode = (HexNode)node);
-
-            //var routePoints = new IMapNode[] {
-            //    map.Nodes.Cast<HexNode>().SelectBy(1,1),
-            //    map.Nodes.Cast<HexNode>().SelectBy(9,9)
-            //};
-            //var routeMock = new Mock<IPatrolRoute>();
-            //routeMock.SetupGet(x => x.Points).Returns(routePoints);
-            //var route = routeMock.Object;
-
-            //var patrolRoutes = new Dictionary<IActor, IPatrolRoute>
-            //{
-            //    { botActor, route }
-            //};
-
-            //var decisionSourceMock = new Mock<IDecisionSource>();
-            //var decisionSource = decisionSourceMock.Object;
-
-            //var taskSource = new MonsterActorTaskSource(botPlayer, patrolRoutes, decisionSource);
+            // Располагаем рядом игрока и бота
+            var intruderActor = CreateActor(2, 1);
+            _actorListInner.Add(intruderActor);
 
 
 
-            //// ACT
-            //var tasks = taskSource.GetActorTasks(map, actorManager);
+            // ACT
+            var tasks = taskSource.GetActorTasks(_map, actorManager);
 
 
 
-            //// ASSERT
-            //// бот должен начать двигаться к игроку.
-            //tasks[0].Should().BeOfType<AttackTask>();
+            // ASSERT
+            // бот должен начать двигаться к игроку.
+            tasks[0].Should().BeOfType<AttackTask>();
 
-            //Action act = () => {
-            //    tasks[0].Execute();
-            //};
+            Action act = () =>
+            {
+                tasks[0].Execute();
+            };
 
-            //act.Should().NotThrow();
+            act.Should().NotThrow();
         }
 
         /// <summary>
@@ -236,6 +180,7 @@ namespace Zilon.Core.Tests.Tactics.Behaviour.Bots
             actorMock.SetupGet(x => x.Node).Returns(() => actorNode);
             actorMock.Setup(x => x.MoveToNode(It.IsAny<IMapNode>()))
                 .Callback<IMapNode>((node) => actorNode = (HexNode)node);
+            actorMock.Setup(x => x.CanBeDamaged()).Returns(true);
             var actor = actorMock.Object;
 
             _actorListInner.Add(actor);
