9b78706a019a61f3e1fc1aaf7c338590a95396d7
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-08-06 13:07:32 +0700

    Восстановлен тест для EquipCommand.

--- Zilon.Core/Zilon.Core.Tests/Commands/EquipCommandTests.cs
+++ Zilon.Core/Zilon.Core.Tests/Commands/EquipCommandTests.cs
@@ -1,17 +1,18 @@
-﻿using System.Linq;
+﻿using FluentAssertions;
 
-using FluentAssertions;
+using LightInject;
 
 using Moq;
 
 using NUnit.Framework;
+
+using System.Linq;
 
 using Zilon.Core.Client;
 using Zilon.Core.Persons;
 using Zilon.Core.Schemes;
 using Zilon.Core.Tactics;
 using Zilon.Core.Tactics.Behaviour;
-using Zilon.Core.Tactics.Behaviour.Bots;
 using Zilon.Core.Tactics.Spatial;
 using Zilon.Core.Tests.TestCommon;
 
@@ -20,6 +21,8 @@ namespace Zilon.Core.Commands.Tests
     [TestFixture]
     public class EquipCommandTests
     {
+        private ServiceContainer _container;
+
         /// <summary>
         /// Тест проверяет, что можно использовать экипировку.
         /// </summary>
@@ -27,62 +30,7 @@ namespace Zilon.Core.Commands.Tests
         public void CanExecuteTest()
         {
             // ARRANGE
-            var actorManagerMock = new Mock<IActorManager>();
-            var actorManager = actorManagerMock.Object;
-
-            var containerManagerMock = new Mock<IPropContainerManager>();
-            var containerManager = containerManagerMock.Object;
-
-            var testMap = new TestGridGenMap();
-            var sector = new Sector(testMap, actorManager, containerManager);
-
-            var sectorManagerMock = new Mock<ISectorManager>();
-            sectorManagerMock.SetupProperty(x => x.CurrentSector, sector);
-            var sectorManager = sectorManagerMock.Object;
-
-
-            var actorMock = new Mock<IActor>();
-            var actorNode = testMap.Nodes.OfType<HexNode>().SelectBy(0, 0);
-            actorMock.SetupGet(x=>x.Node).Returns(actorNode);
-            var actor = actorMock.Object;
-
-            var actorVmMock = new Mock<IActorViewModel>();
-            actorVmMock.SetupProperty(x => x.Actor, actor);
-            var actorVm = actorVmMock.Object;
-
-            var targetMock = new Mock<IActor>();
-            var targetNode = testMap.Nodes.OfType<HexNode>().SelectBy(2, 0);
-            targetMock.SetupGet(x => x.Node).Returns(targetNode);
-            var target = targetMock.Object;
-
-            var targetVmMock = new Mock<IActorViewModel>();
-            targetVmMock.SetupProperty(x => x.Actor, target);
-            var targetVm = targetVmMock.Object;
-
-            var playerStateMock = new Mock<IPlayerState>();
-            playerStateMock.SetupProperty(x => x.ActiveActor, actorVm);
-            playerStateMock.SetupProperty(x => x.HoverViewModel, targetVm);
-            var playerState = playerStateMock.Object;
-
-
-            var propScheme = new PropScheme
-            {
-                Equip = new PropEquipSubScheme
-                {
-
-                }
-            };
-            var equipment = new Equipment(propScheme, new TacticalActScheme[0]);
-
-            var equipmentViewModelMock = new Mock<IPropItemViewModel>();
-            equipmentViewModelMock.SetupGet(x => x.Prop).Returns(equipment);
-            var equipmentViewModel = equipmentViewModelMock.Object;
-
-            var inventoryStateMock = new Mock<IInventoryState>();
-            inventoryStateMock.SetupProperty(x => x.SelectedProp, equipmentViewModel);
-            var inventoryState = inventoryStateMock.Object;
-
-            var command = new EquipCommand(sectorManager, playerState, inventoryState);
+            var command = _container.GetInstance<EquipCommand>();
             command.SlotIndex = 0;
 
 
@@ -95,22 +43,20 @@ namespace Zilon.Core.Commands.Tests
             canExecute.Should().Be(true);
         }
 
-        /// <summary>
-        /// Тест проверяет, что при выполнении команды корректно фисируется намерение игрока на атаку.
-        /// </summary>
-        [Test]
-        [Ignore("Привести к EquipCommand")]
-        public void ExecuteTest()
+        [SetUp]
+        public void SetUp ()
         {
+            _container = new ServiceContainer();
+
             var testMap = new TestGridGenMap();
 
-            var sectorMock = new Mock<ISector>();
-            sectorMock.SetupGet(x => x.Map).Returns(testMap);
+            var sectorMock =  new Mock<ISector>();
             var sector = sectorMock.Object;
 
             var sectorManagerMock = new Mock<ISectorManager>();
             sectorManagerMock.SetupProperty(x => x.CurrentSector, sector);
             var sectorManager = sectorManagerMock.Object;
+            _container.Register(factory => sectorManager);
 
 
             var actorMock = new Mock<IActor>();
@@ -131,10 +77,8 @@ namespace Zilon.Core.Commands.Tests
             targetVmMock.SetupProperty(x => x.Actor, target);
             var targetVm = targetVmMock.Object;
 
-            var decisionSourceMock = new Mock<IDecisionSource>();
-            var decisionSource = decisionSourceMock.Object;
-
-            var humanTaskSourceMock = new Mock<HumanActorTaskSource>(actor, decisionSource);
+            var humanTaskSourceMock = new Mock<IHumanActorTaskSource>();
+            _container.Register(factory => humanTaskSourceMock);
             var humanTaskSource = humanTaskSourceMock.Object;
 
             var playerStateMock = new Mock<IPlayerState>();
@@ -142,8 +86,40 @@ namespace Zilon.Core.Commands.Tests
             playerStateMock.SetupProperty(x => x.HoverViewModel, targetVm);
             playerStateMock.SetupProperty(x => x.TaskSource, humanTaskSource);
             var playerState = playerStateMock.Object;
+            _container.Register(factory => playerState);
 
-            var command = new AttackCommand(sectorManager, playerState);
+
+            var propScheme = new PropScheme
+            {
+                Equip = new PropEquipSubScheme
+                {
+
+                }
+            };
+            var equipment = new Equipment(propScheme, new TacticalActScheme[0]);
+
+            var equipmentViewModelMock = new Mock<IPropItemViewModel>();
+            equipmentViewModelMock.SetupGet(x => x.Prop).Returns(equipment);
+            var equipmentViewModel = equipmentViewModelMock.Object;
+
+            var inventoryStateMock = new Mock<IInventoryState>();
+            inventoryStateMock.SetupProperty(x => x.SelectedProp, equipmentViewModel);
+            var inventoryState = inventoryStateMock.Object;
+            _container.Register(factory => inventoryState);
+
+            _container.Register<EquipCommand>();
+        }
+
+        /// <summary>
+        /// Тест проверяет, что при выполнении команды корректно фисируется намерение игрока на атаку.
+        /// </summary>
+        [Test]
+        public void ExecuteTest()
+        {
+            var command = _container.GetInstance<EquipCommand>();
+            command.SlotIndex = 0;
+
+            var humanTaskSourceMock = _container.GetInstance<Mock<IHumanActorTaskSource>>();
 
 
 
@@ -152,7 +128,7 @@ namespace Zilon.Core.Commands.Tests
 
 
             // ASSERT
-            humanTaskSourceMock.Verify(x => x.IntentAttack(target));
+            humanTaskSourceMock.Verify(x => x.IntentEquip(It.IsAny<Equipment>(), 0));
         }
     }
 }
