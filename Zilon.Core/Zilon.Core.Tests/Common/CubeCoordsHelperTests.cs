4957e4118f45347b6f8d7551e10e285c7ed44a10
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-08-10 01:24:03 +0700

    Добавлены тесты на отрисовку линий в кубических системах координат.

---
+++ Zilon.Core/Zilon.Core.Tests/Common/CubeCoordsHelperTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+using System.Linq;
+using Zilon.Core.Tests.Common.TestCases;
+
+namespace Zilon.Core.Common.Tests
+{
+    [TestFixture]
+    public class CubeCoordsHelperTests
+    {
+        [Test]
+        [TestCaseSource(typeof(CubeCoordsHelperTestCases), nameof(CubeCoordsHelperTestCases.TestCases))]
+        public void CubeDrawLineTest(int sOffsetX, int sOffsetY, int offsetX, int offsetY)
+        {
+            // ARRANGE
+
+            var startCubeCoords = HexHelper.ConvertToCube(sOffsetX, sOffsetY);
+            var finishCubCoords = HexHelper.ConvertToCube(offsetX, offsetY);
+
+
+
+            // ACT
+            var line = CubeCoordsHelper.CubeDrawLine(startCubeCoords, finishCubCoords);
+
+
+
+            // ASSERT
+            foreach (var coord in line)
+            {
+                var sameCoords = line.Where(x => x == coord);
+                sameCoords.Count().Should().Be(1);
+
+                // Проверяем, что у каждой точки линии есть соседи,
+                // т.е. нет изолированных разорванных точк.
+                var neibourOffsets = HexHelper.GetOffsetClockwise();
+
+                var hasNeighbor = false;
+                foreach (var offset in neibourOffsets)
+                {
+                    var neibour = coord + offset;
+
+                    var neighborCoords = line.Where(x => x == coord);
+                    if (neighborCoords.Any())
+                    {
+                        hasNeighbor = true;
+                    }
+                }
+
+                hasNeighbor.Should().Be(true);
+            }
+        }
+    }
+}
---
+++ Zilon.Core/Zilon.Core.Tests/Common/TestCases/CubeCoordsHelperTestCases.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using System.Collections;
+
+namespace Zilon.Core.Tests.Common.TestCases
+{
+    public static class CubeCoordsHelperTestCases
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(0, 0, 5, 7);
+                yield return new TestCaseData(0, 0, 15, 17);
+            }
+        }
+    }
+}
