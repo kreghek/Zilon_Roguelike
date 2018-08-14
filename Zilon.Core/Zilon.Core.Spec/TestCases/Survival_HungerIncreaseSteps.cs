aafb1734e5c5a4ea484eae7576e0ba1528ae265c
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-08-14 08:06:04 +0700

    Добавлены спецификации для модуля выживания.

---
+++ Zilon.Core/Zilon.Core.Spec/TestCases/Survival_HungerIncreaseSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Zilon.Core.Spec.TestCases
+{
+    [Binding]
+    public class Survival_HungerIncreaseSteps
+    {
+        [Given(@"Есть произвольная карта")]
+        public void GivenЕстьПроизвольнаяКарта()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Есть персонаж игрока")]
+        public void GivenЕстьПерсонажИгрока()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Я перемещаю персонажа на одну клетку")]
+        public void WhenЯПеремещаюПерсонажаНаОднуКлетку()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Значение сытости уменьшилось на (.*) единицу")]
+        public void ThenЗначениеСытостиУменьшилосьНаЕдиницу(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
