38beee055ff239d75853f8e6cd6b66399ef8d5fa
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-08-18 13:32:00 +0700

    Забытый файл.

---
+++ Zilon.Core/Zilon.Core/Schemes/DropTableModificatorScheme.cs
@@ -0,0 +1,18 @@
+﻿namespace Zilon.Core.Schemes
+{
+    /// <summary>
+    /// Модификатор таблицы дропа.
+    /// </summary>
+    public sealed class DropTableModificatorScheme: SchemeBase
+    {
+        /// <summary>
+        /// Идентификаторы схем предметов, на которые модификатор влияет.
+        /// </summary>
+        public string[] PropSids { get; set; }
+
+        /// <summary>
+        /// Множитель от 0 до 1, на сколько увеличивается/уменьшается вес указанных предметов.
+        /// </summary>
+        public float WeightBonus { get; set; }
+    }
+}
