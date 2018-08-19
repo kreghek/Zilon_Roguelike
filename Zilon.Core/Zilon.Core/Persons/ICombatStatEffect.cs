2f4c3139514ffe43fd20df202df1ac87ae3b276d
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-08-19 19:08:07 +0700

    Введён интерфейс для эффектов, которые влияют на характеристики.

---
+++ Zilon.Core/Zilon.Core/Persons/ICombatStatEffect.cs
@@ -0,0 +1,20 @@
+﻿namespace Zilon.Core.Persons
+{
+    /// <summary>
+    /// Эффект, который влияет на модуль сражения.
+    /// </summary>
+    public interface ICombatStatEffect
+    {
+        /// <summary>
+        /// Применение эффекта при получении.
+        /// </summary>
+        /// <param name="combatStats"></param>
+        void ApplyOnce(ICombatStats combatStats);
+
+        /// <summary>
+        /// Применение эффекта каждый ход.
+        /// </summary>
+        /// <param name="combatStats"></param>
+        void ApplyEveryTurn(ICombatStats combatStats);
+    }
+}
