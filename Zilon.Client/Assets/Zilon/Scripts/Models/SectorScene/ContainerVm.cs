a993e3cfe3f1e961986b36e87386814f5d2b323e
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-09-08 12:20:40 +0700

    Реализовано визуальное отображение открытых сундуков на клиенте.

--- Zilon.Client/Assets/Zilon/Scripts/Models/SectorScene/ContainerVm.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Models/SectorScene/ContainerVm.cs
@@ -1,22 +1,49 @@
 ﻿using System;
+
 using UnityEngine;
+
 using Zilon.Core.Client;
 using Zilon.Core.Tactics;
 
 public class ContainerVm : MonoBehaviour, IContainerViewModel
 {
+    public SpriteRenderer SpriteRenderer;
+    public Sprite ClosedSprite;
+    public Sprite OpenedSprite;
 
-	public event EventHandler Selected;
 
-	public IPropContainer Container { get; set; }
+    public event EventHandler Selected;
 
-	public void OnMouseDown()
-	{
-		DoSelected();
-	}
+    public IPropContainer Container { get; set; }
 
-	private void DoSelected()
-	{
-		Selected?.Invoke(this, new EventArgs());
-	}
+    public void Start()
+    {
+        SpriteRenderer.sprite = ClosedSprite;
+        Container.IsOpenChanged += Container_IsOpenChanged;
+    }
+
+    public void OnMouseDown()
+    {
+        DoSelected();
+    }
+
+    private void DoSelected()
+    {
+        Selected?.Invoke(this, new EventArgs());
+    }
+
+
+    private void Container_IsOpenChanged(object sender, EventArgs e)
+    {
+        if (Container.IsOpened)
+        {
+            Debug.Log($"Сундук {sender} открыт");
+            SpriteRenderer.sprite = OpenedSprite;
+        }
+        else
+        {
+            Debug.Log($"Сундук {sender} закрыт");
+            SpriteRenderer.sprite = ClosedSprite;
+        }
+    }
 }
