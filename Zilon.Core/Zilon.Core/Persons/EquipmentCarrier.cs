192fb653d15cbf7e38dede52d88dad41b2170a3f
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-09-12 09:00:23 +0700

    Добавлен учёт типов слотов и предмета при экипировки.

--- Zilon.Core/Zilon.Core/Persons/EquipmentCarrier.cs
+++ Zilon.Core/Zilon.Core/Persons/EquipmentCarrier.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using Zilon.Core.Components;
 using Zilon.Core.Schemes;
 
 namespace Zilon.Core.Persons
@@ -10,14 +11,14 @@ namespace Zilon.Core.Persons
     {
         public EquipmentCarrier(IEnumerable<PersonSlotSubScheme> slots)
         {
-            Slots = slots;
+            Slots = slots.ToArray();
 
             Equipments = new Equipment[Slots.Count()];
         }
 
         public Equipment[] Equipments { get; }
 
-        public IEnumerable<PersonSlotSubScheme> Slots { get; }
+        public PersonSlotSubScheme[] Slots { get; }
 
         public event EventHandler<EquipmentChangedEventArgs> EquipmentChanged;
 
@@ -25,9 +26,47 @@ namespace Zilon.Core.Persons
         public void SetEquipment(Equipment equipment, int slotIndex)
         {
             var oldEquipment = Equipments[slotIndex];
-            Equipments[slotIndex] = equipment;
+
+            if (equipment != null)
+            {
+                var slot = Slots[slotIndex];
+
+                if ((slot.Types & equipment.Scheme.Equip.SlotTypes[0]) > 0)
+                {
+                    Equipments[slotIndex] = equipment;
+                }
+                else
+                {
+                    throw new ArgumentException($"Для экипировки указан слот {slot}, не подходящий для данного типа предмета {equipment}.");
+                }
+            }
+            else
+            {
+                Equipments[slotIndex] = equipment;
+            }
+            
 
             DoEquipmentChanged(slotIndex, oldEquipment, equipment);
+        }
+
+        private int FindFreeSlotIndex(EquipmentSlotTypes requiredSlotTypes)
+        {
+            for (var i = 0; i < Slots.Count(); i++)
+            {
+                var slot = Slots[i];
+
+                if (Equipments[i] != null)
+                {
+                    continue;
+                }
+
+                if ((slot.Types & requiredSlotTypes) > 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
         private void DoEquipmentChanged(int slotIndex,
