44983d9b90aad46d95f03d914b0f2dd75e788b74
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-09-21 19:11:16 +0700

    WIP Разработка теста для TacticalActUsageService

---
+++ Zilon.Core/Zilon.Core.Tests/Tactics/TacticalActUsageServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Zilon.Core.Tactics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zilon.Core.Tactics.Tests
+{
+    [TestFixture()]
+    public class TacticalActUsageServiceTests
+    {
+        [Test()]
+        public void UseOnTest()
+        {
+            // ARRANGE
+
+
+            var actUsageService = new TacticalActUsageService();
+        }
+    }
+}
