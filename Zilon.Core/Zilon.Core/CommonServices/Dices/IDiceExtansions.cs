d5728ede0ab1a5dab5c28b4fb1ee53e5cb1e2232
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-09-21 15:28:12 +0700

    Доработка кости для получения случайных чисел.

---
+++ Zilon.Core/Zilon.Core/CommonServices/Dices/IDiceExtansions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Zilon.Core.CommonServices.Dices
+{
+    public static class IDiceExtansions
+    {
+        /// <summary>
+        /// Получение случайного числа в указаном диапазоне [min, max].
+        /// </summary>
+        /// <param name="dice"> Используемая для броска кость. </param>
+        /// <param name="min"> Минимальное значение. </param>
+        /// <param name="max"> Максимальное значение. </param>
+        /// <returns> Возвращает случайное число в указанном диапазоне [min, max]. </returns>
+        public static int Roll(this IDice dice, int min, int max)
+        {
+            if (min < max)
+            {
+                throw new ArgumentException($"Максимальное значение {max} не может быть меньше минимального {min}.", nameof(max));
+            }
+
+            if (min == max)
+            {
+                return min;
+            }
+
+            var range = max - min;
+            var roll = dice.Roll(range);
+
+            return roll - 1 + min;
+        }
+    }
+}
--- Zilon.Core/Zilon.Core/Tactics/DropResolverRandomSource.cs
+++ Zilon.Core/Zilon.Core/Tactics/DropResolverRandomSource.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Zilon.Core.CommonServices.Dices;
+﻿using Zilon.Core.CommonServices.Dices;
 
 namespace Zilon.Core.Tactics
 {
@@ -18,27 +13,18 @@ namespace Zilon.Core.Tactics
 
         public int RollEquipmentPower(int minPower, int maxPower)
         {
-            return Roll(minPower, maxPower);
+            return _dice.Roll(minPower, maxPower);
         }
 
         public int RollResourceCount(int minCount, int maxCount)
         {
-            return Roll(minCount, maxCount);
+            return _dice.Roll(minCount, maxCount);
         }
 
         public int RollWeight(int totalWeight)
         {
             var roll = _dice.Roll(totalWeight);
             return roll;
-        }
-
-        //TODO Вынести в расширения для кости
-        private int Roll(int min, int max)
-        {
-            //TODO Добавить проверки: min <= max, не нули или обработать нули
-            var range = max - min;
-            var roll = _dice.Roll(range);
-            return roll + min;
         }
     }
 }
