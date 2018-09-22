6433950a7409d897328f19536fbcb436958cc7c3
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-09-22 13:19:46 +0700

    Исключение примитивных свойств и конструкторов DropTableLoot и SuccessOpenContainerResult из покрытия тестами.

--- Zilon.Core/Zilon.Core/Tactics/DropTableLoot.cs
+++ Zilon.Core/Zilon.Core/Tactics/DropTableLoot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 using Zilon.Core.Persons;
 using Zilon.Core.Schemes;
@@ -9,11 +10,15 @@ namespace Zilon.Core.Tactics
     /// <summary>
     /// Реализация контейнера для выпавшего из монстра лута.
     /// </summary>
-    public class DropTableLoot: IPropContainer
+    public class DropTableLoot : IPropContainer
     {
+        [ExcludeFromCodeCoverage]
         public IMapNode Node { get; }
 
+        [ExcludeFromCodeCoverage]
         public IPropStore Content { get; }
+
+        [ExcludeFromCodeCoverage]
         public bool IsOpened
         {
             get => true;
@@ -24,10 +29,13 @@ namespace Zilon.Core.Tactics
             }
         }
 
+        [ExcludeFromCodeCoverage]
         public int Id { get; set; }
 
+        [ExcludeFromCodeCoverage]
         public bool IsMapBlock => false;
 
+        [ExcludeFromCodeCoverage]
         public DropTableLoot(IMapNode node,
             DropTableScheme[] dropTables,
             IDropResolver dropResolver)
--- Zilon.Core/Zilon.Core/Tactics/SuccessOpenContainerResult.cs
+++ Zilon.Core/Zilon.Core/Tactics/SuccessOpenContainerResult.cs
@@ -1,16 +1,18 @@
-﻿using Zilon.Core.Persons;
+﻿using System.Diagnostics.CodeAnalysis;
+
+using Zilon.Core.Persons;
 
 namespace Zilon.Core.Tactics
 {
     public class SuccessOpenContainerResult: OpenContainerResultBase
     {
+        [ExcludeFromCodeCoverage]
         public SuccessOpenContainerResult(IProp[] props)
         {
             Props = props;
         }
 
+        [ExcludeFromCodeCoverage]
         public IProp[] Props { get; }
-
-        
     }
 }
