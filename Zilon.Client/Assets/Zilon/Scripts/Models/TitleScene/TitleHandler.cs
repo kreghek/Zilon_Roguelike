14bd1ee842645b8053502792f143e15cdf93e84e
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-10-13 17:26:27 +0700

    Добавлен стартовый экран.

---
+++ Zilon.Client/Assets/Zilon/Scripts/Models/TitleScene/TitleHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleHandler : MonoBehaviour {
+
+    public void CloseButtonHandler()
+    {
+        Application.Quit();
+    }
+
+    public void PlayButtonHandler()
+    {
+        SceneManager.LoadScene("combat");
+    }
+}
