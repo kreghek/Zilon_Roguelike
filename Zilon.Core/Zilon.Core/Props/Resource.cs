369c6cee16512ef8886c3ae094c9c5e46b2a661a
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-10-16 22:12:39 +0700

    Событие на изменение количества ресурса добавлено в класс Resource.

--- Zilon.Core/Zilon.Core/Props/Resource.cs
+++ Zilon.Core/Zilon.Core/Props/Resource.cs
@@ -4,8 +4,10 @@ using Zilon.Core.Schemes;
 
 namespace Zilon.Core.Props
 {
-    public class Resource : PropBase
+    public sealed class Resource : PropBase
     {
+        private int _count;
+
         public Resource(IPropScheme scheme, int count) : base(scheme)
         {
             if (count <= 0)
@@ -19,7 +21,28 @@ namespace Zilon.Core.Props
         /// <summary>
         /// Количество единиц ресурса.
         /// </summary>
-        public int Count { get; set; }
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                if (_count <= 0)
+                {
+                    throw new ArgumentException("Величина не может быть меньше или равна 0.");
+                }
+
+                _count = value;
+                DoChange();
+            }
+        }
+
+        private void DoChange()
+        {
+            Changed?.Invoke(this, new EventArgs());
+        }
 
         /// <summary>
         /// Разделяет текущий сток ресурсов и формирует новый экземпляр с указанным количеством.
@@ -32,5 +55,7 @@ namespace Zilon.Core.Props
             Count -= count;
             return resource2;
         }
+
+        public EventHandler<EventArgs> Changed;
     }
 }
