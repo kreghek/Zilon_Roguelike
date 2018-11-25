c81af0518cdfef7b24c36945a87f016054a4bc8d
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-11-26 06:42:02 +0700

    Удаление ненужного закомментированого кода.

--- Zilon.Core/Zilon.Core/Schemes/JobSubScheme.cs
+++ Zilon.Core/Zilon.Core/Schemes/JobSubScheme.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace Zilon.Core.Schemes
+﻿namespace Zilon.Core.Schemes
 {
     /// <summary>
     /// Структура для хранения работы.
@@ -13,207 +11,24 @@ namespace Zilon.Core.Schemes
         /// <summary>
         /// Тип работы.
         /// </summary>
-        [ExcludeFromCodeCoverage]
         public JobType Type { get; set; }
 
         /// <summary>
         /// Объём работы.
         /// </summary>
-        [ExcludeFromCodeCoverage]
         public int Value { get; set; }
 
         /// <summary>
         /// Область действия работы.
         /// </summary>
-        [ExcludeFromCodeCoverage]
         public JobScope Scope { get; set; }
 
         /// <summary>
         /// Дополнительные данные по работе.
         /// </summary>
-        [ExcludeFromCodeCoverage]
         public string[] Data { get; set; }
 
-        ////TODO Вынести весь этот подсчёт в отдельный сервис в пакете Persons
-        //public void CountDefeat(IActor hitActor)
-        //{
-        //    //TODO Здесь должно быть извлечение текущей фракции персонажа.
-        //    var faction = PersonFaction.Other;
-        //    switch (Type)
-        //    {
-        //        case JobType.Defeats:
-        //            Progress++;
-        //            break;
-
-        //        case JobType.DefeatGaarns:
-        //            if (faction.HasFlag(PersonFaction.Gaarns))
-        //                Progress++;
-        //            break;
-
-        //        case JobType.DefeatAleberts:
-        //            if (faction.HasFlag(PersonFaction.Aleberts))
-        //                Progress++;
-        //            break;
-
-        //        case JobType.DefeatLegions:
-        //            if (faction.HasFlag(PersonFaction.Legion))
-        //                Progress++;
-        //            break;
-
-        //        case JobType.DefeatDeamons:
-        //            if (faction.HasFlag(PersonFaction.Deamons))
-        //                Progress++;
-        //            break;
-
-        //        case JobType.DefeatTechbots:
-        //            if (faction.HasFlag(PersonFaction.Techbots))
-        //                Progress++;
-        //            break;
-
-        //        case JobType.DefeatCults:
-        //            if (faction.HasFlag(PersonFaction.Cult))
-        //                Progress++;
-        //            break;
-        //    }
-        //}
-
-        //public void CountKineticDeflection()
-        //{
-        //    if (Type == JobType.Blocks)
-        //        Progress++;
-        //}
-
-        //public void CountHit()
-        //{
-        //    if (Type == JobType.Hits)
-        //        Progress++;
-        //}
-
-        //public void CountMeleeHits()
-        //{
-        //    if (Type == JobType.MeleeHits)
-        //        Progress++;
-        //}
-
-        //public void CountOneUseHits(int count)
-        //{
-        //    if (Type == JobType.OneUseHits)
-        //        Progress = count;
-        //}
-
-        //public void CountOneUseDefeats(int count)
-        //{
-        //    if (Type == JobType.OneUseDefeats)
-        //        Progress = count;
-        //}
-
-        //public void CountHitsReceived()
-        //{
-        //    if (Type == JobType.ReceiveHits)
-        //        Progress++;
-        //}
-
-        //public void CountDamageReceived(int damage)
-        //{
-        //    if (Type == JobType.ReceiveDamage)
-        //        Progress += damage;
-        //}
-
-        //public void CountDamageReceivedPercent(int damage)
-        //{
-        //    if (Type == JobType.ReceiveDamagePercent)
-        //        Progress += damage;
-        //}
-
-        //public void CountCrits()
-        //{
-        //    if (Type == JobType.Crits)
-        //        Progress++;
-        //}
-
-        //public void CountCombat(bool winner)
-        //{
-        //    if (Type == JobType.Combats)
-        //        Progress++;
-
-        //    if (Type == JobType.Victories && winner)
-        //        Progress++;
-        //}
-
-        //public bool CountCraft(IProp product)
-        //{
-        //    if (Type == JobType.CraftAssaultRifle)
-        //    {
-        //        if (product.Scheme.Sid == "assault-rifle" ||
-        //            product.Scheme.Sid == "assault-rifle-bs" ||
-        //            product.Scheme.Sid == "protbane-rifle")
-        //        {
-        //            Progress++;
-        //            return true;
-        //        }
-        //    }
-
-        //    if (Type == JobType.Craft)
-        //    {
-        //        if (Data == null)
-        //        {
-        //            Progress++;
-        //            return true;
-        //        }
-        //        else
-        //        {
-        //            const string professionDataKey = "profession";
-        //            foreach (var dataItem in Data)
-        //            {
-        //                if (dataItem.StartsWith(professionDataKey))
-        //                {
-        //                    var professionKey = dataItem.Remove(0, professionDataKey.Length + 1);
-        //                    var professionType = (ProfessionTypes)Enum.Parse(typeof(ProfessionTypes), professionKey);
-        //                    var usedProfession = product.Scheme.Craft.Professions.SingleOrDefault(x => x.Profession == professionType);
-        //                    if (usedProfession != null)
-        //                    {
-        //                        switch (product)
-        //                        {
-        //                            case Equipment equipment:
-        //                                Progress++;
-        //                                break;
-
-        //                            case Resource resource:
-        //                                Progress += resource.Count;
-        //                                break;
-
-        //                            default:
-        //                                throw new Exception($"Данный продукт {product} не может учитываться в работам по крафту.");
-        //                        }
-
-        //                        return true;
-        //                    }
-        //                }
-        //            }
-        //        }
-        //    }
-
-
-
-        //    return false;
-        //}
-
-        //public bool CountDefeatGenerations(string defeatedClass)
-        //{
-        //    if (Type != JobType.DefeatClasses)
-        //    {
-        //        return false;
-        //    }
-
-        //    var jobClasses = Data;
-        //    if (jobClasses.Contains(defeatedClass))
-        //    {
-        //        Progress++;
-        //        return true;
-        //    }
-
-        //    return false;
-        //}
+        //TODO Вернуть получение наименования работ для клиента. Доработать с учётом локализации.
 
         //public static string GetDescription(PerkJob schemeJob, PerkJob factJob, bool status = false)
         //{
