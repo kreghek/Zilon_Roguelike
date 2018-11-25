76d6d86dbe3cd84ceca47f54c3c9ad309546976f
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-11-26 06:41:36 +0700

    Разработка теста для проверки DefeatActorJobProgress

---
+++ Zilon.Core/Zilon.Core.Tests/Persons/DefeatActorJobProgressTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+
+using Moq;
+
+using NUnit.Framework;
+using Zilon.Core.Schemes;
+using Zilon.Core.Tactics;
+
+namespace Zilon.Core.Persons.Tests
+{
+    [TestFixture]
+    public class DefeatActorJobProgressTests
+    {
+        [Test]
+        public void ApplyToJobsTest()
+        {
+            // ARRANGE
+            const int startProgress = 1;
+            const int expectedProgress = startProgress + 1;
+
+            var actorMock = new Mock<IActor>();
+            var actor = actorMock.Object;
+
+            var jobMock = new Mock<IJob>();
+            jobMock.SetupProperty(x => x.Progress, startProgress);
+            jobMock.SetupGet(x=>x.Scheme).Returns(new JobSubScheme {
+
+            })
+            var job = jobMock.Object;
+
+            var progress = new DefeatActorJobProgress(actor);
+
+
+
+
+            // ACT
+            progress.ApplyToJobs(new IJob[] { job });
+
+
+
+            // ASSERT
+            job.Progress.Should().Be(expectedProgress);
+        }
+    }
+}
