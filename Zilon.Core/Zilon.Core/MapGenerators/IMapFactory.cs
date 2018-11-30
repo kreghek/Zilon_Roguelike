43526e26c504f798bf172bc071a32e04eb43cbdf
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-11-30 08:31:38 +0700

    Введён интерфейс для фабрики карт.

---
+++ Zilon.Core/Zilon.Core/MapGenerators/IMapFactory.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+
+using Zilon.Core.Tactics.Spatial;
+
+namespace Zilon.Core.MapGenerators
+{
+    /// <summary>
+    /// Интерфейс фабрики карты.
+    /// </summary>
+    /// <remarks>
+    /// Различные реализации фабрики создают карту согласно различным алгоритмам:
+    /// 1. Карта по схеме.
+    /// 2. Процедурная генерация.
+    /// </remarks>
+    public interface IMapFactory
+    {
+        /// <summary>
+        /// Создание карты.
+        /// </summary>
+        /// <returns> Возвращает экземпляр карты. </returns>
+        [NotNull] IMap Create();
+    }
+}
