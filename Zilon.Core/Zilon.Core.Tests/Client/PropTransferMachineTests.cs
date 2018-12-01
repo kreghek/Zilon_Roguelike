64c918af0b1bf66fc991227f21d3af0497ccc4ca
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-12-01 19:30:51 +0700

    Добавлен тест для исправления перекладывания экипировки из сундука в инвентарь.

--- Zilon.Core/Zilon.Core.Tests/Client/PropTransferMachineTests.cs
+++ Zilon.Core/Zilon.Core.Tests/Client/PropTransferMachineTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
 
 using Moq;
 
@@ -10,18 +11,22 @@ using Zilon.Core.Props;
 using Zilon.Core.Schemes;
 using Zilon.Core.Tactics;
 using Zilon.Core.Tactics.Spatial;
+using Zilon.Core.Tests.Common.Schemes;
 
 namespace Zilon.Core.Tests.Client
 {
     [TestFixture]
     public class PropTransferMachineTests
     {
+        /// <summary>
+        /// Тест проверяет корректность переноса ресурса из сундука в инвентарь.
+        /// </summary>
         [Test]
-        public void TransferPropTest()
+        public void TransferProp_Resources()
         {
             // ARRANGE
 
-            var resourceScheme = new PropScheme();
+            var resourceScheme = new TestPropScheme();
 
             // Инвентарь
             var inventory = new Inventory();
@@ -46,10 +51,53 @@ namespace Zilon.Core.Tests.Client
                 transferMachine.Inventory);
 
 
+
             // ASSERT
             transferMachine.Inventory.PropAdded[0].Should().BeOfType<Resource>();
             var invResource = (Resource)transferMachine.Inventory.PropAdded[0];
             invResource.Count.Should().Be(1);
+        }
+
+
+        /// <summary>
+        /// Тест проверяет корректность переноса ресурса из сундука в инвентарь.
+        /// </summary>
+        [Test]
+        public void TransferProp_Equipment()
+        {
+            // ARRANGE
+
+            var resourceScheme = new TestPropScheme {
+                Equip = new TestPropEquipSubScheme()
+            };
+
+            // Инвентарь
+            var inventory = new Inventory();
+
+            // контейнер
+            var containerProps = new IProp[] {
+                new Equipment(resourceScheme, new ITacticalActScheme[0])
+            };
+            var nodeMock = new Mock<IMapNode>();
+            var node = nodeMock.Object;
+            var container = new FixedPropChest(node, containerProps);
+
+            // трансферная машина
+            var transferMachine = new PropTransferMachine(inventory, container.Content);
+
+
+
+            // ACT
+            var transferResource = containerProps.First();
+            transferMachine.TransferProp(transferResource,
+                transferMachine.Container,
+                transferMachine.Inventory);
+
+
+
+            // ASSERT
+            transferMachine.Inventory.PropAdded[0].Should().BeOfType<Equipment>();
+            transferMachine.Container.PropRemoved[0].Should().BeOfType<Equipment>();
         }
     }
 }
