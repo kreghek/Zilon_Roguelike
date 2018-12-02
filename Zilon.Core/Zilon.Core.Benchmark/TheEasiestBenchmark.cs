9d271e517acda8704d9af014cb221dc5c8b79797
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-12-02 15:20:38 +0700

    Проба бенчмарка для отладки лагов.

---
+++ Zilon.Core/Zilon.Core.Benchmark/TheEasiestBenchmark.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+using BenchmarkDotNet.Attributes;
+
+namespace Zilon.Core.Benchmark
+{
+    [ArtifactsPath(@"c:\benchmarkdotnet")]
+    //[DryJob]
+    public class TheEasiestBenchmark
+    {
+        [Benchmark(Description = "Summ100")]
+        public int Test100()
+        {
+            return Enumerable.Range(1, 100).Sum();
+        }
+
+        [Benchmark(Description = "Summ200")]
+        public int Test200()
+        {
+            return Enumerable.Range(1, 200).Sum();
+        }
+    }
+}
---
+++ Zilon.Core/Zilon.Core.Benchmark/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Running;
+
+using NUnit.Framework;
+
+namespace Zilon.Core.Benchmark
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            BenchmarkRunner.Run<TheEasiestBenchmark>();
+        }
+    }
+}
