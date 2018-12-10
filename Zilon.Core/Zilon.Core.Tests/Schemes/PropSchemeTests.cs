2fb62071e81bc5d30431f3e1ffb5391c840e1799
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-12-10 08:43:05 +0700

    Добавлен тест для проверки десериализации экипировки с показателями брони.

---
+++ Zilon.Core/Zilon.Core.Tests/Schemes/PropSchemeTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+
+using Newtonsoft.Json;
+
+using NUnit.Framework;
+
+using Zilon.Core.Common;
+using Zilon.Core.Components;
+using Zilon.Core.Schemes;
+
+namespace Zilon.Core.Tests.Schemes
+{
+    [TestFixture]
+    public class PropSchemeTests
+    {
+        /// <summary>
+        /// Тест проверяет, что работы корректно десериализуется.
+        /// </summary>
+        [Test]
+        public void Deserialization_JobsInLevel_TypeAndValueAreCorrect()
+        {
+            // ARRANGE
+            var sourceText = "";
+//            var sourceText = @"{
+//  Equip: {
+//                "SlotTypes": [ "Body" ],
+//    "Armors": [
+//      {
+//        "Impact": "Kinetic",
+//        "AbsorbtionLevel": "Normal",
+//        "ArmorRank": 5
+//      }
+//    ]
+//  },
+//  "Name": {
+//    "En": "Steel Armor",
+//	  "Ru": "Стальная броня"
+//  }
+//}
+//";
+
+
+
+            // ACT
+            var factPropScheme = JsonConvert.DeserializeObject<PropScheme>(sourceText);
+
+
+
+            // ASSERT
+            factPropScheme.Equip.Armors[0].Impact.Should().Be(ImpactType.Kinetic);
+            factPropScheme.Equip.Armors[0].AbsorbtionLevel.Should().Be(PersonRuleLevel.Normal);
+            factPropScheme.Equip.Armors[0].ArmorRank.Should().Be(5);
+        }
+    }
+}
