5d6e99153b8565b333e9d70315e084fffb415158
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-12-11 14:42:14 +0700

    Добавлен обработчик текста с наименованием сектора.

---
+++ Zilon.Client/Assets/Zilon/Scripts/Models/SectorScene/SectornameHad.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Zilon.Scripts.Services;
+
+using JetBrains.Annotations;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Zenject;
+
+public class SectornameHad : MonoBehaviour
+{
+    public Text SectorNameText;
+
+    [Inject] [NotNull] private readonly IHumanPersonManager _humanPersonManager;
+
+    public void FixedUpdate()
+    {
+        if (_humanPersonManager.SectorName != null)
+        {
+            var name = _humanPersonManager.SectorName;
+            var level = _humanPersonManager.SectorLevel + 1;
+            SectorNameText.text = $"{name} lvl{level}";
+
+            Destroy(this);
+        }
+    }
+}
