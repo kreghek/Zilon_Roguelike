8fe806590be848ef9e05e24a63af2b0411016b45
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-12-12 12:29:53 +0700

    WIP Добавлен запрет на открытие сундука через стены.

--- Zilon.Core/Zilon.Core/Tactics/Behaviour/OpenContainerTask.cs
+++ Zilon.Core/Zilon.Core/Tactics/Behaviour/OpenContainerTask.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using JetBrains.Annotations;
+
 using Zilon.Core.Tactics.Spatial;
 
 namespace Zilon.Core.Tactics.Behaviour
@@ -11,16 +13,20 @@ namespace Zilon.Core.Tactics.Behaviour
     {
         private readonly IPropContainer _container;
         private readonly IOpenContainerMethod _method;
+        private readonly IMap _map;
 
-        public OpenContainerTask(IActor actor, IPropContainer container, IOpenContainerMethod method) : base(actor)
+        public OpenContainerTask([NotNull] IActor actor,
+            [NotNull] IPropContainer container,
+            [NotNull] IOpenContainerMethod method,
+            [NotNull] IMap map) : base(actor)
         {
             _container = container ?? throw new ArgumentNullException(nameof(container));
             _method = method ?? throw new ArgumentNullException(nameof(method));
+            _map = map ?? throw new ArgumentNullException(nameof(map));
         }
 
         protected override void ExecuteTask()
         {
-            //TODO Сделать проверку, чтобы нельзя было открывать сундуки через стены аналогично команде на открытие.
             var actorHexNode = (HexNode)Actor.Node;
             var containerHexNode = (HexNode)_container.Node;
 
@@ -28,10 +34,16 @@ namespace Zilon.Core.Tactics.Behaviour
             var containerCoords = containerHexNode.CubeCoords;
 
             var distance = actorCoords.DistanceTo(containerCoords);
-
             if (distance > 1)
             {
                 throw new InvalidOperationException("Невозможно взаимодействовать с контейнером на расстоянии больше 1.");
+            }
+
+            var targetIsOnLine = MapHelper.CheckNodeAvailability(_map, Actor.Node, containerHexNode);
+
+            if (!targetIsOnLine)
+            {
+                throw new InvalidOperationException("Задачу на открытие сундука нельзя выполнить сквозь стены.");
             }
 
             Actor.OpenContainer(_container, _method);
