027fb89a8e5cecfa485faac43b2950350daac8a0
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-12-18 11:08:08 +0700

    Добавлена проверка на выполнение в команду атаки.

--- Zilon.Core/Zilon.Core/Commands/AttackCommand.cs
+++ Zilon.Core/Zilon.Core/Commands/AttackCommand.cs
@@ -1,8 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 using Zilon.Core.Client;
 using Zilon.Core.Persons;
+using Zilon.Core.Props;
 using Zilon.Core.Tactics;
 using Zilon.Core.Tactics.Behaviour;
 using Zilon.Core.Tactics.Spatial;
@@ -41,21 +43,59 @@ namespace Zilon.Core.Commands
 
             var targetNode = selectedActorViewModel.Actor.Node;
 
-            var targetIsOnLine = MapHelper.CheckNodeAvailability(map, currentNode, targetNode);
             var act = PlayerState.ActiveActor.Actor.Person.TacticalActCarrier.Acts.FirstOrDefault();
             var isInDistance = act.CheckDistance(((HexNode)currentNode).CubeCoords, ((HexNode)targetNode).CubeCoords);
+            if (!isInDistance)
+            {
+                return false;
+            }
 
-            var canExecute = targetIsOnLine && isInDistance;
+            var targetIsOnLine = MapHelper.CheckNodeAvailability(map, currentNode, targetNode);
+            if (!targetIsOnLine)
+            {
+                return false;
+            }
 
-            //TODO Добавить проверку:
-            // 1. Выбран ли вражеский юнит.
-            // 2. Находится ли в пределах досягаемости оружия. (1) Проверяется.
-            // 3. Видим ли текущим актёром. (0.5) Условная видимость по прямой.
-            // 4. Способно ли оружие атаковать.
-            // 5. Доступен ли целевой актёр для атаки.
-            // 6. Возможно ли выполнение каких-либо команд над актёрами
-            // (Нельзя, если ещё выполняется текущая команда. Например, анимация перемещения.)
-            return canExecute;
+            if (act.Constrains?.PropResourceType != null)
+            {
+                var hasPropResource = CheckPropResource(PlayerState.ActiveActor.Actor.Person.Inventory,
+                    act.Constrains.PropResourceType,
+                    act.Constrains.PropResourceCount.Value);
+
+                if (!hasPropResource)
+                {
+                    return false;
+                }
+            }
+
+
+            return true;
+        }
+
+        private bool CheckPropResource(IPropStore inventory,
+            string usedPropResourceType, 
+            int usedPropResourceCount)
+        {
+            var propResources = from prop in inventory.CalcActualItems()
+                                where prop is Resource
+                                where prop.Scheme.Bullet?.Caliber == usedPropResourceType
+                                select prop;
+
+            if (propResources.FirstOrDefault() is Resource propResource)
+            {
+                if (propResource.Count >= usedPropResourceCount)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
 
         private IActorViewModel GetSelectedActorViewModel()
