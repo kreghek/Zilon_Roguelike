93d6acebfe68b1e5517d5756b09ddde67528350d
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-12-26 00:05:06 +0700

    Убраны лишние плашки

--- Zilon.Core/Zilon.Core/Client/PlayerState.cs
+++ Zilon.Core/Zilon.Core/Client/PlayerState.cs
@@ -6,13 +6,10 @@ namespace Zilon.Core.Client
 {
     public class PlayerState : IPlayerState
     {
-        [ExcludeFromCodeCoverage]
         public IActorViewModel ActiveActor { get; set; }
 
-        [ExcludeFromCodeCoverage]
         public ISelectableViewModel HoverViewModel { get; set; }
 
-        [ExcludeFromCodeCoverage]
         public IHumanActorTaskSource TaskSource { get; set; }
     }
 }
