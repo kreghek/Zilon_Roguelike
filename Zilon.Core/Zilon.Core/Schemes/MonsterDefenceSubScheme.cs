f57a7d2487786b2dfae56d516882cd5e53360429
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-12-28 12:59:12 +0700

    Добавлена оборона для редких монстров первого кластера

--- Zilon.Core/Zilon.Core/Schemes/MonsterDefenceSubScheme.cs
+++ Zilon.Core/Zilon.Core/Schemes/MonsterDefenceSubScheme.cs
@@ -1,19 +1,11 @@
-﻿using System.Linq;
-
-using JetBrains.Annotations;
-
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace Zilon.Core.Schemes
 {
     public class MonsterDefenceSubScheme : IMonsterDefenseSubScheme
     {
-        [JsonConstructor]
-        public MonsterDefenceSubScheme([CanBeNull] [ItemNotNull] MonsterDefenceItemSubScheme[] defences)
-        {
-            Defenses = defences?.Cast<IMonsterDefenceItemSubScheme>().ToArray();
-        }
-
-        public IMonsterDefenceItemSubScheme[] Defenses { get; }
+        [JsonProperty]
+        [JsonConverter(typeof(ConcreteTypeConverter<MonsterDefenceItemSubScheme[]>))]
+        public IMonsterDefenceItemSubScheme[] Defenses { get; private set; }
     }
 }
