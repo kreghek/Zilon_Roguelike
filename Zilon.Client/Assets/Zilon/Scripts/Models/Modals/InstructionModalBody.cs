2414e4670027b008fc364d4fcd8ac6f573ab5f0a
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-01-08 02:39:53 +0700

    #104 Модал на клиенте со стартовой инструкции.

---
+++ Zilon.Client/Assets/Zilon/Scripts/Models/Modals/InstructionModalBody.cs
@@ -0,0 +1,63 @@
+﻿using Assets.Zilon.Scripts;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionModalBody : MonoBehaviour, IModalWindowHandler
+{
+    private int _pageCounter;
+
+    public GameObject[] Pages;
+    public Button NextButton;
+    public Button PrevButton;
+
+    public void Start()
+    {
+        _pageCounter = 0;
+        ShowPage();
+    }
+
+    public void NextButton_Handler()
+    {
+        _pageCounter++;
+
+        if (_pageCounter >= Pages.Length)
+        {
+            _pageCounter = Pages.Length - 1;
+        }
+
+        ShowPage();
+    }
+
+    public void PrevButton_Handler()
+    {
+        _pageCounter--;
+
+        if (_pageCounter < 0)
+        {
+            _pageCounter = 0;
+        }
+
+        ShowPage();
+    }
+
+    public void ApplyChanges()
+    {
+
+    }
+
+    public void CancelChanges()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void ShowPage()
+    {
+        for (int i = 0; i < Pages.Length; i++)
+        {
+            var page = Pages[i];
+
+            page.SetActive(i == _pageCounter);
+        }
+    }
+}
