7fc9a166a2fdfc24d7c30eb5371ce12c8f9c0ca3
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-01-07 20:26:21 +0700

    #106 Эффекты отображаются иконками

--- Zilon.Client/Assets/Zilon/Scripts/Models/SectorScene/EffectViewModel.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Models/SectorScene/EffectViewModel.cs
@@ -1,15 +1,64 @@
-﻿using UnityEngine;
+﻿using System;
+
+using UnityEngine;
 using UnityEngine.UI;
+
+using Zilon.Core.Persons;
 
 public class EffectViewModel : MonoBehaviour
 {
+    public Image EffectIcon;
+    public Image Background;
 
-	public Text TitleText;
-	
-	public string Title { get; set; }
+    public Sprite HungerSprite;
+    public Sprite ThristSprite;
 
-	void Update()
-	{
-		TitleText.text = Title;
-	}
+    public SurvivalStatType Type { get; private set; }
+    public SurvivalStatHazardLevel Level { get; private set; }
+
+    public void Init(SurvivalStatType type, SurvivalStatHazardLevel level)
+    {
+        Type = type;
+        Level = level;
+        SelectIcon(type);
+        HighlightLevel(level);
+    }
+
+    private void HighlightLevel(SurvivalStatHazardLevel level)
+    {
+        switch (level)
+        {
+            case SurvivalStatHazardLevel.Lesser:
+                Background.color = Color.white;
+                break;
+
+            case SurvivalStatHazardLevel.Strong:
+                Background.color = new Color(0.3f, 0,0, 1);
+                break;
+
+            case SurvivalStatHazardLevel.Max:
+                Background.color = new Color(1f, 0, 0, 1);
+                break;
+
+            default:
+                throw new InvalidOperationException();
+        }
+    }
+
+    private void SelectIcon(SurvivalStatType type)
+    {
+        switch (type)
+        {
+            case SurvivalStatType.Satiety:
+                EffectIcon.sprite = HungerSprite;
+                break;
+
+            case SurvivalStatType.Water:
+                EffectIcon.sprite = ThristSprite;
+                break;
+
+            default:
+                throw new InvalidOperationException();
+        }
+    }
 }
