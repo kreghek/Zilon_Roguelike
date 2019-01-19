ea0bfe09bb03c56470c8f243fe4df1b8d600ce12
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-01-19 10:01:21 +0700

    #158 Разбор и конвертация TODO в задачи

--- Zilon.Core/Zilon.Core/Commands/OpenContainerCommand.cs
+++ Zilon.Core/Zilon.Core/Commands/OpenContainerCommand.cs
@@ -37,11 +37,6 @@ namespace Zilon.Core.Commands
 
             var canExecute = MapHelper.CheckNodeAvailability(map, currentNode, targetNode);
 
-            //TODO добавить проверки:
-            // 1. Можно ли открыть контейнер.
-            // 2. Способен ли актёр открывать контейнеры.
-            // 3. Находится ли контейнера в зоне видимости. Проверяется.
-
             return canExecute;
         }
 
