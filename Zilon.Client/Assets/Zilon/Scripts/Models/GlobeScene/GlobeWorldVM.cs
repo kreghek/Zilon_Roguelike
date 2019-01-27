8baf6ad20dce56e48a644325a4f2af00725d3c9d
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-01-27 12:53:13 +0700

    #261 Реализация соединения узлов глобальной карты

--- Zilon.Client/Assets/Zilon/Scripts/Models/GlobeScene/GlobeWorldVM.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Models/GlobeScene/GlobeWorldVM.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 using Assets.Zilon.Scripts.Services;
 
@@ -9,6 +10,7 @@ using Zenject;
 using Zilon.Core.Common;
 using Zilon.Core.Players;
 using Zilon.Core.Tactics.Spatial;
+using Zilon.Core.World;
 
 public class GlobeWorldVM : MonoBehaviour
 {
@@ -32,12 +34,34 @@ public class GlobeWorldVM : MonoBehaviour
 
         var region = _globeManager.GenerateRegion(currentGlobeCell);
 
-        foreach (HexNode globeRegionNode in region.Nodes)
+        var locationNodeViewModels = new List<MapLocation>(100);
+        foreach (GlobeRegionNode globeRegionNode in region.Nodes)
         {
             var worldCoords = HexHelper.ConvertToWorld(globeRegionNode.OffsetX, globeRegionNode.OffsetY);
 
             var locationObject = _container.InstantiatePrefab(LocationPrefab, transform);
             locationObject.transform.position = new Vector3(worldCoords[0], worldCoords[1], 0);
+            var locationViewModel = locationObject.GetComponent<MapLocation>();
+            locationViewModel.Node = globeRegionNode;
+            locationNodeViewModels.Add(locationViewModel);
+        }
+
+        var openNodeViewModels = new List<MapLocation>(locationNodeViewModels);
+        while (openNodeViewModels.Any())
+        {
+            var currentNodeViewModel = openNodeViewModels[0];
+            openNodeViewModels.Remove(currentNodeViewModel);
+
+            var neighbors = region.GetNext(currentNodeViewModel.Node);
+            var neighborViewModels = openNodeViewModels.Where(x => neighbors.Contains(x.Node)).ToArray();
+            foreach (var neibourNodeViewModel in neighborViewModels)
+            {
+                var connectorObject = _container.InstantiatePrefab(ConnectorPrefab, transform);
+                var connectorViewModel = connectorObject.GetComponent<MapLocationConnector>();
+                connectorViewModel.gameObject1 = currentNodeViewModel.gameObject;
+                connectorViewModel.gameObject2 = neibourNodeViewModel.gameObject;
+                //openNodeViewModels.Remove(neibourNodeViewModel);
+            }
         }
     }
 }
--- Zilon.Client/Assets/Zilon/Scripts/Models/GlobeScene/MapLocation.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Models/GlobeScene/MapLocation.cs
@@ -2,14 +2,14 @@
 
 using UnityEngine;
 
+using Zilon.Core.World;
+
 public class MapLocation : MonoBehaviour
 {
-    public ModalDialog DialogPrefab;
-    public Canvas Canvas;
     public event EventHandler OnSelect;
     public SpriteRenderer Icon;
 
-    public string Sid { get; set; }
+    public GlobeRegionNode Node { get; set; }
 
     public void OnMouseDown()
     {
