efc90aadfac30a306103d48b0b314cfbb2338d2f
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-02-07 08:15:18 +0700

    #11 Настройки карты и сундуков в схеме генерации сектора

--- Zilon.Core/Zilon.Core/Schemes/ISectorSubScheme.cs
+++ Zilon.Core/Zilon.Core/Schemes/ISectorSubScheme.cs
@@ -8,16 +8,52 @@
         /// <summary>
         /// Идентфиикаторы обычных монстров, встречаемых в секторе.
         /// </summary>
-        string[] RegularMonsterSids { get; set; }
+        string[] RegularMonsterSids { get; }
 
         /// <summary>
         /// Идентификаторы редких монстров, встречаемых в секторе.
         /// </summary>
-        string[] RareMonsterSids { get; set; }
+        string[] RareMonsterSids { get; }
 
         /// <summary>
         /// Идентификаторы боссов, встречаемых в секторе.
         /// </summary>
-        string[] ChampionMonsterSids { get; set; }
+        string[] ChampionMonsterSids { get; }
+
+        /// <summary>
+        /// Тип фабрики для карты.
+        /// </summary>
+        SectorSubSchemeMapFactory MapFactory { get; }
+
+        /// <summary>
+        /// Количество регионов в карте.
+        /// </summary>
+        /// <remarks>
+        /// Для подземелий это количество комнат.
+        /// </remarks>
+        int RegionCount { get; }
+
+        /// <summary>
+        /// Максимальный размер комнат.
+        /// </summary>
+        /// <remarks>
+        /// Минимальный размер всегда 2х2.
+        /// </remarks>
+        int RegionSize { get; }
+
+        /// <summary>
+        /// Количество монстров в секторе.
+        /// </summary>
+        int MonsterCount { get; }
+
+        /// <summary>
+        /// Количество сундуков в секторе.
+        /// </summary>
+        int ChestCount { get; }
+
+        /// <summary>
+        /// Таблицы дропа для сундуков.
+        /// </summary>
+        string[] ChestDropTableSids { get; }
     }
 }
--- Zilon.Core/Zilon.Core/Schemes/SectorSubScheme.cs
+++ Zilon.Core/Zilon.Core/Schemes/SectorSubScheme.cs
@@ -1,4 +1,6 @@
-﻿namespace Zilon.Core.Schemes
+﻿using Newtonsoft.Json;
+
+namespace Zilon.Core.Schemes
 {
     /// <summary>
     /// Реализация схемы сектора.
@@ -8,16 +10,62 @@
         /// <summary>
         /// Идентфиикаторы обычных монстров, встречаемых в секторе.
         /// </summary>
-        public string[] RegularMonsterSids { get; set; }
+        [JsonProperty]
+        public string[] RegularMonsterSids { get; private set; }
 
         /// <summary>
         /// Идентификаторы редких монстров, встречаемых в секторе.
         /// </summary>
-        public string[] RareMonsterSids { get; set; }
+        [JsonProperty]
+        public string[] RareMonsterSids { get; private set; }
 
         /// <summary>
         /// Идентификаторы боссов, встречаемых в секторе.
         /// </summary>
-        public string[] ChampionMonsterSids { get; set; }
+        [JsonProperty]
+        public string[] ChampionMonsterSids { get; private set; }
+
+
+        /// <summary>
+        /// Тип фабрики для карты.
+        /// </summary>
+        [JsonProperty]
+        public SectorSubSchemeMapFactory MapFactory { get; private set; }
+
+        /// <summary>
+        /// Количество регионов в карте.
+        /// </summary>
+        /// <remarks>
+        /// Для подземелий это количество комнат.
+        /// </remarks>
+        [JsonProperty]
+        public int RegionCount { get; private set; }
+
+        /// <summary>
+        /// Максимальный размер комнат.
+        /// </summary>
+        /// <remarks>
+        /// Минимальный размер всегда 2х2.
+        /// </remarks>
+        [JsonProperty]
+        public int RegionSize { get; private set; }
+
+        /// <summary>
+        /// Количество монстров в секторе.
+        /// </summary>
+        [JsonProperty]
+        public int MonsterCount { get; private set; }
+
+        /// <summary>
+        /// Количество сундуков в секторе.
+        /// </summary>
+        [JsonProperty]
+        public int ChestCount { get; private set; }
+
+        /// <summary>
+        /// Таблицы дропа для сундуков.
+        /// </summary>
+        [JsonProperty]
+        public string[] ChestDropTableSids { get; private set; }
     }
 }
