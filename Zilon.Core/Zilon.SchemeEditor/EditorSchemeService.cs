aed4977646b2f7fae8481e63abfbe85188fea518
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-02-17 23:29:22 +0700

    #42 Наработки редактора для схем монстров

---
+++ Zilon.Core/Zilon.SchemeEditor/EditorSchemeService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Zilon.Core.Schemes;
+
+namespace Zilon.SchemeEditor
+{
+    /// <summary>
+    /// Класс для работы со схемами игрового мира.
+    /// </summary>
+    public sealed class EditorSchemeService : ISchemeService
+    {
+        private readonly Dictionary<Type, object> _handlerDict;
+        private readonly ISchemeServiceHandlerFactory _schemeServiceHandlerFactory;
+
+        public EditorSchemeService(ISchemeServiceHandlerFactory schemeServiceHandlerFactory)
+        {
+            _schemeServiceHandlerFactory = schemeServiceHandlerFactory;
+
+            _handlerDict = new Dictionary<Type, object>();
+
+            InitHandler<ILocationScheme, LocationScheme>();
+            InitHandler<IPropScheme, PropScheme>();
+            InitHandler<ITacticalActScheme, TacticalActScheme>();
+            InitHandler<IPersonScheme, PersonScheme>();
+            InitHandler<IDropTableScheme, DropTableScheme>();
+            InitHandler<IPerkScheme, PerkScheme>();
+            InitHandler<IMonsterScheme, MonsterScheme>();
+            InitHandler<IDropTableModificatorScheme, DropTableModificatorScheme>();
+        }
+
+        private void InitHandler<TScheme, TSchemeImpl>() where TScheme : class, IScheme
+            where TSchemeImpl : class, TScheme
+        {
+            var handler = _schemeServiceHandlerFactory.Create<TSchemeImpl>();
+            _handlerDict.Add(typeof(TScheme), handler);
+            handler.LoadSchemes();
+        }
+
+        public TScheme GetScheme<TScheme>(string sid) where TScheme : class, IScheme
+        {
+            if (sid == null)
+            {
+                throw new ArgumentNullException(nameof(sid));
+            }
+
+            var schemeType = typeof(TScheme);
+            if (!schemeType.IsInterface)
+            {
+                throw new ArgumentException("Тип схемы должен быть интерфейсом, унаследованным от IScheme");
+            }
+
+            var handler = GetHandler<TScheme>();
+            var scheme = handler.Get(sid);
+            return scheme;
+        }
+
+        private ISchemeServiceHandler<TScheme> GetHandler<TScheme>()
+            where TScheme : class, IScheme
+        {
+            if (!_handlerDict.TryGetValue(typeof(TScheme), out object handlerObj))
+            {
+                throw new ArgumentException("Указан неизвестный тип схемы.");
+            }
+
+            var handler = (ISchemeServiceHandler<TScheme>)handlerObj;
+            return handler;
+        }
+
+        public TScheme[] GetSchemes<TScheme>() where TScheme : class, IScheme
+        {
+            var schemeType = typeof(TScheme);
+            if (!schemeType.IsInterface)
+            {
+                throw new ArgumentException("Тип схемы должен быть интерфейсом, унаследованным от IScheme");
+            }
+
+            var handler = GetHandler<TScheme>();
+            var scheme = handler.GetAll();
+            return scheme;
+        }
+
+        public void UpdateScheme<TScheme>(TScheme scheme) where TScheme : class, IScheme
+        {
+            var handler = (EditorSchemeServiceHandler<TScheme>)GetHandler<TScheme>();
+            handler.Update(scheme);
+        }
+    }
+}
