2c663a977434be14deb44563237188ca281cff86
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-02-20 00:20:32 +0700

    #42 #285 Сброс очков при рестарте

--- Zilon.Client/Assets/Zilon/Scripts/Models/TitleScene/TitleHandler.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Models/TitleScene/TitleHandler.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 using Zenject;
 
 using Zilon.Core.Players;
+using Zilon.Core.Tactics;
 using Zilon.Core.World;
 
 public class TitleHandler : MonoBehaviour
 {
     [Inject] private readonly IWorldManager _globeManager;
+    [Inject] private readonly IScoreManager _scoreManager;
     [Inject] private readonly HumanPlayer _humanPlayer;
 
     public void Start()
@@ -18,6 +20,7 @@ public class TitleHandler : MonoBehaviour
         _humanPlayer.MainPerson = null;
         _humanPlayer.SectorSid = null;
         _humanPlayer.Terrain = null;
+        _scoreManager.ResetScores();
     }
 
     public void CloseButtonHandler()
