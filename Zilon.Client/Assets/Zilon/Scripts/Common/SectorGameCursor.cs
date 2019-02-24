688674e5c54e7b048f8734122fb30dc55e0f5f09
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-02-24 16:37:32 +0700

    #44 Для курсора выделен отдельный объект

---
+++ Zilon.Client/Assets/Zilon/Scripts/Common/SectorGameCursor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SectorGameCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.localPosition = cursorPosition + new  Vector3(0,0, 11);
+    }
+}
