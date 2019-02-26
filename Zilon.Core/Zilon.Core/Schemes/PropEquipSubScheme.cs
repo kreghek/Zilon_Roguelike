8581cfa7cb7cff2956a709c10253245ba0eab864
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-02-26 12:52:23 +0700

    #200 Корректная десериализация схемы предмета с правилами

--- Zilon.Core/Zilon.Core/Schemes/PropEquipSubScheme.cs
+++ Zilon.Core/Zilon.Core/Schemes/PropEquipSubScheme.cs
@@ -31,6 +31,6 @@ namespace Zilon.Core.Schemes
         /// Правила, которые будут срабатывать при экипировке предмета.
         /// </summary>
         [JsonProperty]
-        public PersonRule[] Rules { get; }
+        public PersonRule[] Rules { get; private set; }
     }
 }
