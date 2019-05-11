5c3426abd447f05d61a8af629deacccc752a3b2e
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-05-11 13:32:12 +0700

    Организация отдельного сервиса для запросов к консольному окружению

--- Zilon.Core/Zilon.BotPlayer/Program.cs
+++ Zilon.Core/Zilon.BotPlayer/Program.cs
@@ -1,24 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Zilon.Bot.Models;
 
 namespace Zilon.BotPlayer
 {
     class Program
     {
-        private static Process process;
-        private static TaskCompletionSource<string> tcs;
+        private static BotEnvironment _botEnvironment;
 
         static async Task Main(string[] args)
         {
             var pathToEnv = ConfigurationManager.AppSettings["env"];
-            process = new Process
+            var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
@@ -31,66 +25,20 @@ namespace Zilon.BotPlayer
                 }
             };
 
-            process.OutputDataReceived += Process_OutputDataReceived;
+            // Вызываем до старта процесса, т.к. BatEnvironment
+            // в конструкторе подписывается на событие получения вывода.
+            _botEnvironment = new BotEnvironment(process);
 
             process.Start();
             process.BeginOutputReadLine();
 
-            // first
-            tcs = new TaskCompletionSource<string>();
-            var startDataTask = tcs.Task;
-            var startData = await startDataTask;
-            var state = JsonConvert.DeserializeObject<State>(startData);
-            tcs = null;
-
-            Console.WriteLine($"Current HP: {state.CurrentHp}");
-            Console.WriteLine($"Current Position: {state.Position.X}, {state.Position.Y}");
-
             Console.ReadLine();
 
-            await SendRequestAsync("map");
+            var response = await _botEnvironment.RequestAsync("map");
+            Console.WriteLine(response);
 
             Console.ReadLine();
-
-            process.OutputDataReceived -= Process_OutputDataReceived;
             process.Kill();
-        }
-
-        private static async Task<string> SendRequestAsync(string reqquest)
-        {
-            if (tcs != null)
-            {
-                tcs.SetCanceled();
-            }
-
-            tcs = new TaskCompletionSource<string>();
-
-            process.StandardInput.WriteLine(reqquest);
-
-            var task = tcs.Task;
-            var data = await task;
-            tcs = null;
-            return data;
-
-        }
-
-        static string Out;
-
-        private static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(e.Data))
-            {
-                Console.WriteLine($"Data from env:{e.Data}");
-
-                if (e.Data == "Enter request:")
-                {
-                    tcs.SetResult(Out);
-                }
-                else
-                {
-                    Out = e.Data;
-                }
-            }
         }
     }
 }
