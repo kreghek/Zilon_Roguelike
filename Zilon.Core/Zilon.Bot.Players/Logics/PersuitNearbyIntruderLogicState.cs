557c490ef37c0283981f894a3969c8217414d1ad
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-05-12 13:49:47 +0700

    Перенос логики преследования в новую модель ботов

---
+++ Zilon.Core/Zilon.Bot.Players/Logics/PersuitNearbyIntruderLogicState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zilon.Core.Tactics.Behaviour;
+
+namespace Zilon.Bot.Players.Logics
+{
+    public class PersuitNearbyIntruderLogicState : ILogicState
+    {
+        public IActorTask GetCurrentTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
