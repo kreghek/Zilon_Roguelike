c5de5125ce38279d1cf172f2880eac01dff0c4d2
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-05-14 23:52:18 +0700

    Добавлены тесты для стратегии

---
+++ Zilon.Core/Zilon.Bot.Players.Tests/Strategies/LogicTreeStrategyTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using Zilon.Bot.Players.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Zilon.Core.Tactics;
+using FluentAssertions;
+using Zilon.Core.Tactics.Behaviour;
+
+namespace Zilon.Bot.Players.Strategies.Tests
+{
+    [TestFixture]
+    public class LogicTreeStrategyTests
+    {
+        /// <summary>
+        /// Тест проверяет, что стартовое состояние без переходов возвращает задачу.
+        /// </summary>
+        [Test]
+        public void GetActorTask_OneStateWithoutTransitions_ReturnsTask()
+        {
+            // ARRAGE
+            var logicTree = new LogicStateTree();
+            var logicStateMock = new Mock<ILogicState>();
+
+            var actorTaskMock = new Mock<IActorTask>();
+            var actorTask = actorTaskMock.Object;
+            logicStateMock.Setup(x => x.GetTask(It.IsAny<IActor>(), It.IsAny<ILogicStateData>()))
+                .Returns(actorTask);
+            var logicState = logicStateMock.Object;
+            logicTree.Transitions.Add(logicState, new LogicTransition[0]);
+            logicTree.StartState = logicState;
+
+            var actorMock = new Mock<IActor>();
+            var actor = actorMock.Object;
+
+            var strategy = new LogicTreeStrategy(actor, logicTree);
+
+
+
+            // ACT
+            var factTask = strategy.GetActorTask();
+
+
+
+            // ASSERT
+            factTask.Should().Be(actorTask);
+        }
+    }
+}
