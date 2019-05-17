f37d9dc3e17257cf60f7353a2e750d7a538dbe78
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-05-17 15:59:52 +0700

    Добавлена защита от зависания окружения

--- Zilon.Core/Zilon.BotMassLauncher/Program.cs
+++ Zilon.Core/Zilon.BotMassLauncher/Program.cs
@@ -92,7 +92,7 @@ namespace Zilon.BotMassLauncher
 
                 process.Start();
 
-                process.WaitForExit();
+                process.WaitForExit(10000);
             }
 
             Console.WriteLine($"[x] {infiniteCounterPreffix}ITERATION {iteration} FINISHED");
