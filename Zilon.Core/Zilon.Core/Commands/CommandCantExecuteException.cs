9af24c703b8a85d65b08afd2934da2ffb77685bd
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-05-27 12:41:28 +0700

    #331 Запрет на путешествия по глобальной карте, если критический уровень голода/жажды

---
+++ Zilon.Core/Zilon.Core/Commands/CommandCantExecuteException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Zilon.Core.Commands
+{
+    [Serializable]
+    internal class CommandCantExecuteException : Exception
+    {
+        public CommandCantExecuteException()
+        {
+        }
+
+        public CommandCantExecuteException(string message) : base(message)
+        {
+        }
+
+        public CommandCantExecuteException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CommandCantExecuteException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
