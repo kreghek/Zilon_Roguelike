cb5a2264e2e4894cbacfdf0b5ec0f5b187e721bd
Gregory <KulakovGV@tomskasu.ru>
2019-06-11 00:12:46 +0700

    #345 Добавил классификацию монстров.

---
+++ Zilon.Core/Zilon.Core/Common/MobTags.cs
@@ -0,0 +1,70 @@
+﻿namespace Zilon.Core.Common
+{
+    /// <summary>
+    /// Классификация монстров по тэгам.
+    /// </summary>
+    public static class MobTags
+    {
+        /// <summary>
+        /// Типы монстров.
+        /// </summary>
+        public static class Type
+        {
+            /// <summary>
+            /// Животные.
+            /// </summary>
+            public const string Animals = "animals";
+
+            /// <summary>
+            /// Боссы.
+            /// </summary>
+            public const string Boss = "boss";
+
+            /// <summary>
+            /// Демоны.
+            /// </summary>
+            public const string Demon = "demon";
+
+            /// <summary>
+            /// Летающие.
+            /// </summary>
+            public const string Fly = "fly";
+
+            /// <summary>
+            /// Сундук.
+            /// </summary>
+            public const string Luggage = "luggage";
+
+            /// <summary>
+            /// Нематериальные.
+            /// </summary>
+            public const string Wisp = "wisp";
+
+            /// <summary>
+            /// Крысы.
+            /// </summary>
+            public static class Rat
+            {
+                /// <summary>
+                /// Трусливые крысы.
+                /// </summary>
+                public const string Cowardly = "cowardly";
+
+                /// <summary>
+                /// Адские крысы.
+                /// </summary>
+                public const string Hell = "hell";
+
+                /// <summary>
+                /// Лунные крысы.
+                /// </summary>
+                public const string Moon = "moon";
+
+                /// <summary>
+                /// Крысы-мутанты.
+                /// </summary>
+                public const string Mutant = "mutant";
+            }
+        }
+    }
+}
