52a35a24f2d562ee7810996b124feb121d1dfa30
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-06-11 10:20:18 +0700

    #358 Оформление окна персонажа аналогично остальным окнам

--- Zilon.Client/Assets/Zilon/Scripts/Models/Modals/PerksModalBody.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Models/Modals/PerksModalBody.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+
 using Assets.Zilon.Scripts;
 
 using JetBrains.Annotations;
 
 using UnityEngine;
-using UnityEngine.UI;
 
 using Zenject;
 
@@ -17,10 +17,7 @@ public class PerksModalBody : MonoBehaviour, IModalWindowHandler
     private IActor _actor;
 
     public Transform PerkItemsParent;
-    public Transform EquipmentSlotsParent;
     public PerkItemViewModel PerkItemPrefab;
-    public InventorySlotVm EquipmentSlotPrefab;
-    public Text Stats;
 
     [NotNull] [Inject] private DiContainer _diContainer;
 
@@ -30,30 +27,7 @@ public class PerksModalBody : MonoBehaviour, IModalWindowHandler
 
     public void Start()
     {
-        CreateSlots();
-    }
 
-    private void CreateSlots()
-    {
-        // TODO Это расположение брать из схемы или из профилей персонажа. Или из ресурсов.
-        var positions = new[]
-        {
-            new Vector3(-50, 16), // Weapon
-			new Vector3(50, 16), // Outhand
-			new Vector3(0, 25), // Body
-			new Vector3(0, 72), // Head
-			new Vector3(0, -25), // Legs
-			new Vector3(-50, 64), // Aux
-			new Vector3(50, 64) // Aux
-		};
-
-        for (var i = 0; i < 7; i++)
-        {
-            var slotObject = _diContainer.InstantiatePrefab(EquipmentSlotPrefab, EquipmentSlotsParent);
-            slotObject.transform.localPosition = positions[i];
-            var slotVm = slotObject.GetComponent<InventorySlotVm>();
-            slotVm.SlotIndex = i;
-        }
     }
 
     public void Init(IActor actor)
@@ -61,19 +35,6 @@ public class PerksModalBody : MonoBehaviour, IModalWindowHandler
         _actor = actor;
         var evolutionData = _actor.Person.EvolutionData;
         UpdatePerksInner(PerkItemsParent, evolutionData.Perks);
-        UpdateStats();
-    }
-
-    private void UpdateStats()
-    {
-        Stats.text = string.Empty;
-
-        var stats = _actor.Person.EvolutionData.Stats;
-
-        foreach (var statItem in stats)
-        {
-            Stats.text += $"{statItem.Stat}: {statItem.Value} \n";
-        }
     }
 
     private void UpdatePerksInner(Transform itemsParent, IPerk[] perks)
