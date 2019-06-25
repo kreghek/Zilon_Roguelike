2f069c6a18d23a58e5c4ecc8899c4c6e49acb1ea
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-06-25 18:30:36 +0700

    #393 Выбор подходящего элемента из списка

--- Zilon.Core/Zilon.Core/Common/ListHelper.cs
+++ Zilon.Core/Zilon.Core/Common/ListHelper.cs
@@ -7,41 +7,58 @@ namespace Zilon.Core.Common
 {
     public static class ListHelper
     {
-        public static T RollRandom<T>(IList<T> list, IDice dice, Predicate<T> predicate) where T: class
+        public static T RollRandom<T>(List<T> list, IDice dice, Predicate<T> predicate) where T: class
         {
             var count = list.Count;
             var currentIndex = dice.Roll(0, count - 1);
-            var startIndex = currentIndex;
-            T foundElement = null;
-            while (foundElement == null)
+
+            var foundIndex = list.FindIndex(currentIndex, predicate);
+            if (foundIndex > -1)
             {
-                var currentElement = list[currentIndex];
-
-                currentIndex++;
-
-                // Зацикливаем индекс
-                if (currentIndex >= count)
-                {
-                    currentIndex = 0;
-                }
-
-                // Проверяем обход всего набора
-                if (startIndex == currentIndex)
-                {
-                    // Достигли точки, с которой начали обход.
-                    // Значит не нашли подходящего агента.
-
-                    break;
-                }
-
-                if (predicate(currentElement))
-                {
-                    foundElement = currentElement;
-                    break;
-                }
+                return list[foundIndex];
             }
 
-            return foundElement;
+            if (foundIndex >= 0)
+            {
+                foundIndex = list.FindIndex(0, currentIndex, predicate);
+                return list[foundIndex];
+            }
+            else
+            {
+                return null;
+            }
+
+            //var startIndex = currentIndex;
+            //T foundElement = null;
+            //while (foundElement == null)
+            //{
+            //    var currentElement = list[currentIndex];
+
+            //    currentIndex++;
+
+            //    // Зацикливаем индекс
+            //    if (currentIndex >= count)
+            //    {
+            //        currentIndex = 0;
+            //    }
+
+            //    // Проверяем обход всего набора
+            //    if (startIndex == currentIndex)
+            //    {
+            //        // Достигли точки, с которой начали обход.
+            //        // Значит не нашли подходящего агента.
+
+            //        break;
+            //    }
+
+            //    if (predicate(currentElement))
+            //    {
+            //        foundElement = currentElement;
+            //        break;
+            //    }
+            //}
+
+            //return foundElement;
         }
     }
 }
