f1eb46d1a08a586699cace79f6bec1405b63c48b
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-07-05 12:27:17 +0700

    #416 Генерация квестов

---
+++ Zilon.Core/Zilon.Core/PersonDialogGeneration/DialogGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Zilon.Core.CommonServices.Dices;
+
+namespace Zilon.Core.PersonDialogs
+{
+    public sealed class DialogGenerator
+    {
+        private readonly IDice _dice;
+
+        public DialogGenerator(IDice dice)
+        {
+            _dice = dice;
+        }
+
+        public Dialog Generate()
+        {
+
+        }
+    }
+}
