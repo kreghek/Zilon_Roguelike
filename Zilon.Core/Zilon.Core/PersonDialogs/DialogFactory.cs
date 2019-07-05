881857b9c84706abc81d458a51ffba55e5069b5e
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-07-05 13:50:31 +0700

    #416 Более информативные тексты

--- Zilon.Core/Zilon.Core/PersonDialogs/DialogFactory.cs
+++ Zilon.Core/Zilon.Core/PersonDialogs/DialogFactory.cs
@@ -6,13 +6,13 @@
         {
             var dialog = new Dialog();
 
-            var root = new DialogNode { Text = "root" };
-            var node1 = new DialogNode { Text = "node1" };
-            var node2 = new DialogNode { Text = "node2" };
+            var root = new DialogNode { Text = "Hi!" };
+            var node1 = new DialogNode { Text = "The man says you what dialogs and quests comming soon." };
+            var node2 = new DialogNode { Text = "You decided to wait. Press Close button on the top right corner." };
 
-            var rootNode1Trans = new DialogTransition { Text = "To node1", StartNode = root, TargetNode = node1 };
-            var rootNode2Trans = new DialogTransition { Text = "To node2", StartNode = root, TargetNode = node2 };
-            var node1ToRootTrans = new DialogTransition { Text = "To node2", StartNode = node1, TargetNode = root };
+            var rootNode1Trans = new DialogTransition { Text = "Ask for dialogs.", StartNode = root, TargetNode = node1 };
+            var rootNode2Trans = new DialogTransition { Text = "Wait.", StartNode = root, TargetNode = node2 };
+            var node1ToRootTrans = new DialogTransition { Text = "Ok", StartNode = node1, TargetNode = root };
 
             dialog.Nodes = new[] { root, node1, node2 };
             dialog.Transitions = new[] { rootNode1Trans, rootNode2Trans, node1ToRootTrans };
