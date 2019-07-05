b2d49b183f44594bc729233041b35d5d0d54ad31
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-07-05 17:31:58 +0700

    #202 Новые предметы добавлены в дроп

--- Helpers/GenerateSwords.ps1
+++ Helpers/GenerateSwords.ps1
@@ -1,17 +1,13 @@
 # Поместить в Resources
 # Скрипт выбирает все новые иконки. И создаёт файлы схем на основе существующей с именованиями новых иконок.
 
-$path='.\Icons\props'
-$schemePath='.\Schemes\Props\Equipments'
-$time = Get-Date -Date "2019-05-07 00:00:00Z"
+$schemePath='..\Zilon.Client\Assets\Resources\Schemes\Props\Equipments'
+$newPropFilePath='.\new-props.txt'
+$protoScheme='short-sword'
+$outDropFile='drop.txt'
 
-$newIcons=get-childitem "$path" -recurse | Where-Object {$_.LastWriteTime -gt $time}
-
-foreach($file in $newIcons)
-{
-	$fileName=[System.IO.Path]::GetFileNameWithoutExtension($file)
-	copy-item -Path "$schemePath\short-sword.json" -Destination "$schemePath\$fileName.json"
-	
-	(Get-Content "$schemePath\$fileName.json" -Encoding UTF8).replace('Короткий меч', "$fileName").replace('Short sword', "$fileName") | Set-Content "$schemePath\$fileName.json"
-
+foreach($propSid in Get-Content $newPropFilePath) {
+    copy-item -Path "$schemePath\$protoScheme.json" -Destination "$schemePath\$propSid.json"
+	(Get-Content "$schemePath\$propSid.json" -Encoding UTF8).replace('Короткий меч', "$propSid").replace('Short sword', "$propSid") | Set-Content "$schemePath\$propSid.json"
+	Add-Content -Path ".\$outDropFile" -Value "{ `"SchemeSid`": `"$propSid`", `"Weight`": 10},"
 }
