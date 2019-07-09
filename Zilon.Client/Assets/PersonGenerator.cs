c6b5059ba00af4e793c65532ad5852811d0c4d02
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-07-09 08:32:37 +0700

    #202 Создание урезанной вью-модели актёра для демо

--- Zilon.Client/Assets/PersonGenerator.cs
+++ Zilon.Client/Assets/PersonGenerator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 using Zenject;
 
 using Zilon.Core.Persons;
+using Zilon.Core.Players;
+using Zilon.Core.Tactics;
+using Zilon.Core.Tactics.Spatial;
 
 public class PersonGenerator : MonoBehaviour
 {
@@ -23,12 +26,25 @@ public class PersonGenerator : MonoBehaviour
     private float _spawnCounter;
     private int _spawnIndex;
     private GameObject[] _persons;
+    private readonly HumanPlayer _humanPlayer;
+    private readonly HexNode _node;
 
-    [Inject] IHumanPersonFactory _humanPersonFactory;
+    [Inject]
+    private readonly IHumanPersonFactory _humanPersonFactory;
+
+    [Inject]
+    private readonly IPerkResolver _perkResolver;
+
+    [NotNull] [Inject]
+    private readonly DiContainer _container;
 
     public PersonGenerator()
     {
         _persons = new GameObject[SPAWN_MATRIX_WIDTH * SPAWN_MATRIX_HEIGHT];
+
+        _humanPlayer = new HumanPlayer();
+
+        _node = new HexNode(0, 0);
     }
 
     public void FixedUpdate()
@@ -37,13 +53,35 @@ public class PersonGenerator : MonoBehaviour
         if (_spawnCounter > 1)
         {
             _spawnCounter = 0;
-            CreateNewPerson();
+            SpawnNewPerson();
         }
     }
 
-    private void CreateNewPerson()
+    private void SpawnNewPerson()
     {
         var person = _humanPersonFactory.Create();
 
+        var actorViewModel = CreateHumanActorViewModel(person, _humanPlayer, _node);
+
+    }
+
+    private ActorViewModel CreateHumanActorViewModel(
+        [NotNull] IPerson person,
+        [NotNull] IPlayer player,
+        [NotNull] IMapNode startNode)
+    {
+        var actor = new Actor(person, player, startNode, _perkResolver);
+
+        var actorViewModelObj = _container.InstantiatePrefab(ActorPrefab, transform);
+        var actorViewModel = actorViewModelObj.GetComponent<ActorViewModel>();
+        var actorGraphic = Instantiate(HumanoidGraphicPrefab, actorViewModel.transform);
+        actorGraphic.transform.position = new Vector3(0, 0.2f, 0);
+        actorViewModel.GraphicRoot = actorGraphic;
+
+        var graphicController = actorViewModel.gameObject.AddComponent<HumanActorGraphicController>();
+        graphicController.Actor = actor;
+        graphicController.Graphic = actorGraphic;
+
+        return actorViewModel;
     }
 }
