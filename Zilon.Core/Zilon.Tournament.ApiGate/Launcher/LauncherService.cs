92b170688604eaabc5be4b3036e5470dd01be082
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-07-09 17:10:55 +0700

    Сервис для фонового запуска ботов

---
+++ Zilon.Core/Zilon.Tournament.ApiGate/Launcher/LauncherService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Zilon.Tournament.ApiGate.Launcher
+{
+    public class LauncherService: BackgroundService
+    {
+        private readonly ILogger<LauncherService> _logger;
+        
+        public LauncherService(ILogger<LauncherService> logger)
+        {
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogDebug("Launcher Service is starting");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                
+            }
+            _logger.LogDebug("Launcher service is stopping");
+        }
+    }
+}
