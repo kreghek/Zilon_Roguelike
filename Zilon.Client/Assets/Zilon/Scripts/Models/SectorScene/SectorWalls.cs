21e05153f9b784b42d6c1d33b87f7a427c363a38
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-07-18 13:01:13 +0700

    #272 Объединение настроек по визуализации стен сектора в отдельные ресурсы

---
+++ Zilon.Client/Assets/Zilon/Scripts/Models/SectorScene/SectorWalls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SectorWalls : MonoBehaviour
+{
+    public Sprite LeftTopWall;
+    public Sprite LeftWall;
+    public Sprite LeftBottomWall;
+
+    public Sprite RightTopWall;
+    public Sprite RightWall;
+    public Sprite RightBottomWall;
+
+    public Sprite Floor;
+
+    public Sprite[] InteriorObjectSprites;
+    public Sprite[] FloorDecorSprites;
+
+    public Sprite[] Walls
+    {
+        get => new[] { LeftWall, LeftTopWall, RightTopWall, RightWall, RightBottomWall, LeftBottomWall };
+    }
+}
