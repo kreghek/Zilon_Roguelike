bc0091b893a737cf447500c5218b7819d6071b87
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-08-12 09:27:52 +0700

    #438 WIP Консольное приложение для массовой генерации миров

---
+++ Zilon.Core/Zilon.Core.MassWorldGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Zilon.Core.MassWorldGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _globalServiceContainer = new ServiceContainer();
+            _startUp = new Startup();
+            _startUp.RegisterServices(_globalServiceContainer);
+        }
+    }
+}
---
+++ Zilon.Core/Zilon.Core.MassWorldGenerator/Startup.cs
@@ -0,0 +1,19 @@
+﻿using LightInject;
+
+using Zilon.Emulation.Common;
+
+namespace Zilon.Core.MassWorldGenerator
+{
+    class Startup : InitialzationBase
+    {
+        public override void ConfigureAux(IServiceFactory serviceFactory)
+        {
+
+        }
+
+        protected override void RegisterBot(IServiceRegistry container)
+        {
+
+        }
+    }
+}
