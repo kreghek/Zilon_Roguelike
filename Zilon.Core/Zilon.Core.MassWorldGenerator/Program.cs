634c42142cf6c3dee9535ff002156bde726dfc10
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-08-12 09:41:03 +0700

    #438 Массовая генерация миров

--- Zilon.Core/Zilon.Core.MassWorldGenerator/Program.cs
+++ Zilon.Core/Zilon.Core.MassWorldGenerator/Program.cs
@@ -1,12 +1,51 @@
-﻿namespace Zilon.Core.MassWorldGenerator
+﻿using System;
+using System.Linq;
+using LightInject;
+using Zilon.Core.WorldGeneration;
+
+namespace Zilon.Core.MassWorldGenerator
 {
     class Program
     {
-        static void Main(string[] args)
+        private static ServiceContainer _globalServiceContainer;
+        private static Startup _startUp;
+
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
+            var schemeCatalog = GetProgramArgument(args, "schemeCatalog");
+
             _globalServiceContainer = new ServiceContainer();
-            _startUp = new Startup();
+            _startUp = new Startup(schemeCatalog);
             _startUp.RegisterServices(_globalServiceContainer);
+
+            while (true)
+            {
+                var worldGenerator = _globalServiceContainer.GetInstance<IWorldGenerator>();
+
+                await worldGenerator.GenerateGlobeAsync();
+            }
+        }
+
+        private static bool HasProgramArgument(string[] args, string testArg)
+        {
+            return args?.Select(x => x?.Trim().ToLowerInvariant()).Contains(testArg.ToLowerInvariant()) == true;
+        }
+
+        private static string GetProgramArgument(string[] args, string testArg)
+        {
+            foreach (var arg in args)
+            {
+                var components = arg.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+                if (string.Equals(components[0], testArg, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    if (components.Length >= 2)
+                    {
+                        return components[1];
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
--- Zilon.Core/Zilon.Core.MassWorldGenerator/Startup.cs
+++ Zilon.Core/Zilon.Core.MassWorldGenerator/Startup.cs
@@ -1,19 +1,54 @@
 ﻿using LightInject;
 
-using Zilon.Emulation.Common;
+using Zilon.Core.CommonServices.Dices;
+using Zilon.Core.Schemes;
+using Zilon.Core.WorldGeneration;
 
 namespace Zilon.Core.MassWorldGenerator
 {
-    class Startup : InitialzationBase
+    public class Startup
     {
-        public override void ConfigureAux(IServiceFactory serviceFactory)
-        {
+        private readonly string _schemeCatalog;
 
+        public Startup(string schemeCatalog)
+        {
+            _schemeCatalog = schemeCatalog;
         }
 
-        protected override void RegisterBot(IServiceRegistry container)
+        public void RegisterServices(IServiceRegistry serviceRegistry)
         {
+            RegisterSchemeService(serviceRegistry);
+            RegisterAuxServices(serviceRegistry);
+            RegisterGlobeServices(serviceRegistry);
+        }
 
+        private void RegisterGlobeServices(IServiceRegistry serviceRegistry)
+        {
+            serviceRegistry.Register<IWorldGenerator, WorldGenerator>();
+        }
+
+        private void RegisterSchemeService(IServiceRegistry container)
+        {
+            container.Register<ISchemeLocator>(factory =>
+            {
+                var schemeLocator = new FileSchemeLocator(_schemeCatalog);
+
+                return schemeLocator;
+            }, new PerContainerLifetime());
+
+            container.Register<ISchemeService, SchemeService>(new PerContainerLifetime());
+
+            container.Register<ISchemeServiceHandlerFactory, SchemeServiceHandlerFactory>(new PerContainerLifetime());
+        }
+
+        /// <summary>
+        /// Подготовка дополнительных сервисов
+        /// </summary>
+        private void RegisterAuxServices(IServiceRegistry container)
+        {
+            var dice = new Dice();
+            container.Register<IDice>(factory => dice, new PerContainerLifetime());
+            
         }
     }
 }
