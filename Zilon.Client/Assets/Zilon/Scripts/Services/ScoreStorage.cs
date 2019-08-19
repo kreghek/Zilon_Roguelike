7bc34f2b7f543fb7868f824f6eeea6175d12053f
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-08-19 17:47:52 +0700

    #359 Подключение к БД

---
+++ Zilon.Client/Assets/Zilon/Scripts/Services/ScoreStorage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Data;
+using System.IO;
+using System.Data.SQLite;
+
+public class ScoreStorage : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(Application.persistentDataPath);
+        var connectionString = "URI=file:" + Application.persistentDataPath + "/mydb.db";
+        var connection = new SQLiteConnection(connectionString);
+        connection.Open();
+        var command = connection.CreateCommand();
+        command.CommandType = System.Data.CommandType.Text;
+        command.CommandText = "CREATE TABLE IF NOT EXISTS 'highscores' ( " +
+                          "  'id' INTEGER PRIMARY KEY, " +
+                          "  'name' TEXT NOT NULL, " +
+                          "  'score' INTEGER NOT NULL" +
+                          ");";
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
