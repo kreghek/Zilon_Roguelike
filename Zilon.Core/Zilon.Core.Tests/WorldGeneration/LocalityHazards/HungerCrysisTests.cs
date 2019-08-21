856d1e9b32a02bea5dac3fe51a7cb8be4a3e4aa8
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-08-21 22:41:59 +0700

    #478 Добавлен кризис - голод

--- Zilon.Core/Zilon.Core.Tests/WorldGeneration/LocalityHazards/HungerCrysisTests.cs
+++ Zilon.Core/Zilon.Core.Tests/WorldGeneration/LocalityHazards/HungerCrysisTests.cs
@@ -1,12 +1,10 @@
-﻿using NUnit.Framework;
-using Zilon.Core.WorldGeneration.LocalityHazards;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Zilon.Core.WorldGeneration.LocalityStructures;
+﻿using System.Collections.Generic;
+
+using FluentAssertions;
+
 using Moq;
+
+using NUnit.Framework;
 
 namespace Zilon.Core.WorldGeneration.LocalityHazards.Tests
 {
@@ -20,28 +18,6 @@ namespace Zilon.Core.WorldGeneration.LocalityHazards.Tests
 
             var locality = new Locality();
 
-            var settlerCamp = new BasicLocalityStructure(name: "Settler Camp",
-                requiredPopulation: new Dictionary<PopulationSpecializations, int> {
-                    { PopulationSpecializations.Workers, 1 },
-                    { PopulationSpecializations.Peasants, 1 },
-                    { PopulationSpecializations.Servants, 1 }
-                },
-                requiredResources: new Dictionary<LocalityResource, int> {
-                    { LocalityResource.Energy, 1 }
-                },
-                productResources: new Dictionary<LocalityResource, int> {
-                    { LocalityResource.Energy, 1 },
-                    { LocalityResource.Food, 3 },
-                    { LocalityResource.Goods, 3 },
-                    { LocalityResource.LivingPlaces, 3 },
-                    { LocalityResource.Manufacture, 1 },
-                });
-
-            var region = new LocalityRegion();
-            region.Structures.Add(settlerCamp);
-
-            locality.Regions.Add(region);
-
             locality.CurrentPopulation.AddRange(new Population[] {
                 new Population{Specialization = PopulationSpecializations.Servants },
                 new Population{Specialization = PopulationSpecializations.Peasants },
@@ -49,19 +25,25 @@ namespace Zilon.Core.WorldGeneration.LocalityHazards.Tests
                 new Population{Specialization = PopulationSpecializations.Servants },
             });
 
-            locality.Stats.Resources[LocalityResource.Energy] = 1;
-            locality.Stats.Resources[LocalityResource.Food] = 3;
-            locality.Stats.Resources[LocalityResource.Goods] = 3;
-            locality.Stats.Resources[LocalityResource.LivingPlaces] = 3;
+            locality.Stats.Resources[LocalityResource.Food] = -1;
 
 
             var crysisRandomSourceMock = new Mock<ICrysisRandomSource>();
-            var 
+            crysisRandomSourceMock.Setup(x => x.RollDeathPass()).Returns(1);
+            crysisRandomSourceMock.Setup(x => x.RollDeadPopulationIndex(It.IsAny<IEnumerable<Population>>())).Returns(0);
             var crysisRandomSource = crysisRandomSourceMock.Object;
 
-            var hunder = new HungerCrysis()
-        }
+            var hunger = new HungerCrysis(crysisRandomSource);
 
-        
+
+
+            // ACT
+            hunger.Update(locality);
+
+
+
+            // ASSERT
+            locality.CurrentPopulation.Should().HaveCount(3);
+        }
     }
 }
