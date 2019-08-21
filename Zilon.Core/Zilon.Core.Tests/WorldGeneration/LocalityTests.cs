a1d18c042f6d8ba3bd0a8ff90b95e789a4823b99
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-08-21 20:17:39 +0700

    #478 Тест, выполняющий два обновления города

--- Zilon.Core/Zilon.Core.Tests/WorldGeneration/LocalityTests.cs
+++ Zilon.Core/Zilon.Core.Tests/WorldGeneration/LocalityTests.cs
@@ -10,11 +10,15 @@ using FluentAssertions;
 
 namespace Zilon.Core.WorldGeneration.Tests
 {
-    [TestFixture()]
+    [TestFixture]
     public class LocalityTests
     {
-        [Test()]
-        public void UpdateTest()
+        /// <summary>
+        /// Тест проверяет, что если в городе есть структура, которая обеспечивает себя и генерирует производство,
+        /// то после одного обновления в городе будет единица производства, а остальные ресурсы не изменятся.
+        /// </summary>
+        [Test]
+        public void Update_ProduceOnlyManufactureInOneIteration_ManufactureIncreaseAndOtherResourcesNotChanges()
         {
             var locality = new Locality();
 
@@ -65,6 +69,66 @@ namespace Zilon.Core.WorldGeneration.Tests
             locality.Stats.Resources[LocalityResource.LivingPlaces].Should().Be(3);
 
             locality.Stats.Resources[LocalityResource.Manufacture].Should().Be(1);
+        }
+
+        /// <summary>
+        /// Тест проверяет, что если в городе есть структура, которая обеспечивает себя и генерирует производство,
+        /// то после одного обновления в городе будет единица производства, а остальные ресурсы не изменятся.
+        /// Все в течении двух итераций.
+        /// </summary>
+        [Test]
+        public void Update_ProduceOnlyManufactureIn2Iteration_ManufactureIncreaseAndOtherResourcesNotChanges()
+        {
+            var locality = new Locality();
+
+            var settlerCamp = new BasicLocalityStructure(name: "Settler Camp",
+                requiredPopulation: new Dictionary<PopulationSpecializations, int> {
+                    { PopulationSpecializations.Workers, 1 },
+                    { PopulationSpecializations.Peasants, 1 },
+                    { PopulationSpecializations.Servants, 1 }
+                },
+                requiredResources: new Dictionary<LocalityResource, int> {
+                    { LocalityResource.Energy, 1 }
+                },
+                productResources: new Dictionary<LocalityResource, int> {
+                    { LocalityResource.Energy, 1 },
+                    { LocalityResource.Food, 3 },
+                    { LocalityResource.Goods, 3 },
+                    { LocalityResource.LivingPlaces, 3 },
+                    { LocalityResource.Manufacture, 1 },
+                });
+
+            var region = new LocalityRegion();
+            region.Structures.Add(settlerCamp);
+
+            locality.Regions.Add(region);
+
+            locality.CurrentPopulation.AddRange(new Population[] {
+                new Population{Specialization = PopulationSpecializations.Peasants },
+                new Population{Specialization = PopulationSpecializations.Workers },
+                new Population{Specialization = PopulationSpecializations.Servants },
+            });
+
+            locality.Stats.Resources[LocalityResource.Energy] = 1;
+            locality.Stats.Resources[LocalityResource.Food] = 3;
+            locality.Stats.Resources[LocalityResource.Goods] = 3;
+            locality.Stats.Resources[LocalityResource.LivingPlaces] = 3;
+
+
+
+            // ACT
+            locality.Update();
+            locality.Update();
+
+
+
+            // ASSERT
+            locality.Stats.Resources[LocalityResource.Energy].Should().Be(1);
+            locality.Stats.Resources[LocalityResource.Food].Should().Be(3);
+            locality.Stats.Resources[LocalityResource.Goods].Should().Be(3);
+            locality.Stats.Resources[LocalityResource.LivingPlaces].Should().Be(3);
+
+            locality.Stats.Resources[LocalityResource.Manufacture].Should().Be(2);
         }
     }
 }
