1f2f350065492835d5922db6021715747c98ba3d
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-08-23 00:56:46 +0700

    #479 Характеристики выживания теперь в схеме персонажа

---
+++ Zilon.Core/Zilon.Core/Schemes/PersonSurvivalStatSubScheme.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Zilon.Core.Schemes
+{
+    /// <summary>
+    /// Базовая реализация подсхемы характеристики выживания персонажа.
+    /// </summary>
+    public sealed class PersonSurvivalStatSubScheme : SubSchemeBase, IPersonSurvivalStatSubScheme
+    {
+        [JsonProperty]
+        public PersonSurvivalStatType Type { get; private set; }
+
+        [JsonProperty]
+        [JsonConverter(typeof(ConcreteTypeConverter<PersonSurvivalStatKeyPointSubScheme[]>))]
+        public IPersonSurvivalStatKeyPointSubScheme[] KeyPoints { get; private set; }
+
+        [JsonProperty]
+        public int StartValue { get; private set; }
+
+        [JsonProperty]
+        public int MinValue { get; private set; }
+
+        [JsonProperty]
+        public int MaxValue { get; private set; }
+    }
+}
