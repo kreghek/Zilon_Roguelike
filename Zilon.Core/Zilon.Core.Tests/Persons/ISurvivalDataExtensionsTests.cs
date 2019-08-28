5919f1afc337a2c7b03992c2ec48a3937c33c405
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-08-28 17:55:55 +0700

    #486 WIP Рефакторинг тестов

--- Zilon.Core/Zilon.Core.Tests/Persons/ISurvivalDataExtensionsTests.cs
+++ Zilon.Core/Zilon.Core.Tests/Persons/ISurvivalDataExtensionsTests.cs
@@ -1,18 +1,55 @@
-﻿using NUnit.Framework;
-using Zilon.Core.Persons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+
 using FluentAssertions;
+
+using NUnit.Framework;
 
 namespace Zilon.Core.Persons.Tests
 {
-    [TestFixture()]
+    [TestFixture]
     public class ISurvivalDataExtensionsTests
     {
-        [Test()]
+        /// <summary>
+        /// Тест проверяет, что при изменении значения в указанном диапазоне
+        /// мы проходим через все предполагаемые ключевые точки.
+        /// </summary>
+        [Test]
+        public void CalcKeyPointsInRange_TestCases(int lesserKeyPoint, int strongKeyPoint, int maxKeyPoint,
+            int minValue, int maxValue,
+            SurvivalStatHazardLevel passedKeyPoint0,
+            SurvivalStatHazardLevel passedKeyPoint1,
+            SurvivalStatHazardLevel passedKeyPoint2)
+        {
+            // ARRANGE
+
+            var keyPoints = new[] {
+                new SurvivalStatKeyPoint(SurvivalStatHazardLevel.Lesser, lesserKeyPoint),
+                new SurvivalStatKeyPoint(SurvivalStatHazardLevel.Strong, strongKeyPoint),
+                new SurvivalStatKeyPoint(SurvivalStatHazardLevel.Max, maxKeyPoint)
+            };
+
+
+
+            // ACT
+
+            var crossedKeyPoints = keyPoints.CalcKeyPointsInRange(minValue, maxValue);
+
+
+
+            // ASSERT
+            var expectedKeyPointsWithNulls = new[] { passedKeyPoint0, passedKeyPoint1, passedKeyPoint2 };
+            var expectedKeyPoints = expectedKeyPointsWithNulls.Where(x => x != SurvivalStatHazardLevel.Undefined).ToArray();
+
+            var factKeyPoints = crossedKeyPoints.Select(x => x.Level).ToArray();
+
+            factKeyPoints.Should().AllBeEquivalentTo(expectedKeyPoints);
+        }
+
+        /// <summary>
+        /// Тест проверяет, что при изменении значения в указанном диапазоне
+        /// мы проходим через все предполагаемые ключевые точки.
+        /// </summary>
+        [Test]
         public void CalcKeyPointsInRange_2PositiveKeyPointsRise_RaisedStrongKeyPoint()
         {
             // ARRANGE
@@ -36,7 +73,11 @@ namespace Zilon.Core.Persons.Tests
             materializedFactCrossedKeyPoints[1].Level.Should().Be(SurvivalStatHazardLevel.Strong);
         }
 
-        [Test()]
+        /// <summary>
+        /// Тест проверяет, что при изменении значения в указанном диапазоне
+        /// мы проходим через все предполагаемые ключевые точки.
+        /// </summary>
+        [Test]
         public void CalcKeyPointsInRange_2PositiveKeyPointsRise_RaisedStrongKeyPoint2()
         {
             // ARRANGE
@@ -59,7 +100,11 @@ namespace Zilon.Core.Persons.Tests
             materializedFactCrossedKeyPoints[0].Level.Should().Be(SurvivalStatHazardLevel.Max);
         }
 
-        [Test()]
+        /// <summary>
+        /// Тест проверяет, что при изменении значения в указанном диапазоне
+        /// мы проходим через все предполагаемые ключевые точки.
+        /// </summary>
+        [Test]
         public void CalcKeyPointsInRange_2PositiveKeyPointsRise_RaisedStrongKeyPoint3()
         {
             // ARRANGE
@@ -84,7 +129,11 @@ namespace Zilon.Core.Persons.Tests
             materializedFactCrossedKeyPoints[2].Level.Should().Be(SurvivalStatHazardLevel.Max);
         }
 
-        [Test()]
+        /// <summary>
+        /// Тест проверяет, что при изменении значения в указанном диапазоне
+        /// мы проходим через все предполагаемые ключевые точки.
+        /// </summary>
+        [Test]
         public void CalcKeyPointsInRange_2NegativeKeyPointsRise_RaisedStrongKeyPoint()
         {
             // ARRANGE
