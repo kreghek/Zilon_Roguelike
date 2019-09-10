36c7e9a166e372b5945d85b757b89c0b2207611e
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-09-10 22:54:03 +0700

    Развитие модели населения

---
+++ Zilon.Core/Zilon.Core/WorldGeneration/IGlobeEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace Zilon.Core.WorldGeneration
+{
+    /// <summary>
+    /// Интерфейс сущности в мире.
+    /// Суда относятся:
+    /// Армии,
+    /// Миграционные группы,
+    /// Бандиты,
+    /// Экспедиции,
+    /// Стаи животных,
+    /// Великие монстры.
+    /// </summary>
+    public interface IGlobeEntity
+    {
+        /// <summary>
+        /// Текущее местоположение сущности.
+        /// </summary>
+        TerrainCell Location { get; set; }
+
+        /// <summary>
+        /// Лидер группы.
+        /// </summary>
+        Agent Leader { get; set; }
+    }
+}
--- Zilon.Core/Zilon.Core/WorldGeneration/PopulationUnit.cs
+++ Zilon.Core/Zilon.Core/WorldGeneration/PopulationUnit.cs
@@ -2,10 +2,21 @@
 {
     /// <summary>
     /// Единица населения города или группы мигрантов.
-    /// Условно одна единица популяции - это 100 человек.
+    /// Условно одна единица популяции - это 10 человек.
     /// </summary>
     public sealed class PopulationUnit
     {
+        private const float MIN_AGE_POWER = 0.3f;
+        private const float POWER_COEF = 1f;
+        private const float MAX_AGE_POWER = 1.3f;
+        private const float OLDMAN_AGE_POWER = 0.5f;
+        private const int PROF_AGE = 40;
+        private const int NOOBIE_AGE = 15;
+        private const int OLDMAN_AGE = 60;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
         public PopulationUnit()
         {
             // Значение сорости роста рассчитано так:
@@ -26,6 +37,11 @@
         public PopulationSpecializations Specialization { get; set; }
 
         /// <summary>
+        /// Раса единицы населения.
+        /// </summary>
+        public PopulationRace Race { get; set; }
+
+        /// <summary>
         /// Текущий счётчик роста.
         /// Это прогресс. Когда достигает 1, тогда единица населения готова для роста.
         /// </summary>
@@ -35,6 +51,59 @@
         /// Скорость роста населения для данной единицы.
         /// </summary>
         public float PopulationGrowthRate { get; private set; }
+
+        /// <summary>
+        /// Здоровье единицы населения.
+        /// Показывает, насколько жизнеспособна единица населения.
+        /// 1f - нормальное здоровье.
+        /// Голод, мор, плохая экология, штурм города и т.д. снижает здоровье. Здоровье не восстанавливается со временем.
+        /// </summary>
+        public float Health { get; set; }
+
+        /// <summary>
+        /// Эффективность единицы населения. 
+        /// </summary>
+        /// <remarks>
+        /// Чем выше эффективность, тем больше
+        /// продукции производит здание, на которое назначено население.
+        /// Если единица населения назначена более чем на одну структуру, то
+        /// эффективность делится на несколько зданий.
+        /// Базовая эффективность равна 1f. Когда единица населения только сформирована,
+        /// эффективность низкая. Предполагается, то единица населения ещё не квалифицирована.
+        /// Со временем эффективность вырастает до пиковой, выше базовой. Предполагается, то эта часть населения
+        /// обладает наибольшим опытом и достигает высот профессионализма.
+        /// Далее, с возрастом, эффективность снижается. Наступает старость.
+        /// 
+        /// Так же эффективность зависит от здоровья. Чем ниже здоровье, тем ниже эффетивность.
+        /// </remarks>
+        public float Power => GetAgePower(Age) * Health * POWER_COEF;
+
+        private float GetAgePower(float age)
+        {
+            if (age < 40)
+            {
+                return Lerp(MIN_AGE_POWER, MAX_AGE_POWER, age / (PROF_AGE - NOOBIE_AGE));
+            }
+            else if (age < 60)
+            {
+                return Lerp(MAX_AGE_POWER, OLDMAN_AGE_POWER, age / (OLDMAN_AGE - PROF_AGE));
+            }
+            else
+            {
+                return OLDMAN_AGE_POWER;
+            }
+        }
+
+        /// <summary>
+        /// Средний возраст единицы населения.
+        /// </summary>
+        /// <remarks>
+        /// Возраст влияет на:
+        /// - Эффективность работы населения. Минимальный возрасть 15. Пиковый возраст 40. Максимальный возраст 60.
+        /// - Возможность репродукции. Репродукция начинается с 20.
+        /// - Проверка на урон от старости. Урон от старости начинается после 60.
+        /// </remarks>
+        public float Age { get; set; }
 
         /// <summary>
         /// Обновление роста населения.
@@ -50,6 +119,11 @@
         public void DropGrowthCounter()
         {
             PopulationGrowthCounter = 1 - PopulationGrowthCounter;
+        }
+
+        private float Lerp(float firstFloat, float secondFloat, float by)
+        {
+            return firstFloat * (1 - by) + secondFloat * by;
         }
     }
 }
