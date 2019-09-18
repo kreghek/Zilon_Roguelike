5e9dc30a700e77eeecdfb863d19ed0220b4f9cdb
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-09-18 09:40:37 +0700

    Сборка через BuildCommand.cs

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,18 +1,28 @@
+#!/usr/bin/env bash
+
+BUILD_TARGET=StandaloneWindows64
+BUILD_NAME=LAST
+
+
 set -e
 set -x
 
+echo "Building for $BUILD_TARGET"
 
-BUILD_PATH=./ClientBuild
-
+export BUILD_PATH=./Builds/$BUILD_TARGET/
 mkdir -p $BUILD_PATH
 
 ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
   -projectPath ./Zilon.Client \
-  -buildWindows64Player $BUILD_PATH/LAST.exe \
+  -quit \
   -batchmode \
-  -logFile /dev/stdout \
-  -quit
-  
+  -buildTarget $BUILD_TARGET \
+  -customBuildTarget $BUILD_TARGET \
+  -customBuildName $BUILD_NAME \
+  -customBuildPath $BUILD_PATH \
+  -executeMethod BuildCommand.PerformBuild \
+  -logFile /dev/stdout
+
 UNITY_EXIT_CODE=$?
 
 if [ $UNITY_EXIT_CODE -eq 0 ]; then
