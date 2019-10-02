58e9ab4f1ad2f893a861a391afb3510d9dfb4870
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-02 22:48:02 +0700

    ConfigureAwait(false) в тестах ботов

--- Zilon.Core/Zilon.Bot.Players.DevelopmentTests/BotActorTaskSourceTests.cs
+++ Zilon.Core/Zilon.Bot.Players.DevelopmentTests/BotActorTaskSourceTests.cs
@@ -32,7 +32,13 @@ namespace Zilon.Bot.Players.DevelopmentTests
         [TestCase("monster")]
         public async Task GetActorTasksTestAsync(string mode)
         {
-            var humanActor = await CreateSectorAsync();
+            //TODO Объяснить, почему тут нужно использовать ConfigureAwait(false)
+            // Это рекомендация Codacy.
+            // Но есть статья https://habr.com/ru/company/clrium/blog/463587/,
+            // в которой объясняется, что не всё так просто.
+            // Нужно чёткое понимание, зачем здесь ConfigureAwait(false) и
+            // к какому результату это приводит по сравнению с простым await.
+            var humanActor = await CreateSectorAsync().ConfigureAwait(false);
 
             var gameLoop = _sectorServiceContainer.GetInstance<IGameLoop>();
             var scoreManager = _globalServiceContainer.GetInstance<IScoreManager>();
@@ -47,6 +53,7 @@ namespace Zilon.Bot.Players.DevelopmentTests
 
                 if (_changeSector)
                 {
+                    //TODO Объяснить, почему тут нужно использовать ConfigureAwait(false)
                     humanActor = await CreateSectorAsync().ConfigureAwait(false);
 
                     gameLoop = _sectorServiceContainer.GetInstance<IGameLoop>();
