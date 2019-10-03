89c6ebca8d79831f2d015485ee16a8b1c72b0e6a
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-04 00:16:50 +0700

    Исправление замечаний CodeFactor

--- Zilon.Core/Zilon.Core/Scoring/ScoreCalculator.cs
+++ Zilon.Core/Zilon.Core/Scoring/ScoreCalculator.cs
@@ -20,37 +20,11 @@
             // Здесь не может быть переполнения, потому что все эти числа получены из turns,
             // который является int. И дни, и часы являются результатом деления целочисленного turns.
 
-            var hours = (int)(hoursTotal - (float)days * 24);
+            float daysCuttedFloat = days * 24;
+            var hours = (int)(hoursTotal - daysCuttedFloat);
 
             return new DetailedLifetime(days, hours);
 
         }
-    }
-
-    /// <summary>
-    /// Структура для хранения разобранного значения времени жизни персонажа.
-    /// </summary>
-    public sealed class DetailedLifetime
-    {
-        /// <summary>
-        /// Конструктор объекта.
-        /// </summary>
-        /// <param name="days"> Количество целых прожитых дней. </param>
-        /// <param name="hours"> Количество целых пожитых часов. </param>
-        public DetailedLifetime(int days, int hours)
-        {
-            Days = days;
-            Hours = hours;
-        }
-
-        /// <summary>
-        /// Количество целых прожитых дней.
-        /// </summary>
-        public int Days { get; }
-
-        /// <summary>
-        /// Количество целых пожитых часов.
-        /// </summary>
-        public int Hours { get; }
     }
 }
