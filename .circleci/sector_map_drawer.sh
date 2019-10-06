814e88e29a3737260ed76c4b47864a2fb8dca0db
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-06 18:01:08 +0700

    Исправление скрипта запуска

--- .circleci/sector_map_drawer.sh
+++ .circleci/sector_map_drawer.sh
@@ -8,7 +8,7 @@ mkdir -p /test_sectorMapDrawer/maps
 for i in $(seq 1 10); do
   diceSeed=$RANDOM
 
-  /test_sectorMapDrawer/Zilon.Core/Zilon.SectorMapDrawer/Zilon.SectorMapDrawer/bin/Release/Zilon.SectorMapDrawer.exe \
+  /test_sectorMapDrawer/Zilon.Core/Zilon.SectorMapDrawer/bin/Release/Zilon.SectorMapDrawer.exe \
     dice_seed=$diceSeed \
     scheme_catalog="/test_sectorMapDrawer/Zilon.Client/Assets/Resources/Schemes" \
     out="/test_sectorMapDrawer/maps/map-$i.bmp"
