ce6640ac461a17611e2cd55ec8389fbf26bc1d29
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-07 14:03:18 +0700

    Корректировка запуска рисовальщика

--- .circleci/sector_map_drawer.sh
+++ .circleci/sector_map_drawer.sh
@@ -8,10 +8,10 @@ mkdir -p /test_sectorMapDrawer/maps
 chmod u+x /test_sectorMapDrawer/Zilon.Core/Zilon.SectorMapDrawer/bin/Release/Zilon.SectorMapDrawer.exe
 
 for i in $(seq 1 10); do
-  diceSeed=$(od -An -N2 -i /dev/random)
+  diceSeed="$(od -An -N2 -i /dev/random)"
 
   mono --debug /test_sectorMapDrawer/Zilon.Core/Zilon.SectorMapDrawer/bin/Release/Zilon.SectorMapDrawer.exe \
-    dice_seed=\""$diceSeed"\" \
+    dice_seed="$(diceSeed)" \
     scheme_catalog="/test_sectorMapDrawer/Zilon.Client/Assets/Resources/Schemes" \
     out="/test_sectorMapDrawer/maps/map-$i.bmp"
 done
