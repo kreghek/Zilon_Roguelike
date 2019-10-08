946348fa89c00f448bab39b31adc583afbd7966e
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-08 16:50:02 +0700

    Устранение дублирования

---
+++ Zilon.Core/Zilon.Core/LogicCalculations/RuleCalculations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zilon.Core.Components;
+
+namespace Zilon.Core.LogicCalculations
+{
+    /// <summary>
+    /// Вспомогательный класс для объединения всех вычислений, связанных с правилами.
+    /// </summary>
+    public static class RuleCalculations
+    {
+        /// <summary>
+        /// Расчёт нового модификатора эффективности в зависимости от правила.
+        /// </summary>
+        /// <param name="currentModifierValue"> Текущие значение модификатора. </param>
+        /// <param name="level"> Уровень правила. </param>
+        /// <returns> Возвращает новое значение модификатора эффективности. </returns>
+        public static int CalcEfficientByRuleLevel(int currentModifierValue, PersonRuleLevel level)
+        {
+            switch (level)
+            {
+                case PersonRuleLevel.Lesser:
+                    currentModifierValue++;
+                    break;
+
+                case PersonRuleLevel.Normal:
+                    currentModifierValue += 3;
+                    break;
+
+                case PersonRuleLevel.Grand:
+                    currentModifierValue += 5;
+                    break;
+
+                case PersonRuleLevel.Absolute:
+                    currentModifierValue += 10;
+                    break;
+            }
+
+            return currentModifierValue;
+        }
+    }
+}
