c5c5162e09b619ba5755b41389c24b2d2a3bcff3
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-08 21:36:23 +0700

    Стартовая локация на основе клеточного автомата

--- .circleci/sector_map_drawer.sh
+++ .circleci/sector_map_drawer.sh
@@ -8,7 +8,9 @@ mkdir -p /test_sectorMapDrawer/maps
 chmod u+x /test_sectorMapDrawer/Zilon.Core/Zilon.SectorMapDrawer/bin/Debug/Zilon.SectorMapDrawer.exe
 
 for i in $(seq 1 100); do
+  echo "======= $i ========="
   mono --debug /test_sectorMapDrawer/Zilon.Core/Zilon.SectorMapDrawer/bin/Debug/Zilon.SectorMapDrawer.exe \
     scheme_catalog="/test_sectorMapDrawer/Zilon.Client/Assets/Resources/Schemes" \
     out="/test_sectorMapDrawer/maps/map-$i.bmp"
+  echo "--------------------"
 done
