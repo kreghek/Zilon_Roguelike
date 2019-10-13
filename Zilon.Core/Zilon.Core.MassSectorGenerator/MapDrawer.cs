4ea0ef59d63f8439d5f1878062cf3fc37203452b
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-13 22:38:20 +0700

    Отлов блокировки коридоров сундуками

--- Zilon.Core/Zilon.Core.MassSectorGenerator/MapDrawer.cs
+++ Zilon.Core/Zilon.Core.MassSectorGenerator/MapDrawer.cs
@@ -11,15 +11,22 @@ namespace Zilon.Core.MassSectorGenerator
     {
         private const int CELLSIZE = 10;
 
-        public static Bitmap DrawMap(IMap map)
+        public static Bitmap DrawNodes(IEnumerable<HexNode> nodes)
         {
-            var hexNodes = map.Nodes.OfType<HexNode>();
-
-            var info = GetImageInfo(hexNodes);
+            var info = GetImageInfo(nodes);
 
             var bitmap = CreateBitmap(info);
 
-            DrawAllNodes(hexNodes, bitmap, info);
+            DrawAllNodes(nodes, bitmap, info);
+
+            return bitmap;
+        }
+
+        public static Bitmap DrawMap(IMap map)
+        {
+            var hexNodes = map.Nodes.OfType<HexNode>().ToArray();
+
+            var bitmap = DrawNodes(hexNodes);
 
             return bitmap;
         }
@@ -28,7 +35,10 @@ namespace Zilon.Core.MassSectorGenerator
         {
             using (var graphics = Graphics.FromImage(bitmap))
             {
-                graphics.FillRectangle(Brushes.Black, 0, 0, bitmap.Width, bitmap.Height);
+                Clear(bitmap, graphics);
+
+                DrawAxisNumbers(info, graphics);
+
                 foreach (var node in nodes)
                 {
                     var coords = HexHelper.ConvertToWorld(node.OffsetX, node.OffsetY);
@@ -36,7 +46,30 @@ namespace Zilon.Core.MassSectorGenerator
                     var x = (coords[0] - info.LeftCoord) * CELLSIZE;
                     var y = (coords[1] - info.BottomCoord) * CELLSIZE;
 
-                    graphics.FillEllipse(Brushes.White, x + CELLSIZE, y + CELLSIZE, CELLSIZE, CELLSIZE);
+                    var cellBrush = !node.IsObstacle ? Brushes.White : Brushes.Gray;
+
+                    graphics.FillEllipse(cellBrush, x + CELLSIZE, y + CELLSIZE, CELLSIZE, CELLSIZE);
+                }
+            }
+        }
+
+        private static void Clear(Bitmap bitmap, Graphics graphics)
+        {
+            graphics.FillRectangle(Brushes.Black, 0, 0, bitmap.Width, bitmap.Height);
+        }
+
+        private static void DrawAxisNumbers(ImageInfo info, Graphics graphics)
+        {
+            using (var font = new Font(SystemFonts.DefaultFont.FontFamily, 6, FontStyle.Regular))
+            {
+                for (var i = 0; i <= info.RightCoord - info.LeftCoord; i++)
+                {
+                    graphics.DrawString(i.ToString(), font, Brushes.White, i * CELLSIZE + CELLSIZE, 0);
+                }
+
+                for (var i = 0; i <= info.TopCoord - info.BottomCoord; i++)
+                {
+                    graphics.DrawString(i.ToString(), font, Brushes.White, CELLSIZE, (i * CELLSIZE) * 3f / 4 + CELLSIZE);
                 }
             }
         }
