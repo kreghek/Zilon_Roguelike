dff0402a3f06d649e23bf52c8d69e8113aa5d1b9
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-14 16:47:12 +0700

    Исправление запуска масс-генератора

--- .circleci/sector_mass_generator.sh
+++ .circleci/sector_mass_generator.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+mkdir -p /test_mass_sector_generator/maps
+
 chmod u+x /test_mass_sector_generator/Zilon.Core/Zilon.Core.MassSectorGenerator/bin/Debug/Zilon.Core.MassSectorGenerator.exe
 
 for i in $(seq 1 100); do
