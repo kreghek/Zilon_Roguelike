33c3740b9d1522c08f91770da89c7a205d18c70b
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-14 16:43:59 +0700

    Исправление запуска масс-ланчера

--- .circleci/sector_mass_generator.sh
+++ .circleci/sector_mass_generator.sh
@@ -8,6 +8,6 @@ chmod u+x /test_mass_sector_generator/Zilon.Core/Zilon.Core.MassSectorGenerator/
 for i in $(seq 1 100); do
   echo "======= $i ========="
   mono --debug /test_mass_sector_generator/Zilon.Core/Zilon.Core.MassSectorGenerator/bin/Debug/Zilon.Core.MassSectorGenerator.exe \
-    out="/test_sectorMapDrawer/maps/map-$i.bmp"
+    out="/test_mass_sector_generator/maps/map-$i.bmp"
   echo "--------------------"
 done
