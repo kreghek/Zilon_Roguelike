6f8ac43dfb606d506039d292cf67c65279ce949b
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-10-31 10:17:54 +0700

    Исправление ошибки с перекрытием прохода

---
+++ Zilon.Core/Zilon.Core.Tests/MapGenerators/CellularAutomatonStyle/InteriorObjectRandomSourceTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+
+using Moq;
+
+using NUnit.Framework;
+
+using Zilon.Core.CommonServices.Dices;
+
+namespace Zilon.Core.MapGenerators.CellularAutomatonStyle.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class InteriorObjectRandomSourceTests
+    {
+        [Test]
+        public void RollInteriorObjectsTest()
+        {
+            // ARRANGE
+
+            var diceMock = new Mock<IDice>();
+            diceMock.Setup(x => x.Roll(It.IsAny<int>())).Returns(1);
+            var dice = diceMock.Object;
+
+            var interiorRandomSource = new InteriorObjectRandomSource(dice);
+
+            var coords = new OffsetCoords[9];
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    coords[i + j * 3] = new OffsetCoords(i, j);
+                }
+            }
+
+            // ACT
+            var factMetas = interiorRandomSource.RollInteriorObjects(coords);
+
+            // ASSERT
+            factMetas.Should().NotBeEmpty();
+        }
+    }
+}
--- Zilon.Core/Zilon.Core/MapGenerators/CellularAutomatonStyle/InteriorObjectRandomSource.cs
+++ Zilon.Core/Zilon.Core/MapGenerators/CellularAutomatonStyle/InteriorObjectRandomSource.cs
@@ -29,23 +29,51 @@ namespace Zilon.Core.MapGenerators.CellularAutomatonStyle
         /// <returns> Возвращает набор метаданных об элементах интерьера. </returns>
         public InteriorObjectMeta[] RollInteriorObjects(OffsetCoords[] regionDraftCoords)
         {
-            var availableCoords = GetAvailableCoords(regionDraftCoords);
-            var openCoords = new List<OffsetCoords>(availableCoords);
+            if (regionDraftCoords is null)
+            {
+                throw new System.ArgumentNullException(nameof(regionDraftCoords));
+            }
+
+           // var availableCoords = GetAvailableCoords(regionDraftCoords);
+            var openCoords = new List<OffsetCoords>(regionDraftCoords);
             if (!openCoords.Any())
             {
-                return new InteriorObjectMeta[0];
+                return System.Array.Empty<InteriorObjectMeta>();
             }
 
             var count = openCoords.Count / 4;
 
-            var rolledCoordsList = new List<InteriorObjectMeta>();
+            var resultMetaList = new List<InteriorObjectMeta>();
             for (var i = 0; i < count; i++)
             {
-                var rolledCoords = _dice.RollFromList(openCoords.ToArray());
-                var interior = new InteriorObjectMeta(rolledCoords);
-                rolledCoordsList.Add(interior);
+                // Выполняем 3 попытки на размещение элемента декора.
+                // 2 из них могут быть неудачными (например, элемент
+                // декора перекрывает проход к какой-либо доступной ячейке).
+                for (var tryIndex = 0; tryIndex < 3; tryIndex++)
+                {
+                    var isValid = TryRollInteriorCoord(openCoords, regionDraftCoords, out var rolledCoord);
 
-                openCoords.Remove(rolledCoords);
+                    // Вне зависимости от корректности rolledCoord
+                    // убираем его из открытых координат.
+
+                    // В случае если он перекрывал одну из доступных ячеек.
+                    // Позже такая координата может стать корректной (например,
+                    // когда будет полукольцо из препятсвий).
+                    // Но мы пренебрегаем этим в целях производительности.
+                    openCoords.Remove(rolledCoord);
+
+                    if (isValid)
+                    {
+                        var interiorMeta = new InteriorObjectMeta(rolledCoord);
+                        resultMetaList.Add(interiorMeta);
+                        break;
+                    }
+
+                    if (!openCoords.Any())
+                    {
+                        break;
+                    }
+                }
 
                 if (!openCoords.Any())
                 {
@@ -53,7 +81,25 @@ namespace Zilon.Core.MapGenerators.CellularAutomatonStyle
                 }
             }
 
-            return rolledCoordsList.ToArray();
+            return resultMetaList.ToArray();
+        }
+
+        private bool TryRollInteriorCoord(IList<OffsetCoords> openCoords,
+            IEnumerable<OffsetCoords> passableRegionCoords,
+            out OffsetCoords rolledCoords)
+        {
+            var supposedRolledCoords = _dice.RollFromList(openCoords);
+
+            // Проверяем, что элемент декора не перекрывает проход.
+            var isNotBlockPass = CheckMapPassable(passableRegionCoords, supposedRolledCoords);
+            if (!isNotBlockPass)
+            {
+                rolledCoords = supposedRolledCoords;
+                return false;
+            }
+
+            rolledCoords = supposedRolledCoords;
+            return true;
         }
 
         private static IEnumerable<OffsetCoords> GetAvailableCoords(OffsetCoords[] regionDraftCoords)
@@ -84,6 +130,40 @@ namespace Zilon.Core.MapGenerators.CellularAutomatonStyle
                 var neighborCube = cube + neighborCubeOffset;
                 var neighborOffsetCoords = HexHelper.ConvertToOffset(neighborCube);
                 if (!coordHash.Contains(neighborOffsetCoords))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool CheckMapPassable(IEnumerable<OffsetCoords> currentCoords, OffsetCoords targetCoords)
+        {
+            var matrix = new Matrix<bool>(1000, 1000);
+            foreach (var coords in currentCoords)
+            {
+                var x = coords.X;
+                var y = coords.Y;
+                matrix.Items[x, y] = true;
+            }
+
+            // Закрываем проверяемый узел
+            matrix.Items[targetCoords.X, targetCoords.Y] = false;
+
+            var startPoint = currentCoords.First();
+            var floodPoints = HexBinaryFiller.FloodFill(matrix, startPoint);
+
+            foreach (var point in floodPoints)
+            {
+                matrix.Items[point.X, point.Y] = false;
+            }
+
+            foreach (var node in currentCoords)
+            {
+                var x = node.X;
+                var y = node.Y;
+                if (matrix.Items[x, y])
                 {
                     return false;
                 }
