89beb5787e35ccd4ac8d9b51025099d4bf788697
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-11-02 22:24:49 +0700

    Добавлен проект для бенчей на перемещение

---
+++ Zilon.Core/Zilon.Core.Benchmarks.NetCore.Move/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Zilon.Core.Benchmarks.Common;
+
+namespace Zilon.Core.Benchmarks.NetCore.Move
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConsoleApplicationConfigCreator.CreateBenchConfig(args);
+            BenchmarkRunner.Run<CreateGlobeBench>(config);
+        }
+    }
+}
