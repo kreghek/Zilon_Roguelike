b39a93fc139d0bc435f2121596a841231e6dbc7c
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-11-06 22:20:48 +0700

    Бенч для дайса в моно и юнити

---
+++ Zilon.Core/Zilon.Core.Benchmarks.Rng/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+using Zilon.Core.Benchmarks.Common;
+
+namespace Zilon.Core.Benchmarks.Rng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConsoleApplicationConfigCreator.CreateBenchConfig(args);
+            BenchmarkRunner.Run<RngBench>(config);
+        }
+    }
+}
--- Zilon.Core/Zilon.Core.Tests/CommonServices/Dices/DiceTestsBase.cs
+++ Zilon.Core/Zilon.Core.Tests/CommonServices/Dices/DiceTestsBase.cs
@@ -20,7 +20,7 @@ namespace Zilon.Core.Tests.CommonServices.Dices
         /// </summary>
         /// <param name="seed"> Зерно рандомизации для кости. </param>
         /// <param name="n"> Количество граней у кости. </param>
-        /// <param name="count"> Количество результатов, которое нужнополучить в течении теста. </param>
+        /// <param name="count"> Количество результатов, которое нужно получить в течении теста. </param>
         [Test]
         public void NextTest([Values(1, 100, 300, int.MaxValue)]int seed,
             [Values(1, 6, 100)] int n,
