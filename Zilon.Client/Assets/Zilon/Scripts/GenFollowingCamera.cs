7829b71244fc51e5e907afa07b1c00ef3ea540c6
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-12-07 16:57:55 +0700

    Камера для слежения за персонажами во время генерации мира

---
+++ Zilon.Client/Assets/Zilon/Scripts/GenFollowingCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+// ReSharper disable once UnusedMember.Global
+public class GenFollowingCamera : MonoBehaviour
+{
+    public ActorsViewModel ActorsViewModel;
+
+    // ReSharper disable once UnusedMember.Local
+    private void Update()
+    {
+        if (!ActorsViewModel.ActorViewModels.Any())
+        {
+            return;
+        }
+
+        var actorViewModel = ActorsViewModel.ActorViewModels.First();
+
+        var targetPosition = actorViewModel.transform.position;
+        var cameraPlanePosition = new Vector3(targetPosition.x, targetPosition.y, -10);
+
+        transform.position = Vector3.Lerp(transform.position,
+            cameraPlanePosition,
+            Time.deltaTime * 3);
+    }
+}
