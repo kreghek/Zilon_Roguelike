e96a9029092dd7477364067178f74684af3faa07
Pavel Kurkutov <kurkutovpa@gmail.com>
2019-12-09 14:29:27 +0700

    Смена наблюдаемого персонажа каждые 2 минуты

--- Zilon.Client/Assets/Zilon/Scripts/Models/GenScene/PersonFollower.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Models/GenScene/PersonFollower.cs
@@ -2,32 +2,72 @@
 
 using UnityEngine;
 
+using Zilon.Core.Persons;
+
 public class PersonFollower : MonoBehaviour
 {
+    /// <summary>
+    /// Время, в течении которого нужно следить за одним персонажем.
+    /// В секундах.
+    /// </summary>
+    private float FOLLOW_DURATION_SEC = 2 * 60;
+    private float _followCounter;
+
     public ActorsViewModel ActorsViewModel;
 
     public ActorViewModel FollowedPerson { get; private set; }
 
     public bool IsFollowing { get => FollowedPerson != null; }
 
-    public void Update()
+    public void FixedUpdate()
     {
         if (FollowedPerson != null)
         {
             return;
         }
 
-        TryFollowFirstPersonInSector();
+        if (FollowedPerson.Actor.Person.CheckIsDead())
+        {
+            FollowedPerson = null;
+            return;
+        }
+
+        _followCounter -= Time.deltaTime;
+
+        if (_followCounter <= 0)
+        {
+            _followCounter = FOLLOW_DURATION_SEC;
+
+            TrySelectPersonInSector();
+        }
     }
 
-    private void TryFollowFirstPersonInSector()
+    private void TrySelectPersonInSector()
     {
-        if (!ActorsViewModel.ActorViewModels.Any())
+        var aliveActors = ActorsViewModel.ActorViewModels.Where(x => !x.Actor.Person.CheckIsDead()).ToArray();
+
+        if (!aliveActors.Any())
         {
             return;
         }
 
-        var actorViewModel = ActorsViewModel.ActorViewModels.First();
+        var actorViewModel = SelectRandomActor(aliveActors);
+
+        FollowActorInSector(actorViewModel);
+    }
+
+    private static ActorViewModel SelectRandomActor(ActorViewModel[] aliveActors)
+    {
+        var personCount = aliveActors.Count();
+
+        var randomPersonIndex = Random.Range(0, personCount);
+
+        var actorViewModel = aliveActors[randomPersonIndex];
+        return actorViewModel;
+    }
+
+    private void FollowActorInSector(ActorViewModel actorViewModel)
+    {
         FollowedPerson = actorViewModel;
     }
 }
