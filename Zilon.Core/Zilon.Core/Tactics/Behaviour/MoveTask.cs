73080c75545b13c70a38075ef983d7b2ac335835
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-02-03 01:01:28 +0700

    Костыльная оптимизация тумана войны

--- Zilon.Core/Zilon.Core/Tactics/Behaviour/MoveTask.cs
+++ Zilon.Core/Zilon.Core/Tactics/Behaviour/MoveTask.cs
@@ -43,8 +43,11 @@ namespace Zilon.Core.Tactics.Behaviour
             Actor.MoveToNode(nextNode);
             _map.HoldNode(nextNode, Actor);
 
-            const int DISTANCE_OF_SIGN = 5;
-            FowHelper.UpdateFowData(Actor.SectorFowData, _map, nextNode, DISTANCE_OF_SIGN);
+            if (Actor.SectorFowData is HumanSectorFowData)
+            {
+                const int DISTANCE_OF_SIGN = 5;
+                FowHelper.UpdateFowData(Actor.SectorFowData, _map, nextNode, DISTANCE_OF_SIGN);
+            }
 
             _path.RemoveAt(0);
 
