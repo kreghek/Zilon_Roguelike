44084ec8bba61ffc004fe81051bbf75c70233d29
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-02-05 15:07:11 +0700

    Исправление замечаний

--- Zilon.Core/Zilon.Core/WorldGeneration/DirectBitmap.cs
+++ Zilon.Core/Zilon.Core/WorldGeneration/DirectBitmap.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 
 namespace Zilon.Core.WorldGeneration
 {
     public class DirectBitmap : IDisposable
     {
-        //public Bitmap Bitmap { get; private set; }
+        public Bitmap Bitmap { get; private set; }
         public int[] Bits { get; private set; }
         public bool Disposed { get; private set; }
         public int Height { get; private set; }
@@ -20,7 +21,7 @@ namespace Zilon.Core.WorldGeneration
             Height = height;
             Bits = new int[width * height];
             BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
-            //Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
+            Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
         }
 
         public void SetPixel(int x, int y, Color colour)
@@ -48,7 +49,7 @@ namespace Zilon.Core.WorldGeneration
             }
 
             Disposed = true;
-            //Bitmap.Dispose();
+            Bitmap.Dispose();
             BitsHandle.Free();
         }
     }
--- Zilon.Core/Zilon.Core/WorldGeneration/Globe.cs
+++ Zilon.Core/Zilon.Core/WorldGeneration/Globe.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 
@@ -66,11 +67,11 @@ namespace Zilon.Core.WorldGeneration
                     }
                 }
 
-                //realmFileName = realmFileName ?? "realms.bmp";
-                //realmBmp.Bitmap.Save(Path.Combine(path, realmFileName), ImageFormat.Bmp);
+                realmFileName = realmFileName ?? "realms.bmp";
+                realmBmp.Bitmap.Save(Path.Combine(path, realmFileName), ImageFormat.Bmp);
 
-                //branchFileName = branchFileName ?? "branches.bmp";
-                //branchmBmp.Bitmap.Save(Path.Combine(path, branchFileName), ImageFormat.Bmp);
+                branchFileName = branchFileName ?? "branches.bmp";
+                branchmBmp.Bitmap.Save(Path.Combine(path, branchFileName), ImageFormat.Bmp);
             }
         }
 
