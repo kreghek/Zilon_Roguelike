9296b3f82483588ddd62c73d28d28ae81c39745a
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-02-19 18:24:27 +0700

    Запуск генератора секторов на CI

--- .circleci/sector_mass_generator.sh
+++ .circleci/sector_mass_generator.sh
@@ -4,14 +4,12 @@ set -e
 
 mkdir -p /test_mass_sector_generator/maps
 
-chmod u+x /test_mass_sector_generator/Zilon.Core/Zilon.Core.MassSectorGenerator/bin/Debug/Zilon.Core.MassSectorGenerator.exe
-
 TOTAL_EXIT_CODE=0
 for i in $(seq 1 100); do
   echo "======= $i ========="
   
-  mono --debug /test_mass_sector_generator/Zilon.Core/Zilon.Core.MassSectorGenerator/bin/Debug/Zilon.Core.MassSectorGenerator.exe \
-    out="/test_mass_sector_generator/maps/map-$i.bmp"
+  dotnet run -p Zilon.Core/Zilon.Core.MassSectorGenerator/Zilon.Core.MassSectorGenerator.csproj -c Release \
+    -- out="/test_mass_sector_generator/maps/map-$i.bmp"
 	
   EXIT_CODE=$?
   
@@ -27,6 +25,6 @@ done
 if [ $TOTAL_EXIT_CODE -eq 0 ]; then
   echo "Run succeeded, no failures occurred";
 else
-  echo "Unexpected exit code $UNITY_EXIT_CODE";
+  echo "Unexpected exit code $TOTAL_EXIT_CODE";
   exit 1
 fi
