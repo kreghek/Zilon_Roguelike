e73404ee122f0f5e5824eb522074b214b2837b69
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-02-23 19:37:23 +0700

    Исправление индикаторов
    #897 #674

--- Zilon.Client/Assets/Zilon/Scripts/Services/StaticPhrases.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Services/StaticPhrases.cs
@@ -82,9 +82,10 @@ namespace Assets.Zilon.Scripts.Services
                 { "no-bullets", new MultilangPhrase{ En = "NO BULLETS", Ru = "НЕТ СНАРЯДОВ" } },
 
                 { "tooltip-no-bullets", new MultilangPhrase{ En = "No bullets for current weapon", Ru = "В инвентаре нет подходящих снарядов для текущего оружия" } },
-                { "tooltip-wait", new MultilangPhrase{ En = "Wait one turn", Ru = "Подождать один ход" } },
-                { "tooltip-person", new MultilangPhrase{ En = "Person window", Ru = "Окно персонажа" } },
-                { "tooltip-transition", new MultilangPhrase{ En = "Go to the next level", Ru = "Перейти на следующий уровень" } },
+                { "tooltip-wait", new MultilangPhrase{ En = "Wait one turn [E]", Ru = "Подождать один ход [E]" } },
+                { "tooltip-person", new MultilangPhrase{ En = "Person window [P]", Ru = "Окно персонажа [P]" } },
+                { "tooltip-transition", new MultilangPhrase{ En = "Go to the next level [T]", Ru = "Перейти на следующий уровень [T]" } },
+                { "tooltip-open-loot", new MultilangPhrase{ En = "Open loot [O]", Ru = "Открыть лут [O]" } },
                 { "tooltip-exit-title", new MultilangPhrase{ En = "Quit game and go to Main menu", Ru = "Покинуть игру и перейти в главное меню" } },
                 { "tooltip-exit", new MultilangPhrase{ En = "Quit game", Ru = "Покинуть игру" } },
 
