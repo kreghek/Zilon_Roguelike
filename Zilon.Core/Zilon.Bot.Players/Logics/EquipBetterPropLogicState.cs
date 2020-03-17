28bbdbc86f23534b77be76959a393d366caa025d
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-03-17 21:43:57 +0700

    Откат изменений

--- Zilon.Core/Zilon.Bot.Players/Logics/EquipBetterPropLogicState.cs
+++ Zilon.Core/Zilon.Bot.Players/Logics/EquipBetterPropLogicState.cs
@@ -1,6 +1,5 @@
 ﻿using System.Linq;
-using Zilon.Core.Common;
-using Zilon.Core.Components;
+
 using Zilon.Core.Persons;
 using Zilon.Core.Props;
 using Zilon.Core.Tactics;
@@ -20,14 +19,8 @@ namespace Zilon.Bot.Players.Logics
 
             for (var slotIndex = 0; slotIndex < equipmentCarrier.Slots.Length; slotIndex++)
             {
-                var targetSlotIndex = i;
-                var slot = actor.Person.EquipmentCarrier.Slots[i];
-                var equiped = actor.Person.EquipmentCarrier[i];
-
-                var availableEquipments = currentInventoryEquipments
-                        .Where(x => (x.Scheme.Equip.SlotTypes[0] & slot.Types) > 0)
-                        .Where(x => x.Scheme.Tags?.Contains(PropTags.Equipment.Ranged) != true);
-
+                var slot = actor.Person.EquipmentCarrier.Slots[slotIndex];
+                var equiped = actor.Person.EquipmentCarrier[slotIndex];
                 if (equiped == null)
                 {
                     var availableEquipments = currentInventoryEquipments
@@ -37,29 +30,10 @@ namespace Zilon.Bot.Players.Logics
 
                     if (availableEquipments.Any())
                     {
-                        var targetEquipmentFromInventory = GetBestEquipmentForSlot(slot, availableEquipments);
+                        var targetEquipmentFromInventory = availableEquipments.First();
                         var targetSlotIndex = slotIndex;
 
-                        
-
                         return new EquipTask(actor, targetEquipmentFromInventory, targetSlotIndex);
-                    }
-                }
-                else
-                {
-                    if (slot.Types.HasFlag(EquipmentSlotTypes.Hand))
-                    {
-                        if (equiped.Scheme.Tags?.Contains(PropTags.Equipment.Weapon) == true)
-                        {
-                            var currentWeaponRating = GetWeaponDamageRating(equiped);
-                            var targetEquipmentFromInventory = GetBestEquipmentForSlot(slot, availableEquipments);
-                            var targetWeaponRating = GetWeaponDamageRating(targetEquipmentFromInventory);
-
-                            if (targetWeaponRating > currentWeaponRating)
-                            {
-                                return new EquipTask(actor, targetEquipmentFromInventory, targetSlotIndex);
-                            }
-                        }
                     }
                 }
             }
@@ -68,58 +42,9 @@ namespace Zilon.Bot.Players.Logics
             return null;
         }
 
-        private Equipment GetBestEquipmentForSlot(PersonSlotSubScheme slot, IEnumerable<Equipment> availableEquipments)
-        {
-            Equipment targetEquipmentFromInventory;
-            if (slot.Types.HasFlag(EquipmentSlotTypes.Hand))
-            {
-                var weaponInventoryEquipments = availableEquipments
-                    .Where(x => x.Scheme.Tags?.Contains(PropTags.Equipment.Weapon) == true)
-                    .Where(x => x.Scheme.Tags?.Contains(PropTags.Equipment.Shield) != true);
-                targetEquipmentFromInventory = OrderWeaponByQuality(weaponInventoryEquipments).First();
-            }
-            else
-            {
-                var weaponInventoryEquipments = availableEquipments
-                    .Where(x => x.Scheme.Tags?.Contains(PropTags.Equipment.Weapon) != true);
-                targetEquipmentFromInventory = OrderArmorByQuality(weaponInventoryEquipments).First();
-            };
-            return targetEquipmentFromInventory;
-        }
-
         protected override void ResetData()
         {
             // Нет состояния.
-        }
-
-        private IEnumerable<Equipment> OrderWeaponByQuality(IEnumerable<Equipment> equipments)
-        {
-            return equipments.OrderByDescending(x => GetWeaponDamageRating(x));
-        }
-
-        private IEnumerable<Equipment> OrderArmorByQuality(IEnumerable<Equipment> equipments)
-        {
-            return equipments.OrderByDescending(x => GetArmorRating(x));
-        }
-
-        private int GetWeaponDamageRating(Equipment equipment)
-        {
-            var weaponEffecient = equipment.Acts[0].Stats.Efficient;
-            var rating = weaponEffecient.Dice * weaponEffecient.Count * weaponEffecient.Count;
-            return rating;
-        }
-
-        private int GetArmorRating(Equipment equipment)
-        {
-            var armors = equipment.Scheme.Equip.Armors;
-            var rating = 0;
-
-            foreach (var armor in armors)
-            {
-                rating = armor.ArmorRank * ((int)armor.AbsorbtionLevel + 1);
-            }
-
-            return rating;
         }
     }
 }
