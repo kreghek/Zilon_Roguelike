29a8e02c0934371f0ffc98f86997410ee7600e8b
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-03-17 22:59:56 +0700

    Введение сервиса для генерации стартовых перков

--- Zilon.Core/Zilon.Core/Persons/EvolutionData.cs
+++ Zilon.Core/Zilon.Core/Persons/EvolutionData.cs
@@ -14,9 +14,13 @@ namespace Zilon.Core.Persons
     {
         private readonly ISchemeService _schemeService;
 
+        private readonly List<IPerk> _buildInPerks;
+
         public EvolutionData(ISchemeService schemeService)
         {
             _schemeService = schemeService;
+
+            _buildInPerks = new List<IPerk>();
 
             Stats = new[] {
                 new SkillStatItem{Stat = SkillStatType.Ballistic, Value = 10 },
@@ -26,15 +30,24 @@ namespace Zilon.Core.Persons
             UpdatePerks();
         }
 
-        /// <summary>
-        /// Перечень навыков.
-        /// </summary>
+        /// <inheritdoc/>
         public SkillStatItem[] Stats { get; }
 
+        /// <inheritdoc/>
         public IPerk[] Perks { get; private set; }
 
+        /// <inheritdoc/>
         public event EventHandler<PerkEventArgs> PerkLeveledUp;
 
+        /// <inheritdoc/>
+        public void AddBuildInPerks(IEnumerable<IPerk> perks)
+        {
+            _buildInPerks.AddRange(perks);
+
+            UpdatePerks();
+        }
+
+        /// <inheritdoc/>
         public void PerkLevelUp(IPerk perk)
         {
             var activePerkIsValid = Perks.Contains(perk);
@@ -72,9 +85,12 @@ namespace Zilon.Core.Persons
 
         private void UpdatePerks()
         {
-            var schemes = _schemeService.GetSchemes<IPerkScheme>();
+            var schemes = _schemeService.GetSchemes<IPerkScheme>()
+                // Для развития годятся только те перки, которые не врождённые.
+                // Врождённые перки даются только при генерации персонажа.
+                .Where(x => !x.IsBuildIn);
 
-            var perks = new List<IPerk>();
+            var perks = new List<IPerk>(_buildInPerks);
             if (Perks != null)
             {
                 perks.AddRange(Perks);
