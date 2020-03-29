72a764ef8a98a151ddb942b6ce4c263a47163124
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-03-29 20:44:30 +0700

    Введение эффекта вируса

---
+++ Zilon.Core/Zilon.Core/Persons/Survival/VirusHazardEffect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Zilon.Core.Components;
+
+namespace Zilon.Core.Persons.Survival
+{
+    public class VirusHazardEffect : IPersonEffect
+    {
+        public VirusHazardEffect(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        public string Name { get; }
+
+        public event EventHandler Changed;
+
+        public EffectRule[] GetRules()
+        {
+            return new[] { new EffectRule(RollEffectType.Efficient, PersonRuleLevel.Lesser) };
+        }
+    }
+}
