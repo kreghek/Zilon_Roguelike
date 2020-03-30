7040517ec7aa86c4a5ddb3881b2ae6ff8176c4e7
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-03-30 15:11:33 +0700

    Исправление тестов

--- Zilon.Core/Zilon.Core.Tests/MapGenerators/SectorProceduralGeneratorTests.cs
+++ Zilon.Core/Zilon.Core.Tests/MapGenerators/SectorProceduralGeneratorTests.cs
@@ -12,6 +12,8 @@ using Zilon.Core.MapGenerators;
 using Zilon.Core.MapGenerators.RoomStyle;
 using Zilon.Core.Players;
 using Zilon.Core.Schemes;
+using Zilon.Core.Tactics;
+using Zilon.Core.Tactics.Spatial;
 using Zilon.Core.Tests.Common.Schemes;
 using Zilon.Core.Tests.MapGenerators.RoomStyle;
 using Zilon.Core.World;
@@ -95,6 +97,11 @@ namespace Zilon.Core.Tests.MapGenerators
             mapFactorySelectorMock.Setup(x => x.GetMapFactory(It.IsAny<ISectorNode>()))
                 .Returns(mapFactory);
             var mapFactorySelector = mapFactorySelectorMock.Object;
+
+            var sectorMock = new Mock<ISector>();
+            var sector = sectorMock.Object;
+            sectorFactoryMock.Setup(x => x.Create(It.IsAny<ISectorMap>(), It.IsAny<ILocationScheme>()))
+                .Returns(sector);            
 
             return new SectorGenerator(mapFactorySelector,
                 sectorFactory,
