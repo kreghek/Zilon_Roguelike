ee4963969705c0dcdd00e91061fbfed525d294bf
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-04-02 21:59:46 +0700

    Удаление веб интерфейса для запуска турниров

--- Zilon.Core/Zilon.Tournament.ApiGate/Launcher/BotInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Zilon.Tournament.ApiGate.Launcher
-{
-    public sealed class BotInfo
-    {
-        public string Catalog { get; set; }
-        public string Assembly { get; set; }
-        public string[] Modes { get; set; }
-    }
-}
--- Zilon.Core/Zilon.Tournament.ApiGate/Models/MeasureValue.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Zilon.Tournament.ApiGate.Models
-{
-    public class MeasureValue
-    {
-        public double TotalValue { get; set; }
-
-        public double LastValue { get; set; }
-    }
-}
