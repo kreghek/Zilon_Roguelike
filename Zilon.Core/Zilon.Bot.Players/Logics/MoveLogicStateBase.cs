ed7d156ff0a5dbcb10a58a890670905c31eb4916
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-04-09 11:56:46 +0700

    Упразднение дублирования

---
+++ Zilon.Core/Zilon.Bot.Players/Logics/MoveLogicStateBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Zilon.Core.Tactics;
+using Zilon.Core.Tactics.Behaviour;
+using Zilon.Core.Tactics.Behaviour.Bots;
+using Zilon.Core.Tactics.Spatial;
+
+namespace Zilon.Bot.Players.Logics
+{
+    public abstract class MoveLogicStateBase: LogicStateBase
+    {
+        protected MoveTask MoveTask { get; set; }
+        protected IdleTask IdleTask { get; set; }
+
+        protected IDecisionSource DecisionSource { get; }
+
+        protected ISectorMap Map { get; }
+
+        public MoveLogicStateBase(IDecisionSource decisionSource, ISectorManager sectorManager)
+        {
+            if (sectorManager is null)
+            {
+                throw new ArgumentNullException(nameof(sectorManager));
+            }
+
+            DecisionSource = decisionSource ?? throw new ArgumentNullException(nameof(decisionSource));
+            Map = sectorManager.CurrentSector.Map;
+        }
+
+        protected override void ResetData()
+        {
+            MoveTask = null;
+            IdleTask = null;
+        }
+    }
+}
