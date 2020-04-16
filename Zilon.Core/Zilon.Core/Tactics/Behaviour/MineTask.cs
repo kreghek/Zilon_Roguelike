137b428b6f0e5d802e9729cc1b851ed3ea58d637
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-04-16 16:24:33 +0700

    Suppress CA1303 as Hard to maintain

--- Zilon.Core/Zilon.Core/Tactics/Behaviour/MineTask.cs
+++ Zilon.Core/Zilon.Core/Tactics/Behaviour/MineTask.cs
@@ -27,14 +27,14 @@ namespace Zilon.Core.Tactics.Behaviour
             var distance = _map.DistanceBetween(Actor.Node, _staticObject.Node);
             if (distance > 1)
             {
-                throw new InvalidOperationException("Невозможно взаимодействовать с контейнером на расстоянии больше 1.");
+                throw new InvalidOperationException("Невозможно взаимодействовать с объектом на расстоянии больше 1.");
             }
 
             var targetIsOnLine = _map.TargetIsOnLine(Actor.Node, _staticObject.Node);
 
             if (!targetIsOnLine)
             {
-                throw new InvalidOperationException("Задачу на открытие сундука нельзя выполнить сквозь стены.");
+                throw new InvalidOperationException("Задачу на добычу нельзя выполнить сквозь стены.");
             }
 
             Actor.MineDeposit(_staticObject, _method);
