683903d9027381a24dc2efd192501370c73dea2d
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-04-17 10:25:38 +0700

    WIP Introducing ILifetimeModule.cs

---
+++ Zilon.Core/Zilon.Core/StaticObjectModules/ILifetimeModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zilon.Core.StaticObjectModules
+{
+    /// <summary>
+    /// Модуль, указывающий, что статический объект может находится в секторе какие-то определённое время.
+    /// Это нужно:
+    /// - Залежам, которые исчезают, когда исчерпаны.
+    /// - Кустам, которые могут быть срезаны.
+    /// - Деревьям, которые могут быть убраны.
+    /// - Лужам, которые исчерпытваются.
+    /// </summary>
+    public interface ILifetimeModule : IStaticObjectModule
+    {
+        event EventHandler Destroyed;
+    }
+}
---
+++ Zilon.Core/Zilon.Core/StaticObjectModules/LifetimeModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zilon.Core.StaticObjectModules
+{
+    public sealed class LifetimeModule : ILifetimeModule
+    {
+        public LifetimeModule()
+        {
+            IsActive = true;
+        }
+
+        /// <inheritdoc/>
+        public string Key { get => nameof(ILifetimeModule); }
+
+        /// <inheritdoc/>
+        public bool IsActive { get; set; }
+
+        /// <inheritdoc/>
+        public event EventHandler Destroyed;
+    }
+}
