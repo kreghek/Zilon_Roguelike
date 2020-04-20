9cad923dcb7ea0d8c0dd88068b9b8503cd0be270
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-04-20 17:37:59 +0700

    Split classes to separate files

--- Zilon.Core/Zilon.Core/World/IResourceDepositData.cs
+++ Zilon.Core/Zilon.Core/World/IResourceDepositData.cs
@@ -6,27 +6,4 @@ namespace Zilon.Core.World
     {
         IEnumerable<ResourceDepositDataItem> Items { get; }
     }
-
-    public sealed class ResourceDepositData : IResourceDepositData
-    {
-        public ResourceDepositData(IEnumerable<ResourceDepositDataItem> items)
-        {
-            Items = items ?? throw new System.ArgumentNullException(nameof(items));
-        }
-
-        public IEnumerable<ResourceDepositDataItem> Items { get; }
-    }
-
-    public sealed class ResourceDepositDataItem
-    {
-        public ResourceDepositDataItem(SectorResourceType resourceType, float share)
-        {
-            ResourceType = resourceType;
-            Share = share;
-        }
-
-        public SectorResourceType ResourceType { get; }
-
-        public float Share { get; }
-    }
 }
--- Zilon.Core/Zilon.Core/World/IResourceMaterializationMap.cs
+++ Zilon.Core/Zilon.Core/World/IResourceMaterializationMap.cs
@@ -17,18 +17,4 @@
         /// </remarks>
         IResourceDepositData GetDepositData(ISectorNode sectorNode);
     }
-
-    public sealed class ResourceMaterializationMap : IResourceMaterializationMap
-    {
-        public IResourceDepositData GetDepositData(ISectorNode sectorNode)
-        {
-            var items = new[] { 
-                new ResourceDepositDataItem(SectorResourceType.Iron, 10),
-                new ResourceDepositDataItem(SectorResourceType.Stones, 10),
-                new ResourceDepositDataItem(SectorResourceType.WaterPuddles, 10),
-                new ResourceDepositDataItem(SectorResourceType.CherryBrushes, 10),
-            };
-            return new ResourceDepositData(items);
-        }
-    }
 }
