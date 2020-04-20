9eaf494c34235166ce0424f86f9ff3491d5c98f0
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-04-20 13:26:46 +0700

    IStaticObjectFactoryCollector registered as single on client

--- Zilon.Client/Assets/Zilon/Scripts/DependencyInjection/StaticObjectPrefabSelectorExtensions.cs
+++ Zilon.Client/Assets/Zilon/Scripts/DependencyInjection/StaticObjectPrefabSelectorExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Assets.Zilon.Scripts.Services;
 
 using Zenject;
+
 using Zilon.Core.MapGenerators.StaticObjectFactories;
 
 namespace Assets.Zilon.Scripts.DependencyInjection
@@ -11,12 +12,13 @@ namespace Assets.Zilon.Scripts.DependencyInjection
         {
             diContainer.Bind<StaticObjectViewModelSelector>().AsSingle();
 
-            diContainer.Bind<IStaticObjectFactoryCollector>().FromMethod(diFactory => {
+            diContainer.Bind<IStaticObjectFactoryCollector>().FromMethod(diFactory =>
+            {
                 var factories = diFactory.Container.ResolveAll<IStaticObjectFactory>().ToArray();
                 return new StaticObjectFactoryCollector(factories);
-            });
+            }).AsSingle();
             diContainer.Bind<IStaticObjectFactory>().To<StoneDepositFactory>().AsSingle();
-            diContainer.Bind<IStaticObjectFactory>().To<OreDepositFactory >().AsSingle();
+            diContainer.Bind<IStaticObjectFactory>().To<OreDepositFactory>().AsSingle();
             diContainer.Bind<IStaticObjectFactory>().To<TrashHeapFactory>().AsSingle();
             diContainer.Bind<IStaticObjectFactory>().To<CherryBrushFactory>().AsSingle();
             diContainer.Bind<IStaticObjectFactory>().To<PitFactory>().AsSingle();
