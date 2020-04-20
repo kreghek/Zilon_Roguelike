c39da15eee959725a1f6ae033f882da5bd23405a
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-04-20 11:04:28 +0700

    Creating static objects via factories

---
+++ Zilon.Core/Zilon.Core/MapGenerators/StaticObjectFactories/IStaticObjectfactoryCollector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zilon.Core.MapGenerators.StaticObjectFactories
+{
+    public interface IStaticObjectfactoryCollector
+    {
+        IStaticObjectFactory[] GetFactories();
+    }
+}
