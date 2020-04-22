e7e305947ff47884569517ccd3e9104a035cf01e
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-04-22 22:09:46 +0700

    Add IDurabilityModule implementation for deposit

---
+++ Zilon.Core/Zilon.Core/StaticObjectModules/DepositDurabilityModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Zilon.Core.StaticObjectModules
+{
+    public sealed class DepositDurabilityModule : IDurabilityModule
+    {
+        private readonly IPropDepositModule _propDepositModule;
+        private readonly int _damagePerMineUnit;
+        private int _mineDamageCounter;
+
+        public DepositDurabilityModule(IPropDepositModule propDepositModule, int damagePerMineUnit)
+        {
+            IsActive = true;
+            _propDepositModule = propDepositModule;
+            _damagePerMineUnit = damagePerMineUnit;
+            _mineDamageCounter = _damagePerMineUnit;
+        }
+
+        public float Value { get => _propDepositModule.Stock; }
+        public string Key { get => nameof(IDurabilityModule); }
+        public bool IsActive { get; set; }
+
+        public void TakeDamage(int damageValue)
+        {
+            if (_mineDamageCounter <= 0)
+            {
+                throw new InvalidOperationException("Попытка нанести урон залежам с нулевым счётчиком.");
+            }
+
+            _mineDamageCounter -= damageValue;
+            if (_mineDamageCounter <= 0)
+            {
+                _propDepositModule.Mine();
+                _mineDamageCounter = _damagePerMineUnit;
+            }
+        }
+    }
+}
