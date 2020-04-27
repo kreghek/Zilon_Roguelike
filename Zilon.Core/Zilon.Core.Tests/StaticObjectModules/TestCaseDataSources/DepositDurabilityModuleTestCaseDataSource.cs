e57bd7fe046efb979e6dee408f543d2b47b9b13b
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-04-27 21:55:42 +0700

    Fix Codacy issues

--- Zilon.Core/Zilon.Core.Tests/StaticObjectModules/TestCaseDataSources/DepositDurabilityModuleTestCaseDataSource.cs
+++ Zilon.Core/Zilon.Core.Tests/StaticObjectModules/TestCaseDataSources/DepositDurabilityModuleTestCaseDataSource.cs
@@ -10,14 +10,14 @@ namespace Zilon.Core.Tests.StaticObjectModules.TestCaseDataSources
         {
             get
             {
-                // StockValue = 1;
-                // DamagePerMineUnit = 10;
-                // damages = new int[] { 6, 4 };
-                yield return new TestCaseData(1, 10, new int[] { 6, 4 });
-                yield return new TestCaseData(2, 10, new int[] { 6, 4, 5, 5 });
-                yield return new TestCaseData(1, 1, new int[] { 2 });
-                yield return new TestCaseData(1, 10, new int[] { 10 });
-                yield return new TestCaseData(1, 10, new int[] { 6, 6 });
+                // StockValue, for example 1.
+                // DamagePerMineUnit, for example 10.
+                // damages, for example new [] { 6, 4 }.
+                yield return new TestCaseData(1, 10, new[] { 6, 4 });
+                yield return new TestCaseData(2, 10, new[] { 6, 4, 5, 5 });
+                yield return new TestCaseData(1, 1, new[] { 2 });
+                yield return new TestCaseData(1, 10, new[] { 10 });
+                yield return new TestCaseData(1, 10, new[] { 6, 6 });
             }
         }
     }
