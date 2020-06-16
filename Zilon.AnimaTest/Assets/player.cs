8fb83dd635add37916258a41048773402376a5ad
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-06-16 20:55:40 +0700

    New head

---
+++ Zilon.AnimaTest/Assets/player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Animator Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) /*&& Animator.GetCurrentAnimatorClipInfo(0)[0].clip.name != "Human Player Walk"*/)
+        {
+            Animator.Play("Walk");
+        }
+    }
+}
