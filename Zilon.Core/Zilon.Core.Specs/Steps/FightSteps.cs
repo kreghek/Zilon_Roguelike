bd54cdd93da44e84842e912869d8d5f7949a6427
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-11-16 22:37:59 +0700

    Set LINQ indentions like dotnet-format

--- Zilon.Core/Zilon.Core.Specs/Steps/FightSteps.cs
+++ Zilon.Core/Zilon.Core.Specs/Steps/FightSteps.cs
@@ -159,8 +159,8 @@ namespace Zilon.Core.Specs.Steps
                     }
 
                     var equipmentActs = from act in actor.Person.GetModule<ICombatActModule>().CalcCombatActs()
-                        where act.Equipment == slotEquipment
-                        select act;
+                                        where act.Equipment == slotEquipment
+                                        select act;
 
                     var usedAct = equipmentActs.FirstOrDefault();
 
