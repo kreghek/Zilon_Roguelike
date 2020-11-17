db2da7675bae951fa5ffad1b33501e8b68106fb4
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-11-17 14:01:04 +0700

    Revent branch 3

---
+++ Zilon.Core/Zilon.Core/Persons/PhysicalSize.cs
@@ -0,0 +1,17 @@
+﻿namespace Zilon.Core.Persons
+{
+    public enum PhysicalSize
+    {
+        Undefined = 0,
+
+        /// <summary>
+        /// Объект размеров в 1 ячейку.
+        /// </summary>
+        Size1,
+
+        /// <summary>
+        /// Объект размером в ячейку и её окружность.
+        /// </summary>
+        Size7
+    }
+}
