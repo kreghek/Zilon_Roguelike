0c7d5e408dcdd500960c68ecf7e2025139a3c34b
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-11-17 10:47:52 +0700

    Fix Codacy issues - [SonarCSharp_S2228]

--- Zilon.Core/Zilon.BotMassLauncher/Program.cs
+++ Zilon.Core/Zilon.BotMassLauncher/Program.cs
@@ -1,174 +1,38 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.IO;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Zilon.BotMassLauncher
 {
-    internal class Program
+    internal static class Program
     {
-        private static string _pathToEnv;
-        private static int _launchCount;
-        private static string _scorePreffix;
-        private static string _parallel;
-        private static bool _isInfinite;
-        private static ulong _infiniteCounter;
-        private static string _botMode;
-        private static string _scorePath;
-        private static string _botCatalog;
-        private static string _botAssembly;
-        private static string _schemeCatlogPath;
-        private static CancellationToken _shutdownToken;
-        private static CancellationTokenSource _shutdownTokenSource;
-
-        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        private static void BuildConfig(IConfigurationBuilder builder)
         {
-            if (_shutdownTokenSource != null)
-            {
-                _shutdownTokenSource.Cancel();
-            }
-        }
-
-        private static string GetProgramArgument(string[] args, string testArg)
-        {
-            if (args == null)
-            {
-                return null;
-            }
-
-            foreach (var arg in args)
-            {
-                var components = arg.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
-                if (string.Equals(components[0], testArg, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    if (components.Length >= 2)
-                    {
-                        return components[1];
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        private static bool HasProgramArgument(string[] args, string testArg)
-        {
-            return args?.Select(x => x?.Trim().ToLowerInvariant()).Contains(testArg.ToLowerInvariant()) == true;
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables();
         }
 
         private static void Main(string[] args)
         {
-            Console.WriteLine("[x] START");
+            var configBuilder = new ConfigurationBuilder();
+            BuildConfig(configBuilder);
 
-            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+            var configuration = configBuilder.Build();
 
-            _pathToEnv = GetProgramArgument(args, "env");
-            _launchCount = int.Parse(GetProgramArgument(args, "launchCount"));
-            _scorePreffix = DateTime.UtcNow.ToString().Replace(":", "_").Replace(".", "_");
+            var host = Host
+                .CreateDefaultBuilder()
+                .ConfigureServices((context, services) => services
+                .AddLogging(loggingBuilder => loggingBuilder.AddConsole().AddConfiguration(configuration))
+                )
+                .Build();
+            
+            var workload = ActivatorUtilities.CreateInstance<Workload>(host.Services);
 
-            _parallel = GetProgramArgument(args, "parallel");
-            _isInfinite = HasProgramArgument(args, "infinite");
-            _botMode = GetProgramArgument(args, "mode");
-            _scorePath = GetProgramArgument(args, "output");
-
-            _botCatalog = GetProgramArgument(args, "botCatalog");
-            _botAssembly = GetProgramArgument(args, "botAssembly");
-
-            _schemeCatlogPath = GetProgramArgument(args, "schemeCatalogPath");
-
-            _shutdownTokenSource = new CancellationTokenSource();
-            _shutdownToken = _shutdownTokenSource.Token;
-
-            do
-            {
-                if (_isInfinite)
-                {
-                    Console.WriteLine($"[x] INFINITE COUNTER {_infiniteCounter}");
-                }
-
-                if (!string.IsNullOrWhiteSpace(_parallel))
-                {
-                    RunParallel(int.Parse(_parallel));
-                }
-                else
-                {
-                    RunLinear();
-                }
-
-                _infiniteCounter++;
-                if (_infiniteCounter == ulong.MaxValue)
-                {
-                    _infiniteCounter = 0;
-                }
-            } while (_isInfinite);
-
-            Console.WriteLine("[x] COMPLETE");
-        }
-
-        private static void RunEnvironment(int iteration)
-        {
-            var modeArg = string.Empty;
-            if (!string.IsNullOrEmpty(_botMode))
-            {
-                modeArg = $" mode={_botMode}";
-            }
-
-            var infiniteCounterPreffix = string.Empty;
-            if (_isInfinite)
-            {
-                infiniteCounterPreffix = _infiniteCounter + " ";
-            }
-
-            Console.WriteLine($"[x] {infiniteCounterPreffix}ITERATION {iteration} STARTED");
-
-            using (var process = new Process())
-            {
-                process.StartInfo = new ProcessStartInfo
-                {
-                    FileName = _pathToEnv,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    Arguments =
-                        $"serverrun ScorePreffix=\"{_scorePreffix}\"{modeArg} schemeCatalogPath={_schemeCatlogPath} output={_scorePath} botCatalog={_botCatalog} botAssembly={_botAssembly}",
-
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true
-                };
-
-                process.Start();
-
-                var output = process.StandardOutput.ReadToEnd();
-                var error = process.StandardError.ReadToEnd();
-
-                Console.WriteLine("[x]OUTPUT");
-                Console.WriteLine(output);
-                Console.WriteLine("[x]ERROR");
-                Console.WriteLine(error);
-
-                process.WaitForExit(30000);
-            }
-
-            Console.WriteLine($"[x] {infiniteCounterPreffix}ITERATION {iteration} FINISHED");
-        }
-
-        private static void RunLinear()
-        {
-            for (var i = 0; i < _launchCount; i++)
-            {
-                RunEnvironment(i);
-            }
-        }
-
-        private static void RunParallel(int maxDegreeOfParallelism)
-        {
-            var parallelOptions = new ParallelOptions
-            {
-                MaxDegreeOfParallelism = maxDegreeOfParallelism,
-                CancellationToken = _shutdownToken
-            };
-
-            Parallel.For(0, _launchCount, parallelOptions, RunEnvironment);
+            workload.Run(args);
         }
     }
 }
