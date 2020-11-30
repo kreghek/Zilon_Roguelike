9669c001fd4eecdfbeabcc14e4595d48898569f3
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-11-30 22:39:44 +0700

    Split BiomeInitializer and IGlobeExpander.cs to different documents

--- Zilon.Core/Zilon.Core/World/BiomeInitializer.cs
+++ Zilon.Core/Zilon.Core/World/BiomeInitializer.cs
@@ -7,20 +7,6 @@ using Zilon.Core.Schemes;
 
 namespace Zilon.Core.World
 {
-    /// <summary>
-    /// Интерфейс расширителя мира.
-    /// Каждый раз, когда кто-нибудь доходит до края мира, мир будет расширяться.
-    /// </summary>
-    public interface IGlobeExpander
-    {
-        /// <summary>
-        /// Расширение мира в указанной узл.
-        /// В результате указанный узел будет материализован и иметь переходы в новые нематериализованные узлы.
-        /// </summary>
-        /// <param name="sectorNode"> Узел расширения. </param>
-        Task ExpandAsync(ISectorNode sectorNode);
-    }
-
     public class BiomeInitializer : IBiomeInitializer, IGlobeExpander
     {
         private readonly IBiomeSchemeRoller _biomeSchemeRoller;
