71d6293bf08f83483eed9d87a0de110518ae7296
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-12-01 20:53:16 +0700

    MassSectorGenerator works again

---
+++ Zilon.Core/Zilon.Core.MassSectorGenerator/ImageHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+using Zilon.Core.Tactics;
+using Zilon.Core.Tactics.Spatial;
+
+namespace Zilon.Core.MassSectorGenerator
+{
+    internal class ImageHelper
+    {
+        private static void SaveMapAsImage(ISectorMap map, string outputPath)
+        {
+            using var bmp = MapDrawer.DrawMap(map);
+            bmp.Save(outputPath);
+        }
+
+        public static Task SaveMapAsImageAsync(string outputPath, ISector sector)
+        {
+            if (outputPath != null)
+            {
+                SaveMapAsImage(sector.Map, outputPath);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Zilon.Core/Zilon.Core.MassSectorGenerator/ValidatorCollector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Zilon.Core.MassSectorGenerator
+{
+    internal static class ValidatorCollector
+    {
+        public static ISectorValidator[] GetValidatorsInAssembly()
+        {
+            var thisAssembly = Assembly.GetExecutingAssembly();
+            var validatorTypes = thisAssembly.GetTypes()
+                .Where(x => typeof(ISectorValidator).IsAssignableFrom(x))
+                .Where(x => !x.IsInterface && !x.IsAbstract);
+
+            var validators = validatorTypes.Select(x => Activator.CreateInstance(x)).Cast<ISectorValidator>();
+
+            return validators.ToArray();
+        }
+    }
+}
