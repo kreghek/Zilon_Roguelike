e2da549ac84cc2d9edec7d24b8a2fdab0ed73df5
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-12-06 22:47:23 +0700

    Reset the game state if quit to title menu

--- Zilon.Client/Assets/Zilon/Scripts/Commands/QuitTitleCommand.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Commands/QuitTitleCommand.cs
@@ -1,11 +1,26 @@
-﻿using UnityEngine.SceneManagement;
+﻿using Assets.Zilon.Scripts.Services;
+
+using JetBrains.Annotations;
+
+using UnityEngine.SceneManagement;
+
+using Zenject;
 
 using Zilon.Core.Commands;
+using Zilon.Core.Players;
 
 namespace Assets.Zilon.Scripts.Commands
 {
     sealed class QuitTitleCommand : ICommand
     {
+        [Inject]
+        [NotNull]
+        private readonly IPlayer _player;
+
+        [NotNull]
+        [Inject]
+        private readonly GlobeStorage _globeStorage;
+
         public bool CanExecute()
         {
             return true;
@@ -13,6 +28,9 @@ namespace Assets.Zilon.Scripts.Commands
 
         public void Execute()
         {
+            _player.Reset();
+            _globeStorage.Reset();
+
             SceneManager.LoadScene("title");
         }
     }
