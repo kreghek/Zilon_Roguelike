bbdeb107b07053eb0ae08d668e2c5315f515315a
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-12-14 10:28:14 +0000

    [Bot] Automated dotnet-format update

--- Zilon.Core/Zilon.TextClient/GlobeSelectionScreenHandler.cs
+++ Zilon.Core/Zilon.TextClient/GlobeSelectionScreenHandler.cs
@@ -8,7 +8,7 @@ using Zilon.Core.World;
 
 namespace Zilon.TextClient
 {
-    internal class GlobeSelectionScreenHandler: IScreenHandler
+    internal class GlobeSelectionScreenHandler : IScreenHandler
     {
         public async Task<GameScreen> StartProcessingAsync(IServiceScope serviceScope)
         {
@@ -17,7 +17,7 @@ namespace Zilon.TextClient
 
             Console.WriteLine("Globe created");
             Console.WriteLine($"Nodes: {globe.SectorNodes.Count()}");
-            Console.WriteLine($"Persons: {globe.SectorNodes.Select(x=>x.Sector).SelectMany(x=>x.ActorManager.Items).Count()}");
+            Console.WriteLine($"Persons: {globe.SectorNodes.Select(x => x.Sector).SelectMany(x => x.ActorManager.Items).Count()}");
             Console.WriteLine("Press Enter to continue...");
             Console.ReadLine();
 
