99a1ac2b324c80b5751824ce78f428e9d859aeeb
Pavel Kurkutov <kurkutovpa@gmail.com>
2020-12-15 10:08:44 +0700

    Docs

--- Zilon.Core/Zilon.TextClient/GameState.cs
+++ Zilon.Core/Zilon.TextClient/GameState.cs
@@ -2,10 +2,24 @@
 
 namespace Zilon.TextClient
 {
+    /// <summary>
+    /// The common system text game data.
+    /// </summary>
     internal class GameState
     {
+        /// <summary>
+        /// Current game screen.
+        /// </summary>
         public GameScreen CurrentScreen { get; set; }
+
+        /// <summary>
+        /// Full service provider.
+        /// </summary>
         public ServiceProvider ServiceProvider { get; internal set; }
+
+        /// <summary>
+        /// Service provider scope for current game state.
+        /// </summary>
         public IServiceScope ServiceScope { get; set; }
     }
 }
