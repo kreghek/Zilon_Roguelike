70f4821e84db0d5cc8ee1ea53ee3f2832e2ad173
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-06 11:39:34 +0000

    [Bot] Automated ReSharper CLT update

--- Zilon.Core/Zilon.TextClient/MainScreenHandler.cs
+++ Zilon.Core/Zilon.TextClient/MainScreenHandler.cs
@@ -23,6 +23,133 @@ namespace Zilon.TextClient
     /// </summary>
     internal class MainScreenHandler : IScreenHandler
     {
+        private static ISectorNode GetPlayerSectorNode(IPlayer player, IGlobe globe)
+        {
+            return (from sectorNode in globe.SectorNodes
+                    from actor in sectorNode.Sector.ActorManager.Items
+                    where actor.Person == player.MainPerson
+                    select sectorNode).SingleOrDefault();
+        }
+
+        private static void HandleDeadCommand(IPlayer player)
+        {
+            var survivalModule = player.MainPerson.GetModule<ISurvivalModule>();
+            survivalModule.SetStatForce(Core.Persons.SurvivalStatType.Health, 0);
+        }
+
+        private static void HandleIdleCommand(IServiceScope serviceScope)
+        {
+            var command = serviceScope.ServiceProvider.GetRequiredService<IdleCommand>();
+
+            if (command.CanExecute())
+            {
+                command.Execute();
+            }
+            else
+            {
+                Console.WriteLine(UiResource.CommandCantExecuteMessage);
+            }
+        }
+
+        private static void HandleLookCommand(ISectorUiState uiState, ISectorNode playerActorSectorNode)
+        {
+            var nextMoveNodes = playerActorSectorNode.Sector.Map.GetNext(uiState.ActiveActor.Actor.Node);
+            var actorFow = uiState.ActiveActor.Actor.Person.GetModule<IFowData>();
+            var fowNodes = actorFow.GetSectorFowData(playerActorSectorNode.Sector)
+                .Nodes.Where(x => x.State == SectorMapNodeFowState.Observing)
+                .Select(x => x.Node);
+
+            var fowNodesAll = actorFow.GetSectorFowData(playerActorSectorNode.Sector)
+                .Nodes.Where(x =>
+                    x.State == SectorMapNodeFowState.Explored || x.State == SectorMapNodeFowState.Observing)
+                .Select(x => x.Node);
+
+            Console.WriteLine($"{UiResource.NodesLabel}:");
+            Console.WriteLine();
+            foreach (var node in fowNodes)
+            {
+                Console.Write(node);
+
+                if (nextMoveNodes.Contains(node))
+                {
+                    Console.Write($" {UiResource.NextNodeMarker}");
+                }
+
+                if (playerActorSectorNode.Sector.Map.Transitions.TryGetValue(node, out var _))
+                {
+                    Console.Write($" {UiResource.TransitionNodeMarker}");
+                }
+
+                var undiscoveredNodes = playerActorSectorNode.Sector.Map.GetNext(node)
+                    .Where(x => !fowNodesAll.Contains(x));
+                if (undiscoveredNodes.Any())
+                {
+                    Console.Write($" {UiResource.UndiscaveredNodeMarker}");
+                }
+
+                var monsterInNode =
+                    playerActorSectorNode.Sector.ActorManager.Items.SingleOrDefault(x => x.Node == node);
+                if (monsterInNode != null && monsterInNode != uiState.ActiveActor.Actor)
+                {
+                    Console.Write(
+                        $" {UiResource.MonsterNodeMarker} {monsterInNode.Person.Id}:{monsterInNode.Person}");
+                }
+
+                var staticObjectInNode =
+                    playerActorSectorNode.Sector.StaticObjectManager.Items.SingleOrDefault(x => x.Node == node);
+                if (staticObjectInNode != null)
+                {
+                    Console.Write(
+                        $" {UiResource.StaticObjectNodeMarker} {staticObjectInNode.Id}:{staticObjectInNode.Purpose}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void HandleMoveCommand(IServiceScope serviceScope, ISectorUiState uiState,
+            ISectorNode playerActorSectorNode, string inputText)
+        {
+            var components = inputText.Split(' ');
+            var x = int.Parse(components[1], CultureInfo.InvariantCulture);
+            var y = int.Parse(components[2], CultureInfo.InvariantCulture);
+            var offsetCoords = new OffsetCoords(x, y);
+
+            ISectorMap map = playerActorSectorNode.Sector.Map;
+
+            var targetNode = map.Nodes.OfType<HexNode>()
+                .SingleOrDefault(node => node.OffsetCoords == offsetCoords);
+
+            var command = serviceScope.ServiceProvider.GetRequiredService<MoveCommand>();
+
+            uiState.SelectedViewModel = new NodeViewModel { Node = targetNode };
+
+            if (command.CanExecute())
+            {
+                command.Execute();
+            }
+            else
+            {
+                Console.WriteLine(UiResource.CommandCantExecuteMessage);
+            }
+        }
+
+        private static void HandleSectorTransitCommand(IServiceScope serviceScope)
+        {
+            var command = serviceScope.ServiceProvider.GetRequiredService<SectorTransitionMoveCommand>();
+            command.Execute();
+        }
+
+        private static void PrintCommandDescriptions()
+        {
+            Console.WriteLine(UiResource.MoveCommandDescription);
+            Console.WriteLine(UiResource.TransitionCommandDescription);
+            Console.WriteLine(UiResource.LookCommandDescription);
+            Console.WriteLine(UiResource.IdleCommandDescription);
+            Console.WriteLine(UiResource.DeadCommandDescription);
+            Console.WriteLine(UiResource.ExitCommandDescription);
+        }
+
         private static void PrintState(IActor actor)
         {
             Console.WriteLine(new string('=', 10));
@@ -103,132 +230,6 @@ namespace Zilon.TextClient
 
             cancellationTokenSource.Cancel();
             return Task.FromResult(GameScreen.Scores);
-        }
-
-        private static ISectorNode GetPlayerSectorNode(IPlayer player, IGlobe globe)
-        {
-            return (from sectorNode in globe.SectorNodes
-                    from actor in sectorNode.Sector.ActorManager.Items
-                    where actor.Person == player.MainPerson
-                    select sectorNode).SingleOrDefault();
-        }
-
-        private static void HandleSectorTransitCommand(IServiceScope serviceScope)
-        {
-            var command = serviceScope.ServiceProvider.GetRequiredService<SectorTransitionMoveCommand>();
-            command.Execute();
-        }
-
-        private static void HandleDeadCommand(IPlayer player)
-        {
-            var survivalModule = player.MainPerson.GetModule<ISurvivalModule>();
-            survivalModule.SetStatForce(Core.Persons.SurvivalStatType.Health, 0);
-        }
-
-        private static void HandleIdleCommand(IServiceScope serviceScope)
-        {
-            var command = serviceScope.ServiceProvider.GetRequiredService<IdleCommand>();
-
-            if (command.CanExecute())
-            {
-                command.Execute();
-            }
-            else
-            {
-                Console.WriteLine(UiResource.CommandCantExecuteMessage);
-            }
-        }
-
-        private static void HandleLookCommand(ISectorUiState uiState, ISectorNode playerActorSectorNode)
-        {
-            var nextMoveNodes = playerActorSectorNode.Sector.Map.GetNext(uiState.ActiveActor.Actor.Node);
-            var actorFow = uiState.ActiveActor.Actor.Person.GetModule<IFowData>();
-            var fowNodes = actorFow.GetSectorFowData(playerActorSectorNode.Sector)
-                .Nodes.Where(x => x.State == SectorMapNodeFowState.Observing)
-                .Select(x => x.Node);
-
-            var fowNodesAll = actorFow.GetSectorFowData(playerActorSectorNode.Sector)
-                .Nodes.Where(x =>
-                    x.State == SectorMapNodeFowState.Explored || x.State == SectorMapNodeFowState.Observing)
-                .Select(x => x.Node);
-
-            Console.WriteLine($"{UiResource.NodesLabel}:");
-            Console.WriteLine();
-            foreach (var node in fowNodes)
-            {
-                Console.Write(node);
-
-                if (nextMoveNodes.Contains(node))
-                {
-                    Console.Write($" {UiResource.NextNodeMarker}");
-                }
-
-                if (playerActorSectorNode.Sector.Map.Transitions.TryGetValue(node, out var _))
-                {
-                    Console.Write($" {UiResource.TransitionNodeMarker}");
-                }
-
-                var undiscoveredNodes = playerActorSectorNode.Sector.Map.GetNext(node)
-                    .Where(x => !fowNodesAll.Contains(x));
-                if (undiscoveredNodes.Any())
-                {
-                    Console.Write($" {UiResource.UndiscaveredNodeMarker}");
-                }
-
-                var monsterInNode =
-                    playerActorSectorNode.Sector.ActorManager.Items.SingleOrDefault(x => x.Node == node);
-                if (monsterInNode != null && monsterInNode != uiState.ActiveActor.Actor)
-                {
-                    Console.Write(
-                        $" {UiResource.MonsterNodeMarker} {monsterInNode.Person.Id}:{monsterInNode.Person}");
-                }
-
-                var staticObjectInNode =
-                    playerActorSectorNode.Sector.StaticObjectManager.Items.SingleOrDefault(x => x.Node == node);
-                if (staticObjectInNode != null)
-                {
-                    Console.Write(
-                        $" {UiResource.StaticObjectNodeMarker} {staticObjectInNode.Id}:{staticObjectInNode.Purpose}");
-                }
-
-                Console.WriteLine();
-            }
-        }
-
-        private static void HandleMoveCommand(IServiceScope serviceScope, ISectorUiState uiState, ISectorNode playerActorSectorNode, string inputText)
-        {
-            var components = inputText.Split(' ');
-            var x = int.Parse(components[1], CultureInfo.InvariantCulture);
-            var y = int.Parse(components[2], CultureInfo.InvariantCulture);
-            var offsetCoords = new OffsetCoords(x, y);
-
-            ISectorMap map = playerActorSectorNode.Sector.Map;
-
-            var targetNode = map.Nodes.OfType<HexNode>()
-                .SingleOrDefault(node => node.OffsetCoords == offsetCoords);
-
-            var command = serviceScope.ServiceProvider.GetRequiredService<MoveCommand>();
-
-            uiState.SelectedViewModel = new NodeViewModel { Node = targetNode };
-
-            if (command.CanExecute())
-            {
-                command.Execute();
-            }
-            else
-            {
-                Console.WriteLine(UiResource.CommandCantExecuteMessage);
-            }
-        }
-
-        private static void PrintCommandDescriptions()
-        {
-            Console.WriteLine(UiResource.MoveCommandDescription);
-            Console.WriteLine(UiResource.TransitionCommandDescription);
-            Console.WriteLine(UiResource.LookCommandDescription);
-            Console.WriteLine(UiResource.IdleCommandDescription);
-            Console.WriteLine(UiResource.DeadCommandDescription);
-            Console.WriteLine(UiResource.ExitCommandDescription);
         }
     }
 }
