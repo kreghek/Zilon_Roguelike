b7e104058ef396896172b772e38251fe1435ef7b
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-07 00:16:50 +0700

    Implementation of task source to autoplay

--- Zilon.Core/Zilon.Core/Tactics/Behaviour/SwitchHumanActorTaskSource.cs
+++ Zilon.Core/Zilon.Core/Tactics/Behaviour/SwitchHumanActorTaskSource.cs
@@ -10,13 +10,18 @@ namespace Zilon.Core.Tactics.Behaviour
         Bot = 2
     }
 
-    public class SwitchHumanActorTaskSource<TContext> : IHumanActorTaskSource<TContext>
+    public interface IActorTaskControlSwitcher
+    {
+        void Switch(ActorTaskSourceControl taskSourceControl);
+    }
+
+    public class SwitchHumanActorTaskSource<TContext> : IActorTaskControlSwitcher, IHumanActorTaskSource<TContext>
         where TContext : ISectorTaskSourceContext
     {
         private readonly IActorTaskSource<TContext> _botActorTaskContext;
         private readonly IHumanActorTaskSource<TContext> _humanActorTaskSource;
 
-        private readonly ActorTaskSourceControl _taskSourceControl;
+        private ActorTaskSourceControl _taskSourceControl;
 
         public SwitchHumanActorTaskSource(IHumanActorTaskSource<TContext> humanActorTaskSource,
             IActorTaskSource<TContext> botActorTaskContext)
@@ -53,29 +58,106 @@ namespace Zilon.Core.Tactics.Behaviour
             throw new NotImplementedException();
         }
 
-        public Task<IActorTask> GetActorTaskAsync(IActor actor, TContext context)
+        public void DropIntentionWaiting()
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<IActorTask> GetActorTaskAsync(IActor actor, TContext context)
+        {
+            switch (_taskSourceControl)
+            {
+                case ActorTaskSourceControl.Human:
+                    return await _humanActorTaskSource.GetActorTaskAsync(actor, context).ConfigureAwait(false);
+
+                case ActorTaskSourceControl.Bot:
+                    return await _botActorTaskContext.GetActorTaskAsync(actor, context).ConfigureAwait(false);
+
+                case ActorTaskSourceControl.Undefined:
+                    //TODO Add common TaskSourceException
+                    throw new InvalidOperationException("Task source is undefined.");
+                default:
+                    throw new InvalidOperationException($"Task source {_taskSourceControl} is unknown.");
+            }
         }
 
         public void Intent(IIntention intention, IActor activeActor)
         {
-            throw new NotImplementedException();
+            if (_taskSourceControl == ActorTaskSourceControl.Human)
+            {
+                _humanActorTaskSource.Intent(intention, activeActor);
+            }
+            else
+            {
+                throw new InvalidOperationException("Intension available only under human control.");
+            }
         }
 
-        public Task IntentAsync(IIntention intention, IActor activeActor)
+        public async Task IntentAsync(IIntention intention, IActor activeActor)
         {
-            throw new NotImplementedException();
+            if (_taskSourceControl == ActorTaskSourceControl.Human)
+            {
+                await _humanActorTaskSource.IntentAsync(intention, activeActor);
+            }
+            else
+            {
+                throw new InvalidOperationException("Intension available only under human control.");
+            }
         }
 
         public void ProcessTaskComplete(IActorTask actorTask)
         {
-            throw new NotImplementedException();
+            switch (_taskSourceControl)
+            {
+                case ActorTaskSourceControl.Human:
+                    _humanActorTaskSource.ProcessTaskComplete(actorTask);
+                    break;
+
+                case ActorTaskSourceControl.Bot:
+                    _botActorTaskContext.ProcessTaskComplete(actorTask);
+                    break;
+
+                case ActorTaskSourceControl.Undefined:
+                    //TODO Add common TaskSourceException
+                    throw new InvalidOperationException("Task source is undefined.");
+                default:
+                    throw new InvalidOperationException($"Task source {_taskSourceControl} is unknown.");
+            }
         }
 
         public void ProcessTaskExecuted(IActorTask actorTask)
         {
-            throw new NotImplementedException();
+            switch (_taskSourceControl)
+            {
+                case ActorTaskSourceControl.Human:
+                    _humanActorTaskSource.ProcessTaskExecuted(actorTask);
+                    break;
+
+                case ActorTaskSourceControl.Bot:
+                    _botActorTaskContext.ProcessTaskExecuted(actorTask);
+                    break;
+
+                case ActorTaskSourceControl.Undefined:
+                    //TODO Add common TaskSourceException
+                    throw new InvalidOperationException("Task source is undefined.");
+                default:
+                    throw new InvalidOperationException($"Task source {_taskSourceControl} is unknown.");
+            }
+        }
+
+        public void Switch(ActorTaskSourceControl taskSourceControl)
+        {
+            switch (taskSourceControl)
+            {
+                case ActorTaskSourceControl.Undefined:
+                default:
+                    throw new ArgumentException($"Invalid value {taskSourceControl}", nameof(taskSourceControl));
+
+                case ActorTaskSourceControl.Human:
+                case ActorTaskSourceControl.Bot:
+                    _taskSourceControl = taskSourceControl;
+                    break;
+            }
         }
     }
 }
