2ded63bf6cd9e84ef0ceaee470ba5c109698f181
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-06 20:13:06 +0700

    Split identifier generation on different documents

--- Zilon.Core/Zilon.Core/PersonGeneration/IMonsterIdentifierGenerator.cs
+++ Zilon.Core/Zilon.Core/PersonGeneration/IMonsterIdentifierGenerator.cs
@@ -1,19 +1,14 @@
 ﻿namespace Zilon.Core.PersonGeneration
 {
+    /// <summary>
+    /// Provider for monster indentifier to select monster without graphic clients. Used by tests and the text game.
+    /// </summary>
     public interface IMonsterIdentifierGenerator
     {
+        /// <summary>
+        /// Get new identifier for monster.
+        /// </summary>
+        /// <returns>New unique identifier.</returns>
         int GetNewId();
-    }
-
-    public sealed class MonsterIdentifierGenerator : IMonsterIdentifierGenerator
-    {
-        private int _currentIdCounter;
-
-        public int GetNewId()
-        {
-            _currentIdCounter++;
-
-            return _currentIdCounter;
-        }
     }
 }
