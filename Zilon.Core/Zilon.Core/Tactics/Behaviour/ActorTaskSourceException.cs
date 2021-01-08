0c11b8a4b8bd58aad28a4d23ed63e86076492b25
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-08 05:58:18 +0000

    [Bot] Automated ReSharper CLT update

--- Zilon.Core/Zilon.Core/Tactics/Behaviour/ActorTaskSourceException.cs
+++ Zilon.Core/Zilon.Core/Tactics/Behaviour/ActorTaskSourceException.cs
@@ -8,8 +8,11 @@ namespace Zilon.Core.Tactics.Behaviour
         public ActorTaskSourceException() { }
         public ActorTaskSourceException(string message) : base(message) { }
         public ActorTaskSourceException(string message, Exception inner) : base(message, inner) { }
+
         protected ActorTaskSourceException(
-          System.Runtime.Serialization.SerializationInfo info,
-          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
