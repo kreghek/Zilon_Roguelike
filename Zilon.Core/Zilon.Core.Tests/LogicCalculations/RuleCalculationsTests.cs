a663e318c01ec840c406bffdc6cd9e19a96612eb
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-10 15:50:28 +0700

    Add RuleCalculationsTests to test exceptions

---
+++ Zilon.Core/Zilon.Core.Tests/LogicCalculations/RuleCalculationsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using Zilon.Core.Components;
+
+namespace Zilon.Core.LogicCalculations.Tests
+{
+    [TestFixture]
+    public class RuleCalculationsTests
+    {
+        /// <summary>
+        /// Test checks no exceptions in result of adding new levels.
+        /// </summary>
+        [Test]
+        [TestCaseSource(typeof(RuleCalculationsTestCaseSource), nameof(RuleCalculationsTestCaseSource.AllKnownLevels))]
+        public void CalcEfficientByRuleLevel_AllKnownLevels_NotThrowException(int currentModifier, PersonRuleLevel level)
+        {
+            // ACT
+            Action act = () =>
+            {
+                var _ = RuleCalculations.CalcEfficientByRuleLevel(currentModifier, level);
+            };
+
+            // ASSERT
+            act.Should().NotThrow<NotSupportedException>();
+        }
+    }
+}
