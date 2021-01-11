de5fd3b82b5cce09bd61218851b636bb1284c18e
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-11 22:30:52 +0700

    Refactor path visualizer

--- Zilon.Client/Assets/Zilon/Scripts/Models/SectorScene/MovePathVisualizer.cs
+++ Zilon.Client/Assets/Zilon/Scripts/Models/SectorScene/MovePathVisualizer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 using UnityEngine;
 
@@ -22,29 +23,60 @@ public class MovePathVisualizer : MonoBehaviour
 
     public void Update()
     {
+        if (!(_playerState.HoverViewModel is IMapNodeViewModel))
+        {
+            ClearPreviousPath();
+            return;
+        }
+
         var moveCommand = (MoveCommand)_moveCommand;
+        var canMove = _moveCommand.CanExecute();
+
+        if (!canMove)
+        {
+            ClearPreviousPath();
+            return;
+        }
+
         var path = moveCommand.Path;
 
+        if (_lastPath == path)
+        {
+            // If last and current are equals just do nothing.
+            // Because path is not changed so current visualization is correct.
+            return;
+        }
+
+        _lastPath = path;
+
+        if (path == null || path.Count <= 1)
+        {
+            // First element of the path is the cell with the player person.
+            // If path is null or path length equals 1 then just clear current path visualization.
+
+            ClearPreviousPath();
+
+            return;
+        }
+
+        ClearPreviousPath();
+        var pathToVisualize = path.Skip(1);
+
+        foreach (var pathNode in pathToVisualize)
+        {
+            var hexPathNode = (HexNode)pathNode;
+            var worldPosition = HexHelper.ConvertToWorld(hexPathNode.OffsetCoords);
+
+            var item = Instantiate(VisualizationItemPrefab, transform);
+            item.transform.position = new Vector3(worldPosition[0], worldPosition[1] / 2);
+        }
+    }
+
+    private void ClearPreviousPath()
+    {
         foreach (Transform visualizationItem in transform)
         {
             Destroy(visualizationItem.gameObject);
-        }
-
-        if (_playerState.HoverViewModel is IMapNodeViewModel)
-        {
-            _lastPath = path;
-
-            if (_lastPath != null)
-            {
-                foreach (var pathNode in _lastPath)
-                {
-                    var hexPathNode = (HexNode)pathNode;
-                    var worldPosition = HexHelper.ConvertToWorld(hexPathNode.OffsetCoords);
-
-                    var item = Instantiate(VisualizationItemPrefab, transform);
-                    item.transform.position = new Vector3(worldPosition[0], worldPosition[1] / 2);
-                }
-            }
         }
     }
 }
