93129b6bc0dae16b59eb40c7dddad0f2440cdaf9
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-12 11:40:53 +0000

    [Bot] Automated ReSharper CLT update

--- Zilon.Core/Zilon.Bot.Players.Tests/Logics/ExploreLogicStateTests.cs
+++ Zilon.Core/Zilon.Bot.Players.Tests/Logics/ExploreLogicStateTests.cs
@@ -11,6 +11,7 @@ using NUnit.Framework;
 
 using Zilon.Bot.Players.Logics;
 using Zilon.Core.Graphs;
+using Zilon.Core.MapGenerators;
 using Zilon.Core.PersonModules;
 using Zilon.Core.Persons;
 using Zilon.Core.Tactics;
@@ -30,28 +31,37 @@ namespace Zilon.Bot.Players.Logics.Tests
 
             var mapNodes = new[] { Mock.Of<IGraphNode>(), Mock.Of<IGraphNode>() };
 
-            var decisionSource = Mock.Of<IDecisionSource>(x => x.SelectTargetRoamingNode(It.IsAny<IEnumerable<IGraphNode>>()) == mapNodes.First());
+            var decisionSource = Mock.Of<IDecisionSource>(x =>
+                x.SelectTargetRoamingNode(It.IsAny<IEnumerable<IGraphNode>>()) == mapNodes.First());
 
             var logic = new ExploreLogicState(decisionSource);
 
             var actor = Mock.Of<IActor>(actor => actor.Person == Mock.Of<IPerson>(
-                person => person.HasModule(nameof(IFowData)) == true &&
-                    person.GetModule<IFowData>(nameof(IFowData)) == Mock.Of<IFowData>(
-                        fowModule => fowModule.GetSectorFowData(It.IsAny<ISector>()) == Mock.Of<ISectorFowData>(
-                            sectorFowData => sectorFowData.GetFowNodeByState(SectorMapNodeFowState.Explored) == Array.Empty<SectorMapFowNode>().AsEnumerable()
-                            )
-                )) &&
-                actor.Node == mapNodes.First()
-           );
+                                                     person => person.HasModule(nameof(IFowData)) == true &&
+                                                               person.GetModule<IFowData>(nameof(IFowData)) ==
+                                                               Mock.Of<IFowData>(
+                                                                   fowModule =>
+                                                                       fowModule.GetSectorFowData(
+                                                                           It.IsAny<ISector>()) ==
+                                                                       Mock.Of<ISectorFowData>(
+                                                                           sectorFowData =>
+                                                                               sectorFowData.GetFowNodeByState(
+                                                                                   SectorMapNodeFowState.Explored) ==
+                                                                               Array.Empty<SectorMapFowNode>()
+                                                                                   .AsEnumerable()
+                                                                       )
+                                                               )) &&
+                                                 actor.Node == mapNodes.First()
+            );
 
             var context = Mock.Of<ISectorTaskSourceContext>(x => x.Sector == Mock.Of<ISector>(
                 sector => sector.Map == Mock.Of<ISectorMap>(
                     map => map.Nodes == mapNodes &&
-                        map.Transitions == new Dictionary<IGraphNode, Core.MapGenerators.SectorTransition>() &&
-                        map.GetNext(It.IsAny<IGraphNode>()) == mapNodes.Skip(1) &&
-                        map.IsPositionAvailableFor(It.IsAny<IGraphNode>(), It.IsAny<IActor>()) == true
-                    )
-                ));
+                           map.Transitions == new Dictionary<IGraphNode, SectorTransition>() &&
+                           map.GetNext(It.IsAny<IGraphNode>()) == mapNodes.Skip(1) &&
+                           map.IsPositionAvailableFor(It.IsAny<IGraphNode>(), It.IsAny<IActor>()) == true
+                )
+            ));
 
             var strategyData = Mock.Of<ILogicStrategyData>();
 
