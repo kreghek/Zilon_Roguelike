764abcce201b4af83dd90833b0a57a7a3c1634c7
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-12 14:14:17 +0700

    Add tests to DeathReasonService

---
+++ Zilon.Core/Zilon.Core.Tests/ScoreResultGenerating/DeathReasonServiceTestCaseSource.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+using Moq;
+
+using NUnit.Framework;
+
+using Zilon.Core.Localization;
+using Zilon.Core.Persons;
+using Zilon.Core.Schemes;
+using Zilon.Core.Tactics;
+using Zilon.Core.Tests.Common;
+
+namespace Zilon.Core.ScoreResultGenerating.Tests
+{
+    public static class DeathReasonServiceTestCaseSource
+    { 
+        public static IEnumerable TestCases {
+            get {
+                var languages = Enum.GetValues(typeof(Language)).Cast<Language>().Where(x => x != Language.Undefined).ToArray();
+                var effectTypes = new[] { SurvivalStatType.Hydration, SurvivalStatType.Intoxication, SurvivalStatType.Satiety };
+
+                var schemeService = CreateSchemeService();
+                var monsterSchemes = schemeService.GetSchemes<IMonsterScheme>();
+
+                foreach (var language in languages)
+                {
+                    foreach (var effectType in effectTypes)
+                    {
+                        var effect = new SurvivalStatHazardEffect(
+                            effectType,
+                            SurvivalStatHazardLevel.Max,
+                            Mock.Of<ISurvivalRandomSource>());
+                        var playerEvent = new SurvivalEffectDamageEvent(effect);
+                        yield return new TestCaseData(playerEvent, language);
+                    }
+
+                    foreach (var monsterScheme in monsterSchemes)
+                    {
+                        if (monsterScheme.Sid == "default")
+                        {
+                            continue;
+                        }
+
+                        var act = Mock.Of<ITacticalAct>();
+                        var monster = new MonsterPerson(monsterScheme);
+                        var monsterActor = Mock.Of<IActor>(x => x.Person == monster);
+                        var playerEvent = new PlayerDamagedEvent(act, monsterActor);
+
+                        yield return new TestCaseData(playerEvent, language);
+                    }
+                }
+            }
+        }
+
+        private static ISchemeService CreateSchemeService()
+        {
+            var schemeLocator = FileSchemeLocator.CreateFromEnvVariable();
+
+            var schemeHandlerFactory = new StrictSchemeServiceHandlerFactory(schemeLocator);
+
+            return new SchemeService(schemeHandlerFactory);
+        }
+    }
+}
---
+++ Zilon.Core/Zilon.Core.Tests/ScoreResultGenerating/DeathReasonServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+using Zilon.Core.Localization;
+using Zilon.Core.Scoring;
+
+namespace Zilon.Core.ScoreResultGenerating.Tests
+{
+    [TestFixture()]
+    [Parallelizable(ParallelScope.All)]
+    public class DeathReasonServiceTests
+    {
+        [Test]
+        [TestCaseSource(typeof(DeathReasonServiceTestCaseSource), nameof(DeathReasonServiceTestCaseSource.TestCases))]
+        public void GetDeathReasonSummaryTest(IPlayerEvent playerEvent, Language language)
+        {
+            // ARRANGE
+
+            var service = new DeathReasonService();
+
+            // ACT
+            var fact = service.GetDeathReasonSummary(playerEvent, language);
+
+            // ASSERT
+            fact.Should().NotBeNullOrEmpty();
+        }
+    }
+}
