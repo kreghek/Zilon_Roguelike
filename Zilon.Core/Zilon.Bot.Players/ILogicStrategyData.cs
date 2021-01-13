a408f8cbf53122cea7a4f75f6aac8017f219af05
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-13 02:52:48 +0000

    [Bot] Automated ReSharper CLT update

--- Zilon.Core/Zilon.Bot.Players/ILogicStrategyData.cs
+++ Zilon.Core/Zilon.Bot.Players/ILogicStrategyData.cs
@@ -24,12 +24,12 @@ namespace Zilon.Bot.Players
         Resource ResourceToReduceHazard { get; set; }
 
         /// <summary>
-        /// Beter equipment to slot in <see cref="TargetEquipmentSlotIndex"/>. Both property must be assigned not null values.
+        /// Beter equipment to slot in <see cref="TargetEquipmentSlotIndex" />. Both property must be assigned not null values.
         /// </summary>
         Equipment TargetEquipment { get; set; }
 
         /// <summary>
-        /// Slot for better equipment assigned in <see cref="TargetEquipment"/>. Both property must be assigned not null values.
+        /// Slot for better equipment assigned in <see cref="TargetEquipment" />. Both property must be assigned not null values.
         /// </summary>
         int? TargetEquipmentSlotIndex { get; set; }
 
