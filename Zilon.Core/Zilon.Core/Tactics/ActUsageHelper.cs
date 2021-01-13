daf627656eca62db941801ad922e334927e0c835
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-13 11:27:03 +0000

    [Bot] Automated ReSharper CLT update

--- Zilon.Core/Zilon.Core/Tactics/ActUsageHelper.cs
+++ Zilon.Core/Zilon.Core/Tactics/ActUsageHelper.cs
@@ -5,6 +5,21 @@ namespace Zilon.Core.Tactics
 {
     public static class ActUsageHelper
     {
+        public static bool SectorHasAttackTarget(ISector sector, IAttackTarget target)
+        {
+            switch (target)
+            {
+                case IActor actor:
+                    return SectorHasAttackedActor(sector, actor);
+
+                case IStaticObject staticObject:
+                    return SectorHasAttackedStaticObject(sector, staticObject);
+
+                default:
+                    throw new InvalidOperationException($"Unknown attack target type {target.GetType().FullName}.");
+            }
+        }
+
         private static bool SectorHasAttackedActor(ISector sector, IAttackTarget target)
         {
             if (sector.ActorManager is null)
@@ -25,21 +40,6 @@ namespace Zilon.Core.Tactics
             }
 
             return sector.StaticObjectManager.Items.Any(x => ReferenceEquals(x, target));
-        }
-
-        public static bool SectorHasAttackTarget(ISector sector, IAttackTarget target)
-        {
-            switch (target)
-            {
-                case IActor actor:
-                    return SectorHasAttackedActor(sector, actor);
-
-                case IStaticObject staticObject:
-                    return SectorHasAttackedStaticObject(sector, staticObject);
-
-                default:
-                    throw new InvalidOperationException($"Unknown attack target type {target.GetType().FullName}.");
-            }
         }
     }
 }
