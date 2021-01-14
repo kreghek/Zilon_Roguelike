2988398c18738e8294334aa86ac3fe3ac8055a9d
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-14 04:19:45 +0000

    [Bot] Automated dotnet-format update

--- Zilon.Core/Zilon.Core/Schemes/IPropUseSubScheme.cs
+++ Zilon.Core/Zilon.Core/Schemes/IPropUseSubScheme.cs
@@ -30,7 +30,7 @@
     }
 
     public sealed class UsageRestrictionItem
-    { 
+    {
         public UsageRestrictionType Type { get; }
     }
 
@@ -40,7 +40,7 @@
     }
 
     public enum UsageRestrictionType
-    { 
+    {
         Undefined,
 
         /// <summary>
