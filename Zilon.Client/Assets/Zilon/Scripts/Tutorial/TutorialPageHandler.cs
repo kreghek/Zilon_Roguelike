660ef3fafbfe910590330c674d4811536bfa2d47
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-01-28 22:33:59 +0700

    Add tutorial page localization and handler

---
+++ Zilon.Client/Assets/Zilon/Scripts/Tutorial/TutorialPageHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Assets.Zilon.Scripts.Services;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Zenject;
+
+public class TutorialPageHandler : MonoBehaviour
+{
+    [Inject]
+    private readonly UiSettingService _uiSettingService;
+
+    public Text DescriptionText;
+    public Image DescriptionImage;
+
+    public int PageIndex;
+
+    public void Start()
+    {
+        var currentLanguage = _uiSettingService.CurrentLanguage;
+
+        DescriptionText.text = GetLocalizedText(currentLanguage, $"page{PageIndex}");
+    }
+
+    private static string GetLocalizedText(Language currentLanguage, string mainKey)
+    {
+        string langKey;
+        switch (currentLanguage)
+        {
+            case Language.Undefined:
+                if (Debug.isDebugBuild)
+                {
+                    throw new ArgumentException($"Некоректное значение языка {currentLanguage}.");
+                }
+                else
+                {
+                    langKey = "en";
+                }
+                break;
+
+            case Language.Russian:
+                langKey = "ru";
+                break;
+
+            
+            case Language.English:
+                langKey = "en";
+                break;
+
+            default:
+                langKey = "en";
+                break;
+        }
+        var text = Resources.Load<TextAsset>($@"Tutorial\{mainKey}-{langKey}");
+
+        return text.text;
+    }
+}
