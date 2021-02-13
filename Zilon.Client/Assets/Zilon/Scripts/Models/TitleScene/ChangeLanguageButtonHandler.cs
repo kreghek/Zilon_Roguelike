7d3afb58de2d4f27cf5b81b2d8e05fbbba2cdca4
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-02-13 13:00:25 +0700

    Display localized text on Unity Client

---
+++ Zilon.Client/Assets/Zilon/Scripts/Models/TitleScene/ChangeLanguageButtonHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Assets.Zilon.Scripts.Services;
+
+using UnityEngine;
+
+using Zenject;
+
+public class ChangeLanguageButtonHandler : MonoBehaviour
+{
+    [Inject]
+    private readonly UiSettingService _uiSettingService;
+
+    public void ChangeLanguage()
+    {
+        var currentLanguage = _uiSettingService.CurrentLanguage;
+
+        switch (currentLanguage)
+        {
+            case Language.English:
+                _uiSettingService.CurrentLanguage = Language.Russian;
+                break;
+
+            case Language.Russian:
+                _uiSettingService.CurrentLanguage = Language.English;
+                break;
+
+            case Language.Undefined:
+            default:
+                if (Debug.isDebugBuild)
+                {
+                    throw new ArgumentException($"Incorrect UI language value {currentLanguage}.");
+                }
+
+                _uiSettingService.CurrentLanguage = Language.English;
+                break;
+        }
+    }
+}
