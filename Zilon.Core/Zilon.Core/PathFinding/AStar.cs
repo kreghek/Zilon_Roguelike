63b8a1f92519927c6bf3df1de3cb20fb5692d8e8
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-03-09 08:50:38 +0000

    [Bot] Automated dotnet-format update

--- Zilon.Core/Zilon.Core/PathFinding/AStar.cs
+++ Zilon.Core/Zilon.Core/PathFinding/AStar.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 using Zilon.Core.Graphs;
-
-using System.Diagnostics.CodeAnalysis;
 
 namespace Zilon.Core.PathFinding
 {
