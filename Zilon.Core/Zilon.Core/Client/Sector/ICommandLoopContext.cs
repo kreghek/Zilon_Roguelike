2d95eba37e323946cce4405e6b01052e9a2933ee
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-03-23 16:52:41 +0000

    [Bot] Automated dotnet-format update

--- Zilon.Core/Zilon.Core/Client/Sector/ICommandLoopContext.cs
+++ Zilon.Core/Zilon.Core/Client/Sector/ICommandLoopContext.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace Zilon.Core.Client.Sector
 {
     public interface ICommandLoopContext
-    { 
+    {
         bool HasNextIteration { get; }
 
         Task WaitForUpdate(CancellationToken cancellationToken);
