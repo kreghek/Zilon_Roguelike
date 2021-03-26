8e9c5ad80b816d8da5bcafe353fdb74caa4f83aa
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-03-26 15:30:10 +0700

    Add test for repeatable commands

--- Zilon.Core/Zilon.Core.Tests/Client/Sector/CommandLoopUpdaterTests.cs
+++ Zilon.Core/Zilon.Core.Tests/Client/Sector/CommandLoopUpdaterTests.cs
@@ -17,7 +17,7 @@ namespace Zilon.Core.Client.Sector.Tests
     public class CommandLoopUpdaterTests
     {
         /// <summary>
-        /// The test checks the command executes if it in the command pool.
+        /// The test checks the command executes if it is in the command pool.
         /// 1. Start the <see cref="CommandLoopUpdater" />. It can perform some idle iteration without command.
         /// 2. Push the command into the pool. And wait until ICommand.Execute() will be called.
         /// 3. Command processing loop pops the command from pool and executes it.
@@ -62,7 +62,7 @@ namespace Zilon.Core.Client.Sector.Tests
         }
 
         /// <summary>
-        /// The test checks the command executes if it in the command pool.
+        /// The test checks the command executes if it is in the command pool.
         /// 1. Push the command into the pool.
         /// 2. Start the <see cref="CommandLoopUpdater" />. It must takes the command from pool during first iteration.
         /// 3. Wait until Execute() called.
@@ -190,6 +190,55 @@ namespace Zilon.Core.Client.Sector.Tests
             // ASSERT
 
             eventRaised.Should().BeTrue();
+        }
+
+        /// <summary>
+        /// The test checks the command executes twice if it is repeatable and can repeat once.
+        /// Note! Command must be abel to repeate and execute. Do not think that ability to repeat automaticaly means command can execute.
+        /// </summary>
+        [Test]
+        [Timeout(5000)]
+        public async Task StartAsync_CommandIsRepeatableOnce_ExecutesCommandTwice()
+        {
+            // ARRANGE
+
+            var contextMock = new Mock<ICommandLoopContext>();
+            contextMock.SetupGet(x => x.HasNextIteration).Returns(true);
+            contextMock.Setup(x => x.WaitForUpdate(CancellationToken.None)).Returns(Task.CompletedTask);
+            var context = contextMock.Object;
+
+            var tcs = new TaskCompletionSource<bool>();
+            var testTask = tcs.Task;
+
+            var repeatIteration = 0;
+            var commandMock = new Mock<IRepeatableCommand>();
+            commandMock.Setup(x => x.Execute()).Callback(() =>
+            {
+                if (repeatIteration >= 1)
+                {
+                    tcs.SetResult(true);
+                }
+            });
+            commandMock.Setup(x => x.CanRepeat()).Callback(() => { repeatIteration++; }).Returns(() => repeatIteration <= 1);
+
+            var command = commandMock.Object;
+
+            var commandPool = new TestCommandPool();
+            commandPool.Push(command);
+
+            var commandLoopUpdater = new CommandLoopUpdater(context, commandPool);
+
+            // ACT
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            commandLoopUpdater.StartAsync(CancellationToken.None).ConfigureAwait(false);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            await testTask;
+
+            // ASSERT
+
+            commandMock.Verify(x => x.Execute(), Times.Exactly(2));
         }
 
         private sealed class TestCommandPool : ICommandPool
