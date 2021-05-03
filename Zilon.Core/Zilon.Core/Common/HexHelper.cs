37583e927fdfdd7b25b874be21eb233c263fc29f
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-05-03 12:06:26 +0600

    Fix Codacy Issues - [SonarCSharp_S1244]

--- Zilon.Core/Zilon.Core/Common/HexHelper.cs
+++ Zilon.Core/Zilon.Core/Common/HexHelper.cs
@@ -175,7 +175,8 @@ namespace Zilon.Core.Common
             [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
             public bool Equals(AxialCoords other)
             {
-                return Q == other.Q && R == other.R;
+const float EPSILON = 0,000001f;
+                return NearlyEqual(Q, other.Q,EPSILON ) && NearlyEqual( R, other.R, EPSILON) ;
             }
 
             [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
@@ -195,6 +196,21 @@ namespace Zilon.Core.Common
             {
                 return !(left == right);
             }
+private static boolean NearlyEqual(float a, float b, float epsilon) {
+    final float absA = Math.abs(a);
+    final float absB = Math.abs(b);
+    final float diff = Math.abs(a - b);
+
+    if (a == b) { // shortcut, handles infinities
+        return true;
+    } else if (a == 0 || b == 0 || absA + absB < Float.MIN_NORMAL) {
+        // a or b is zero or both are extremely close to it
+        // relative error is less meaningful here
+        return diff < (epsilon * Float.MIN_NORMAL);
+    } else { // use relative error
+        return diff / (absA + absB) < epsilon;
+    }
+}
         }
     }
 }
