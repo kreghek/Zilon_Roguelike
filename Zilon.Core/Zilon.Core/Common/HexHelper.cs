74821ad9a6a241e80dabaec4d6e5f23b81057e71
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-05-03 09:21:09 +0000

    [Bot] Automated ReSharper CLT update

--- Zilon.Core/Zilon.Core/Common/HexHelper.cs
+++ Zilon.Core/Zilon.Core/Common/HexHelper.cs
@@ -208,20 +208,20 @@ namespace Zilon.Core.Common
                 var diff = Math.Abs(a - b);
 
                 if (a.Equals(b))
-                { // shortcut, handles infinities
+                {
+                    // shortcut, handles infinities
                     return true;
                 }
-                else if (a == 0 || b == 0 || absA + absB < MIN_NORMAL)
+
+                if (a == 0 || b == 0 || absA + absB < MIN_NORMAL)
                 {
                     // a or b is zero or both are extremely close to it
                     // relative error is less meaningful here
                     return diff < (epsilon * MIN_NORMAL);
                 }
-                else
-                { // use relative error
-                    return diff / (absA + absB) < epsilon;
-                }
 
+                // use relative error
+                return diff / (absA + absB) < epsilon;
             }
         }
     }
