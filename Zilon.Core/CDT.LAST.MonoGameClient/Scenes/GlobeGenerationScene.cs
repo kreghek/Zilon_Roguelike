d9682ecc70b533c2d296a56ca02976e98c25fcd3
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-05-18 17:37:46 +0700

    Add generate button in generate globe scene

--- Zilon.Core/CDT.LAST.MonoGameClient/Scenes/GlobeGenerationScene.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/Scenes/GlobeGenerationScene.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
+using CDT.LAST.MonoGameClient.Engine;
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -17,6 +19,7 @@ namespace CDT.LAST.MonoGameClient.Scenes
     {
         public static string? _lastError = "";
         private readonly MainScene _mainScene;
+        private readonly Button _generateButton;
         private readonly SpriteBatch _spriteBatch;
         private bool _generationWasStarted;
 
@@ -25,6 +28,55 @@ namespace CDT.LAST.MonoGameClient.Scenes
         {
             _spriteBatch = spriteBatch;
             _mainScene = new MainScene(game, spriteBatch);
+
+            var buttonTexture = game.Content.Load<Texture2D>("Sprites/ui/button");
+            var font = Game.Content.Load<SpriteFont>("Fonts/Main");
+
+            _generateButton = new Button("generate", buttonTexture, font, new Rectangle(150, 150, 100, 20))
+            {
+                Click = GenerateButtonClickHandler
+            };
+        }
+
+        private void GenerateButtonClickHandler()
+        {
+            if (!_generationWasStarted)
+            {
+                _generationWasStarted = true;
+
+                var generateGlobeTask = Task.Run(async () =>
+                {
+                    var serviceScope = ((LivGame)Game).ServiceProvider;
+                    var globeInitializer = serviceScope.GetRequiredService<IGlobeInitializer>();
+                    var globe = await globeInitializer.CreateGlobeAsync("intro").ConfigureAwait(false);
+
+                    if (globe is null)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    var gameLoop = serviceScope.GetRequiredService<IGlobeLoopUpdater>();
+
+                    gameLoop.Start();
+
+                    var commandLoop = serviceScope.GetRequiredService<ICommandLoopUpdater>();
+                    var commandLoopTask = commandLoop.StartAsync(CancellationToken.None);
+                    commandLoopTask.ContinueWith(task => _lastError += task.Exception.ToString(),
+                        TaskContinuationOptions.OnlyOnFaulted);
+                    commandLoopTask.ContinueWith(task => _lastError += "Game loop stopped.",
+                        TaskContinuationOptions.OnlyOnCanceled);
+                });
+
+                generateGlobeTask.ContinueWith(task =>
+                {
+                    TargetScene = _mainScene;
+                }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+                generateGlobeTask.ContinueWith(task =>
+                {
+                    Debug.WriteLine(task.Exception);
+                }, TaskContinuationOptions.OnlyOnFaulted);
+            }
         }
 
         public override void Draw(GameTime gameTime)
@@ -37,6 +89,7 @@ namespace CDT.LAST.MonoGameClient.Scenes
 
             _spriteBatch.DrawString(font, "Генерация мира", new Vector2(100, 100), Color.White);
             _spriteBatch.DrawString(font, _lastError, new Vector2(100, 120), Color.White);
+            _generateButton.Draw(_spriteBatch);
 
             _spriteBatch.End();
         }
@@ -54,46 +107,7 @@ namespace CDT.LAST.MonoGameClient.Scenes
                 Game.Exit();
             }
 
-            if (state.IsKeyDown(Keys.Up))
-            {
-                if (!_generationWasStarted)
-                {
-                    _generationWasStarted = true;
-
-                    var generateGlobeTask = Task.Run(async () =>
-                    {
-                        var serviceScope = ((LivGame)Game).ServiceProvider;
-                        var globeInitializer = serviceScope.GetRequiredService<IGlobeInitializer>();
-                        var globe = await globeInitializer.CreateGlobeAsync("intro").ConfigureAwait(false);
-
-                        if (globe is null)
-                        {
-                            throw new InvalidOperationException();
-                        }
-
-                        var gameLoop = serviceScope.GetRequiredService<IGlobeLoopUpdater>();
-
-                        gameLoop.Start();
-
-                        var commandLoop = serviceScope.GetRequiredService<ICommandLoopUpdater>();
-                        var commandLoopTask = commandLoop.StartAsync(CancellationToken.None);
-                        commandLoopTask.ContinueWith(task => _lastError += task.Exception.ToString(),
-                            TaskContinuationOptions.OnlyOnFaulted);
-                        commandLoopTask.ContinueWith(task => _lastError += "Game loop stopped.",
-                            TaskContinuationOptions.OnlyOnCanceled);
-                    });
-
-                    generateGlobeTask.ContinueWith(task =>
-                    {
-                        TargetScene = _mainScene;
-                    }, TaskContinuationOptions.OnlyOnRanToCompletion);
-
-                    generateGlobeTask.ContinueWith(task =>
-                    {
-                        Debug.WriteLine(task.Exception);
-                    }, TaskContinuationOptions.OnlyOnFaulted);
-                }
-            }
+            _generateButton.Update();
         }
     }
 }
