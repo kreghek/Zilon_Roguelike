abdbc0131115d9875aae59cd9157cfe85c45a8e3
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-05-18 08:24:02 +0000

    [Bot] Automated dotnet-format update

--- Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/IActorGraphics.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/IActorGraphics.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework;
 namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
 {
     public interface IActorGraphics
-    { 
+    {
         SpriteContainer RootSprite { get; }
         Vector2 HitEffectPosition { get; }
     }
