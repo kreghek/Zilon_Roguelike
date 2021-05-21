e753c4f63af76acebe851974794b606187d44952
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-05-21 15:16:31 +0700

    Fix CodeFactor Issues - [Complex Method]

--- Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/SectorViewModelContext.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/SectorViewModelContext.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections.Generic;
 
+using Zilon.Core.Tactics;
+
 namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
 {
     public sealed class SectorViewModelContext
     {
-        public SectorViewModelContext(EffectManager effectManager)
+        public SectorViewModelContext(ISector sector)
         {
+            _sector = sector;
+
             GameObjects = new List<GameObjectBase>();
-            EffectManager = effectManager;
+            EffectManager = new EffectManager();
+        }
+
+        public IEnumerable<IActor> GetActors()
+        {
+            return _sector.ActorManager.Items;
         }
 
         public EffectManager EffectManager { get; }
 
         public List<GameObjectBase> GameObjects { get; }
+
+        private readonly ISector _sector;
     }
 }
