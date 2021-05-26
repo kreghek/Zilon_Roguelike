b30b9b53bea33555c2d81b21efb95b067d2823a4
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-05-26 23:30:18 +0700

    Different sprites for static objects

--- Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/StaticObjectGraphics.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/StaticObjectGraphics.cs
@@ -1,22 +1,57 @@
 ﻿
+using System.Diagnostics;
+
 using CDT.LAST.MonoGameClient.Engine;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
+using Zilon.Core.Tactics;
+
 namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
 {
     internal sealed class StaticObjectGraphics : SpriteContainer
     {
-        public StaticObjectGraphics(Game game)
+        public StaticObjectGraphics(Game game, IStaticObject staticObject)
         {
-            var staticObjectTexture = game.Content.Load<Texture2D>("Sprites/game-objects/environment/Grass");
+            var textureName = GetTextureNameByPurpose(staticObject.Purpose);
+            var staticObjectTexture = game.Content.Load<Texture2D>($"Sprites/game-objects/Environment/{textureName}");
             var staticObjectSprite = new Sprite(staticObjectTexture)
             {
                 Origin = new Vector2(0.5f, 0.75f)
             };
 
             AddChild(staticObjectSprite);
+        }
+
+        private static string GetTextureNameByPurpose(PropContainerPurpose purpose)
+        {
+            switch (purpose)
+            {
+                case PropContainerPurpose.CherryBrush:
+                case PropContainerPurpose.Treasures:
+                    return "Grass";
+
+                case PropContainerPurpose.OreDeposits:
+                    return "StoneBrown";
+
+                case PropContainerPurpose.StoneDeposits:
+                    return "StoneBlue";
+
+                case PropContainerPurpose.TrashHeap:
+                case PropContainerPurpose.Trash:
+                    return "TrashHeap";
+
+                case PropContainerPurpose.Puddle:
+                    return "Puddle";
+
+                case PropContainerPurpose.Pit:
+                    return "Pit";
+
+                default:
+                    Debug.Fail($"Texture for specified purpose {purpose}.");
+                    return "Grass";
+            }
         }
     }
 }
