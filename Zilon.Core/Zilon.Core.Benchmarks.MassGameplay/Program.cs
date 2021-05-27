f8f64ba729fe93e54280754249390812b5c2b551
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-05-27 16:03:00 +0700

    Reduce count of invocation for huge test

--- Zilon.Core/Zilon.Core.Benchmarks.MassGameplay/Program.cs
+++ Zilon.Core/Zilon.Core.Benchmarks.MassGameplay/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 namespace Zilon.Core.Benchmarks.Move
@@ -14,5 +15,12 @@ namespace Zilon.Core.Benchmarks.Move
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 #endif
         }
+
+        static IConfig GetGlobalConfig()
+            => DefaultConfig.Instance
+            .AddJob(Job.Default
+                .WithWarmupCount(1)
+                .WithInvocationCount(10)
+                .AsDefault()); // the KEY to get it working
     }
 }
