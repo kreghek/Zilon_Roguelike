87398c97bc32068cf0dc7f527fbd77666f0d7710
Куркутов П.А <kurkutovpa@tomskasu.ru>
2021-05-27 08:49:19 +0700

    Fix bench

--- Zilon.Core/Zilon.Core.Benchmarks.CreateSector/CreateCellularAutomatonSectorBench.cs
+++ Zilon.Core/Zilon.Core.Benchmarks.CreateSector/CreateCellularAutomatonSectorBench.cs
@@ -1,13 +1,10 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 using BenchmarkDotNet.Attributes;
 
 using Microsoft.Extensions.DependencyInjection;
 
-using Zilon.Core.MapGenerators;
 using Zilon.Core.Schemes;
-using Zilon.Core.Tactics;
 using Zilon.Core.World;
 
 namespace Zilon.Core.Benchmarks.CreateSector
@@ -19,16 +16,12 @@ namespace Zilon.Core.Benchmarks.CreateSector
         [Benchmark(Description = "Create CA Sector")]
         public async Task CreateSectorAsync()
         {
-            var sectorGenerator = _serviceProvider.GetRequiredService<ISectorGenerator>();
             var biomInitializer = _serviceProvider.GetRequiredService<IBiomeInitializer>();
             var schemeService = _serviceProvider.GetRequiredService<ISchemeService>();
 
             var testScheme = schemeService.GetScheme<ILocationScheme>("intro");
 
-            var biom = await biomInitializer.InitBiomeAsync(testScheme);
-            var tesSectorNode = biom.Sectors.First();
-
-            await sectorGenerator.GenerateAsync(tesSectorNode).ConfigureAwait(false);
+            await biomInitializer.InitBiomeAsync(testScheme).ConfigureAwait(false);
         }
 
 
