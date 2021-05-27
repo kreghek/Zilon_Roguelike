7a7f68b4a42df44c86502c9a1e3e68b64e11b7ad
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-05-27 10:45:08 +0000

    [Bot] Automated ReSharper CLT update

--- Zilon.Core/Zilon.Core.Benchmarks.MassGameplay/Program.cs
+++ Zilon.Core/Zilon.Core.Benchmarks.MassGameplay/Program.cs
@@ -6,28 +6,30 @@ namespace Zilon.Core.Benchmarks.Move
 {
     internal static class Program
     {
-        private static void Main(string[] args)
-        {
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args,
-                config: GetSpecifiedConfig());
-        }
-
-        static IConfig GetSpecifiedConfig()
-            => GetConfigInstance()
-            .AddJob(Job.Default
-                .WithWarmupCount(1)
-                .WithInvocationCount(1)
-                .WithUnrollFactor(1)
-                .WithIterationCount(10)
-                .AsDefault()); // the KEY to get it working
-
-        static IConfig GetConfigInstance()
+        private static IConfig GetConfigInstance()
         {
 #if DEBUG
             return new DebugInProcessConfig();
 #else
             return DefaultConfig.Instance;
 #endif
+        }
+
+        private static IConfig GetSpecifiedConfig()
+        {
+            return GetConfigInstance()
+                .AddJob(Job.Default
+                    .WithWarmupCount(1)
+                    .WithInvocationCount(1)
+                    .WithUnrollFactor(1)
+                    .WithIterationCount(10)
+                    .AsDefault()); // the KEY to get it working
+        }
+
+        private static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args,
+                config: GetSpecifiedConfig());
         }
     }
 }
