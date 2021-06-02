5a4e8b3816973b5e274c18340775a5477837e0a6
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-06-02 18:31:49 +0700

    Introduce icons of clothes

--- Zilon.Core/CDT.LAST.MonoGameClient/Screens/UiContentStorage.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/Screens/UiContentStorage.cs
@@ -71,19 +71,19 @@ namespace CDT.LAST.MonoGameClient.Screens
             _propIcons.Add("short-sword",
                 new[]
                 {
-                    contentManager.Load<Texture2D>("Sprites/game-objects/Equipments/HandParts/ShortSwordBase")
+                    contentManager.Load<Texture2D>("Sprites/game-objects/Equipments/HandParts/ShortSwordIcon")
                 });
             _propIcons.Add("wooden-shield",
                 new[]
                 {
-                    contentManager.Load<Texture2D>("Sprites/game-objects/Equipments/HandParts/WoodenShieldBase")
+                    contentManager.Load<Texture2D>("Sprites/game-objects/Equipments/HandParts/WoodenShieldIcon")
                 });
             _propIcons.Add("work-clothes",
-                new[] { contentManager.Load<Texture2D>("Sprites/game-objects/Equipments/BodyParts/WorkClothes/Body") });
+                new[] { contentManager.Load<Texture2D>("Sprites/game-objects/Equipments/BodyParts/WorkClothes/Icon") });
             _propIcons.Add("traveler-camisole",
                 new[]
                 {
-                    contentManager.Load<Texture2D>("Sprites/game-objects/Equipments/BodyParts/TravelerCamisole/Body")
+                    contentManager.Load<Texture2D>("Sprites/game-objects/Equipments/BodyParts/TravelerCamisole/Icon")
                 });
             _propIcons.Add("EmptyPropIcon", new[] { contentManager.Load<Texture2D>("Sprites/ui/EmptyPropIcon") });
         }
