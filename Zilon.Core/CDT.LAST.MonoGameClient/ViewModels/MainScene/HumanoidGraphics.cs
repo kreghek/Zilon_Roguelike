e06b5cdf547311d23f21265e07b101c821a404e4
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-06-02 08:24:28 +0000

    [Bot] Automated ReSharper CLT update

--- Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/HumanoidGraphics.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/HumanoidGraphics.cs
@@ -10,6 +10,7 @@ using Microsoft.Xna.Framework;
 using Zilon.Core.Common;
 using Zilon.Core.PersonModules;
 using Zilon.Core.Persons;
+using Zilon.Core.Props;
 
 namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
 {
@@ -29,6 +30,150 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
             CreateSpriteHierarchy(equipmentModule);
 
             equipmentModule.EquipmentChanged += EquipmentModule_EquipmentChanged;
+        }
+
+        private void AddLeftArmHierarchy()
+        {
+            var humanParts = _personVisualizationContentStorage.GetHumanParts();
+            var armLeftTexture = humanParts.Single(x => x.Type == BodyPartType.ArmLeft).Texture;
+
+            // Slot 1 according the person scheme is body.
+            var dressedLeftArmPart = GetDressedPartAccordingBodySlot(_equipmentModule, BodyPartType.ArmLeft);
+
+            AddChild(new Sprite(armLeftTexture)
+            {
+                Position = new Vector2(-10, -20),
+                Origin = new Vector2(0.5f, 0.5f)
+            });
+
+            if (dressedLeftArmPart != null)
+            {
+                AddChild(new Sprite(dressedLeftArmPart.Texture)
+                {
+                    Position = new Vector2(-10, -20),
+                    Origin = new Vector2(0.5f, 0.5f)
+                });
+            }
+        }
+
+        private void AddLeftShieldHierarchy(Equipment equipment)
+        {
+            var shieldSid = equipment.Scheme.Sid;
+            if (shieldSid == null)
+            {
+                Debug.Fail("There are no scheme without SID. This looks loke some kind of error.");
+                // Do nothing. We can't draw unknown shield.
+                return;
+            }
+
+            var handParts = _personVisualizationContentStorage.GetHandParts(shieldSid);
+            var shieldBackBaseTexture = handParts.Single(x => x.Type == HandPartType.BaseBack).Texture;
+
+            AddChild(new Sprite(shieldBackBaseTexture)
+            {
+                Position = new Vector2(-14, -21),
+                Origin = new Vector2(0.5f, 0.5f)
+            });
+        }
+
+        private void AddLeftWeaponHierarchy(Equipment equipment)
+        {
+            var weaponSid = equipment.Scheme.Sid;
+            if (weaponSid == null)
+            {
+                // Do nothing. We can't draw unknown weapon.
+                Debug.Fail("There are no schemes without SID. Looks like some kind of error.");
+            }
+
+            var handParts = _personVisualizationContentStorage.GetHandParts(weaponSid);
+            var weaponBaseTexture = handParts.Single(x => x.Type == HandPartType.Base).Texture;
+
+            AddChild(new Sprite(weaponBaseTexture)
+            {
+                Position = new Vector2(-14, -21),
+                Origin = new Vector2(0.5f, 0.5f)
+            });
+        }
+
+        private void AddRightArmHierarchy()
+        {
+            var humanParts = _personVisualizationContentStorage.GetHumanParts();
+            var armRightTexture = humanParts.Single(x => x.Type == BodyPartType.ArmRightSimple).Texture;
+
+            var dressedRightHandPart = GetDressedPartAccordingBodySlot(_equipmentModule, BodyPartType.ArmRightSimple);
+
+            AddChild(CreateRightArmSprite(armRightTexture));
+
+            if (dressedRightHandPart != null)
+            {
+                AddChild(CreateRightArmSprite(dressedRightHandPart.Texture));
+            }
+        }
+
+        private void AddRightShieldHierarchy(Equipment equipment)
+        {
+            var shieldSid = equipment.Scheme.Sid;
+            if (shieldSid == null)
+            {
+                Debug.Fail("There are no schemes without SID. So this looks like some kind of error.");
+                return;
+            }
+
+            var handParts = _personVisualizationContentStorage.GetHandParts(shieldSid);
+            var shieldBaseTexture = handParts.Single(x => x.Type == HandPartType.Base).Texture;
+
+            AddChild(new Sprite(shieldBaseTexture)
+            {
+                Position = new Vector2(11, -10),
+                Origin = new Vector2(0.5f, 0.5f)
+            });
+        }
+
+        private void AddRightWeaponHierarchy(Equipment equipment)
+        {
+            var weaponSid = equipment.Scheme.Sid;
+            if (weaponSid == null)
+            {
+                Debug.Fail("There are no schemes without SID. So this looks like some kind of error.");
+                return;
+            }
+
+            var handParts = _personVisualizationContentStorage.GetHandParts(weaponSid);
+            var weaponBaseTexture = handParts.Single(x => x.Type == HandPartType.Base).Texture;
+
+            AddChild(new InvertedFlipXSprite(weaponBaseTexture)
+            {
+                Position = new Vector2(11, -10),
+                Origin = new Vector2(0.5f, 0.5f),
+                Rotation = (float)(-Math.PI / 2)
+            });
+        }
+
+        private static Sprite CreateChestSprite(Microsoft.Xna.Framework.Graphics.Texture2D chestTexture)
+        {
+            return new Sprite(chestTexture)
+            {
+                Position = new Vector2(3, -22),
+                Origin = new Vector2(0.5f, 0.5f)
+            };
+        }
+
+        private static Sprite CreateLegsSprite(Microsoft.Xna.Framework.Graphics.Texture2D legsTexture)
+        {
+            return new Sprite(legsTexture)
+            {
+                Position = new Vector2(0, 0),
+                Origin = new Vector2(0.5f, 0.75f)
+            };
+        }
+
+        private static Sprite CreateRightArmSprite(Microsoft.Xna.Framework.Graphics.Texture2D armRightTexture)
+        {
+            return new Sprite(armRightTexture)
+            {
+                Position = new Vector2(13, -20),
+                Origin = new Vector2(0.5f, 0.5f)
+            };
         }
 
         private void CreateSpriteHierarchy(IEquipmentModule equipmentModule)
@@ -74,15 +219,6 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
             }
         }
 
-        private static Sprite CreateChestSprite(Microsoft.Xna.Framework.Graphics.Texture2D chestTexture)
-        {
-            return new Sprite(chestTexture)
-            {
-                Position = new Vector2(3, -22),
-                Origin = new Vector2(0.5f, 0.5f)
-            };
-        }
-
         private void DrawLeftHand(IEquipmentModule equipmentModule)
         {
             // Slot 3 according the person scheme is second/left hand.
@@ -126,91 +262,6 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
             }
         }
 
-        private void AddLeftShieldHierarchy(Zilon.Core.Props.Equipment equipment)
-        {
-            var shieldSid = equipment.Scheme.Sid;
-            if (shieldSid == null)
-            {
-                Debug.Fail("There are no scheme without SID. This looks loke some kind of error.");
-                // Do nothing. We can't draw unknown shield.
-                return;
-            }
-
-            var handParts = _personVisualizationContentStorage.GetHandParts(shieldSid);
-            var shieldBackBaseTexture = handParts.Single(x => x.Type == HandPartType.BaseBack).Texture;
-
-            AddChild(new Sprite(shieldBackBaseTexture)
-            {
-                Position = new Vector2(-14, -21),
-                Origin = new Vector2(0.5f, 0.5f)
-            });
-        }
-
-        private void AddLeftWeaponHierarchy(Zilon.Core.Props.Equipment equipment)
-        {
-            var weaponSid = equipment.Scheme.Sid;
-            if (weaponSid == null)
-            {
-                // Do nothing. We can't draw unknown weapon.
-                Debug.Fail("There are no schemes without SID. Looks like some kind of error.");
-            }
-
-            var handParts = _personVisualizationContentStorage.GetHandParts(weaponSid);
-            var weaponBaseTexture = handParts.Single(x => x.Type == HandPartType.Base).Texture;
-
-            AddChild(new Sprite(weaponBaseTexture)
-            {
-                Position = new Vector2(-14, -21),
-                Origin = new Vector2(0.5f, 0.5f)
-            });
-        }
-
-        private void AddLeftArmHierarchy()
-        {
-            var humanParts = _personVisualizationContentStorage.GetHumanParts();
-            var armLeftTexture = humanParts.Single(x => x.Type == BodyPartType.ArmLeft).Texture;
-
-            // Slot 1 according the person scheme is body.
-            var dressedLeftArmPart = GetDressedPartAccordingBodySlot(_equipmentModule, BodyPartType.ArmLeft);
-
-            AddChild(new Sprite(armLeftTexture)
-            {
-                Position = new Vector2(-10, -20),
-                Origin = new Vector2(0.5f, 0.5f)
-            });
-
-            if (dressedLeftArmPart != null)
-            {
-                AddChild(new Sprite(dressedLeftArmPart.Texture)
-                {
-                    Position = new Vector2(-10, -20),
-                    Origin = new Vector2(0.5f, 0.5f)
-                });
-            }
-        }
-
-        private BodyPart? GetDressedPartAccordingBodySlot(IEquipmentModule equipmentModule, BodyPartType partType)
-        {
-            // Slot 1 according the person scheme is body.
-            var bodyEquipment = equipmentModule[1];
-            if (bodyEquipment == null)
-            {
-                return null;
-            }
-
-            var bodyEquipmentSid = bodyEquipment.Scheme.Sid;
-            if (bodyEquipmentSid != null)
-            {
-                var equipmentParts = _personVisualizationContentStorage.GetBodyParts(bodyEquipmentSid);
-                return equipmentParts.SingleOrDefault(x => x.Type == partType);
-            }
-            else
-            {
-                Debug.Fail("There are no schemes without SID. So this looks like some kind of error.");
-                return null;
-            }
-        }
-
         private IEnumerable<BodyPart> DrawLegs()
         {
             var humanParts = _personVisualizationContentStorage.GetHumanParts();
@@ -238,15 +289,6 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
             }
 
             return humanParts;
-        }
-
-        private static Sprite CreateLegsSprite(Microsoft.Xna.Framework.Graphics.Texture2D legsTexture)
-        {
-            return new Sprite(legsTexture)
-            {
-                Position = new Vector2(0, 0),
-                Origin = new Vector2(0.5f, 0.75f)
-            };
         }
 
         private void DrawRightHand(IEquipmentModule equipmentModule)
@@ -295,69 +337,6 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
             }
         }
 
-        private void AddRightShieldHierarchy(Zilon.Core.Props.Equipment equipment)
-        {
-            var shieldSid = equipment.Scheme.Sid;
-            if (shieldSid == null)
-            {
-                Debug.Fail("There are no schemes without SID. So this looks like some kind of error.");
-                return;
-            }
-
-            var handParts = _personVisualizationContentStorage.GetHandParts(shieldSid);
-            var shieldBaseTexture = handParts.Single(x => x.Type == HandPartType.Base).Texture;
-
-            AddChild(new Sprite(shieldBaseTexture)
-            {
-                Position = new Vector2(11, -10),
-                Origin = new Vector2(0.5f, 0.5f)
-            });
-        }
-
-        private void AddRightWeaponHierarchy(Zilon.Core.Props.Equipment equipment)
-        {
-            var weaponSid = equipment.Scheme.Sid;
-            if (weaponSid == null)
-            {
-                Debug.Fail("There are no schemes without SID. So this looks like some kind of error.");
-                return;
-            }
-
-            var handParts = _personVisualizationContentStorage.GetHandParts(weaponSid);
-            var weaponBaseTexture = handParts.Single(x => x.Type == HandPartType.Base).Texture;
-
-            AddChild(new InvertedFlipXSprite(weaponBaseTexture)
-            {
-                Position = new Vector2(11, -10),
-                Origin = new Vector2(0.5f, 0.5f),
-                Rotation = (float)(-Math.PI / 2)
-            });
-        }
-
-        private void AddRightArmHierarchy()
-        {
-            var humanParts = _personVisualizationContentStorage.GetHumanParts();
-            var armRightTexture = humanParts.Single(x => x.Type == BodyPartType.ArmRightSimple).Texture;
-
-            var dressedRightHandPart = GetDressedPartAccordingBodySlot(_equipmentModule, BodyPartType.ArmRightSimple);
-
-            AddChild(CreateRightArmSprite(armRightTexture));
-
-            if (dressedRightHandPart != null)
-            {
-                AddChild(CreateRightArmSprite(dressedRightHandPart.Texture));
-            }
-        }
-
-        private static Sprite CreateRightArmSprite(Microsoft.Xna.Framework.Graphics.Texture2D armRightTexture)
-        {
-            return new Sprite(armRightTexture)
-            {
-                Position = new Vector2(13, -20),
-                Origin = new Vector2(0.5f, 0.5f)
-            };
-        }
-
         private void EquipmentModule_EquipmentChanged(object? sender, EquipmentChangedEventArgs e)
         {
             var childrenSprites = GetChildren().ToArray();
@@ -367,6 +346,26 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
             }
 
             CreateSpriteHierarchy(_equipmentModule);
+        }
+
+        private BodyPart? GetDressedPartAccordingBodySlot(IEquipmentModule equipmentModule, BodyPartType partType)
+        {
+            // Slot 1 according the person scheme is body.
+            var bodyEquipment = equipmentModule[1];
+            if (bodyEquipment == null)
+            {
+                return null;
+            }
+
+            var bodyEquipmentSid = bodyEquipment.Scheme.Sid;
+            if (bodyEquipmentSid != null)
+            {
+                var equipmentParts = _personVisualizationContentStorage.GetBodyParts(bodyEquipmentSid);
+                return equipmentParts.SingleOrDefault(x => x.Type == partType);
+            }
+
+            Debug.Fail("There are no schemes without SID. So this looks like some kind of error.");
+            return null;
         }
 
         public SpriteContainer RootSprite => this;
