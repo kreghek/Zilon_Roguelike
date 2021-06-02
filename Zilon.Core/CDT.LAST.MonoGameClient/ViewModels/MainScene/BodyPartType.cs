2a9ae3d01f0c750261552a2e97ccb79bedc3a5c4
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-06-02 13:59:45 +0700

    Fix BodyPartType file

--- Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/BodyPartType.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/BodyPartType.cs
@@ -1,15 +1,36 @@
 ﻿namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
 {
+    /// <summary>
+    /// The part of person body visualization available on the client.
+    /// </summary>
     public enum BodyPartType
     {
         Undefined = 0,
         Head,
         Chest,
         ArmLeft,
+        /// <summary>
+        /// Used when:
+        /// - Idle mode.
+        /// - No things in the left hand.
+        /// - One handed thing in the hand.
+        /// </summary>
         ArmRightSimple,
+
+        /// <summary>
+        /// Used to bear some things like rifle.
+        /// </summary>
         ArmRightRifle,
+
+        /// <summary>
+        /// Used to bear two-handed weapon like staffs, grate swords or war hammers.
+        /// </summary>
         ArmRightDual,
         LegsIdle,
+
+        /// <summary>
+        /// Used to visualize combat agressive stance of a person.
+        /// </summary>
         LegsCombat
     }
 }
