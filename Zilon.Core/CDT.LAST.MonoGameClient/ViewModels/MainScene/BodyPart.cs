9429fb8acdc3a832d0d85003f16c6a72802ce00f
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-06-03 12:07:00 +0700

    Separate head visualization's classes in its own documents

--- Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/BodyPart.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/BodyPart.cs
@@ -1,11 +1,10 @@
-﻿using System;
-
+﻿
 using Microsoft.Xna.Framework.Graphics;
 
 namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
 {
     /// <summary>
-    /// Helper structure to keep texture and metadata forbody visualization.
+    /// Helper structure to keep texture and metadata for body visualization.
     /// </summary>
     public record BodyPart
     {
@@ -18,25 +17,5 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene
         public Texture2D Texture { get; }
 
         public BodyPartType Type { get; }
-    }
-
-    public record HeadPart
-    {
-        public HeadPart(HeadPartType type, Texture2D texture)
-        {
-            Texture = texture ?? throw new ArgumentNullException(nameof(texture));
-            Type = type;
-        }
-
-        public Texture2D Texture { get; }
-
-        public HeadPartType Type { get; }
-    }
-
-    public enum HeadPartType
-    {
-        Undefined,
-        Base,
-        Inside
     }
 }
