79a9fbdf11c708e289d876fbd92b6f6f7ca7a76b
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-06-14 05:28:35 +0000

    [Bot] Automated ReSharper CLT update

--- Zilon.Core/CDT.LAST.MonoGameClient/Screens/UiContentStorage.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/Screens/UiContentStorage.cs
@@ -14,20 +14,18 @@ namespace CDT.LAST.MonoGameClient.Screens
     {
         private readonly IDictionary<string, Texture2D[]> _propIcons;
 
-        private IDictionary<SurvivalStatType, Texture2D>? _survivalStatConditionIcons;
-        private Texture2D[]? _conditionIconBackgroundTextures;
-        private Texture2D? _conditionDeseaseSymptomIcon;
-        private Texture2D? _conditionDefaultIcon;
-
-        private Texture2D? _hintBackgorundTexture;
-
         private Texture2D? _attributeIconsTexture;
         private Texture2D? _attributesBackgroundTexture;
         private SpriteFont? _buttonFont;
         private Texture2D? _buttonTexture;
+        private Texture2D? _conditionDefaultIcon;
+        private Texture2D? _conditionDeseaseSymptomIcon;
+        private Texture2D[]? _conditionIconBackgroundTextures;
         private Texture2D? _contextualMenuBorderTexture;
         private Texture2D? _contextualMenuItemBackgroundTexture;
         private SpriteFont? _contextualMenuItemFont;
+
+        private Texture2D? _hintBackgorundTexture;
         private SpriteFont? _hintTitleFont;
         private Texture2D[]? _modalBottomTextures;
         private Texture2D? _modalShadowTexture;
@@ -35,9 +33,132 @@ namespace CDT.LAST.MonoGameClient.Screens
         private Texture2D? _smallVerticalButtonBackgroundTexture;
         private Texture2D? _smallVerticalButtonIconsTexture;
 
+        private IDictionary<SurvivalStatType, Texture2D>? _survivalStatConditionIcons;
+
         public UiContentStorage()
         {
             _propIcons = new Dictionary<string, Texture2D[]>();
+        }
+
+        private static Texture2D GetStatHazardConditionLevelClient(SurvivalStatHazardLevel level,
+            Texture2D[] orderedTextures)
+        {
+            var defaultBackground = orderedTextures[0];
+
+            switch (level)
+            {
+                case SurvivalStatHazardLevel.Lesser:
+                    return orderedTextures[0];
+                case SurvivalStatHazardLevel.Strong:
+                    return orderedTextures[1];
+                case SurvivalStatHazardLevel.Max:
+                    return orderedTextures[2];
+                case SurvivalStatHazardLevel.Undefined:
+                    Debug.Fail("Undefined value can't be assigned.");
+                    return defaultBackground;
+                default:
+                    Debug.Fail($"Value {level} is unknown.");
+                    return defaultBackground;
+            }
+        }
+
+        private Texture2D GetStatHazardConditionTypeClient(SurvivalStatType type)
+        {
+            if (_survivalStatConditionIcons is null)
+            {
+                throw new InvalidOperationException("Survival condition icons is not loaded.");
+            }
+
+            if (!_survivalStatConditionIcons.TryGetValue(type, out var iconTexture))
+            {
+                Debug.Fail("Every stat must has its own icon.");
+            }
+
+            return iconTexture;
+        }
+
+        private void InitConditionIconsAndBackgrounds(ContentManager contentManager)
+        {
+            _survivalStatConditionIcons = new Dictionary<SurvivalStatType, Texture2D>
+            {
+                {
+                    SurvivalStatType.Satiety,
+                    contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/HungerConditionIcon")
+                },
+                {
+                    SurvivalStatType.Hydration,
+                    contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/ThristConditionIcon")
+                },
+                {
+                    SurvivalStatType.Intoxication,
+                    contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/IntoxicationConditionIcon")
+                },
+                {
+                    SurvivalStatType.Health,
+                    contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/InjureConditionIcon")
+                }
+            };
+
+            _conditionIconBackgroundTextures = new[]
+            {
+                contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/ConditionIconLesserBackground"),
+                contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/ConditionIconStrongBackground"),
+                contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/ConditionIconCriticalBackground")
+            };
+
+            _conditionDeseaseSymptomIcon =
+                contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/DiseaseSymptomConditionIcon");
+            _conditionDefaultIcon = contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/DefaultIcon");
+        }
+
+        private void InitPropIcons(ContentManager contentManager)
+        {
+            // Place textures in order to display. Latest will display on the top.
+            _propIcons.Add("short-sword",
+                new[]
+                {
+                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/ShortSword")
+                });
+            _propIcons.Add("great-sword",
+                new[]
+                {
+                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/GreatSword")
+                });
+            _propIcons.Add("combat-staff",
+                new[]
+                {
+                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/CombatStaff")
+                });
+            _propIcons.Add("tribal-spear",
+                new[]
+                {
+                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/Spear")
+                });
+            _propIcons.Add("wooden-shield",
+                new[]
+                {
+                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/WoodenShield")
+                });
+            _propIcons.Add("work-clothes",
+                new[] { contentManager.Load<Texture2D>("Sprites/ui/ClothIcons/WorkClothes") });
+            _propIcons.Add("traveler-camisole",
+                new[]
+                {
+                    contentManager.Load<Texture2D>("Sprites/ui/ClothIcons/TravelerCamisole")
+                });
+            _propIcons.Add("knitted-hat",
+                new[] { contentManager.Load<Texture2D>("Sprites/ui/ClothIcons/KnittedHat") });
+            _propIcons.Add("steel-helmet",
+                new[] { contentManager.Load<Texture2D>("Sprites/ui/ClothIcons/SteelHelmet") });
+
+            _propIcons.Add("med-kit",
+                new[] { contentManager.Load<Texture2D>("Sprites/ui/PropIcons/MedKit") });
+            _propIcons.Add("water-bottle",
+                new[] { contentManager.Load<Texture2D>("Sprites/ui/PropIcons/Waterskin") });
+            _propIcons.Add("packed-food",
+                new[] { contentManager.Load<Texture2D>("Sprites/ui/PropIcons/PackedFood") });
+
+            _propIcons.Add("EmptyPropIcon", new[] { contentManager.Load<Texture2D>("Sprites/ui/EmptyPropIcon") });
         }
 
         public Texture2D GetSmallVerticalButtonBackgroundTexture()
@@ -105,76 +226,6 @@ namespace CDT.LAST.MonoGameClient.Screens
             InitConditionIconsAndBackgrounds(contentManager);
         }
 
-        private void InitConditionIconsAndBackgrounds(ContentManager contentManager)
-        {
-            _survivalStatConditionIcons = new Dictionary<SurvivalStatType, Texture2D>
-            {
-                { SurvivalStatType.Satiety, contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/HungerConditionIcon") },
-                { SurvivalStatType.Hydration, contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/ThristConditionIcon") },
-                { SurvivalStatType.Intoxication, contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/IntoxicationConditionIcon") },
-                { SurvivalStatType.Health, contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/InjureConditionIcon") },
-            };
-
-            _conditionIconBackgroundTextures = new[] {
-                contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/ConditionIconLesserBackground"),
-                contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/ConditionIconStrongBackground"),
-                contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/ConditionIconCriticalBackground")
-            };
-
-            _conditionDeseaseSymptomIcon = contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/DiseaseSymptomConditionIcon");
-            _conditionDefaultIcon = contentManager.Load<Texture2D>("Sprites/ui/PersonConditions/DefaultIcon");
-        }
-
-        private void InitPropIcons(ContentManager contentManager)
-        {
-            // Place textures in order to display. Latest will display on the top.
-            _propIcons.Add("short-sword",
-                new[]
-                {
-                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/ShortSword")
-                });
-            _propIcons.Add("great-sword",
-                new[]
-                {
-                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/GreatSword")
-                });
-            _propIcons.Add("combat-staff",
-                new[]
-                {
-                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/CombatStaff")
-                });
-            _propIcons.Add("tribal-spear",
-                new[]
-                {
-                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/Spear")
-                });
-            _propIcons.Add("wooden-shield",
-                new[]
-                {
-                    contentManager.Load<Texture2D>("Sprites/ui/WeaponIcons/WoodenShield")
-                });
-            _propIcons.Add("work-clothes",
-                new[] { contentManager.Load<Texture2D>("Sprites/ui/ClothIcons/WorkClothes") });
-            _propIcons.Add("traveler-camisole",
-                new[]
-                {
-                    contentManager.Load<Texture2D>("Sprites/ui/ClothIcons/TravelerCamisole")
-                });
-            _propIcons.Add("knitted-hat",
-                new[] { contentManager.Load<Texture2D>("Sprites/ui/ClothIcons/KnittedHat") });
-            _propIcons.Add("steel-helmet",
-                new[] { contentManager.Load<Texture2D>("Sprites/ui/ClothIcons/SteelHelmet") });
-
-            _propIcons.Add("med-kit",
-                new[] { contentManager.Load<Texture2D>("Sprites/ui/PropIcons/MedKit") });
-            _propIcons.Add("water-bottle",
-                new[] { contentManager.Load<Texture2D>("Sprites/ui/PropIcons/Waterskin") });
-            _propIcons.Add("packed-food",
-                new[] { contentManager.Load<Texture2D>("Sprites/ui/PropIcons/PackedFood") });
-
-            _propIcons.Add("EmptyPropIcon", new[] { contentManager.Load<Texture2D>("Sprites/ui/EmptyPropIcon") });
-        }
-
         public SpriteFont GetHintTitleFont()
         {
             return _hintTitleFont ?? throw new InvalidOperationException();
@@ -222,7 +273,8 @@ namespace CDT.LAST.MonoGameClient.Screens
                 case SurvivalStatHazardCondition statCondition:
 
                     var typeTexture = GetStatHazardConditionTypeClient(statCondition.Type);
-                    var levelBackgroundTexture = GetStatHazardConditionLevelClient(statCondition.Level, _conditionIconBackgroundTextures);
+                    var levelBackgroundTexture =
+                        GetStatHazardConditionLevelClient(statCondition.Level, _conditionIconBackgroundTextures);
                     return new PersonConditionTextures(typeTexture, levelBackgroundTexture);
 
                 case DiseaseSymptomCondition:
@@ -232,48 +284,13 @@ namespace CDT.LAST.MonoGameClient.Screens
                         throw new InvalidOperationException("Desease icon is not loaded.");
                     }
 
-                    return new PersonConditionTextures(_conditionDeseaseSymptomIcon, _conditionIconBackgroundTextures[0]);
+                    return new PersonConditionTextures(_conditionDeseaseSymptomIcon,
+                        _conditionIconBackgroundTextures[0]);
 
                 default:
                     Debug.Fail("Every condition must have icon.");
                     return new PersonConditionTextures(_conditionDefaultIcon, _conditionIconBackgroundTextures[0]);
             }
-        }
-
-        private static Texture2D GetStatHazardConditionLevelClient(SurvivalStatHazardLevel level, Texture2D[] orderedTextures)
-        {
-            var defaultBackground = orderedTextures[0];
-
-            switch (level)
-            {
-                case SurvivalStatHazardLevel.Lesser:
-                    return orderedTextures[0];
-                case SurvivalStatHazardLevel.Strong:
-                    return orderedTextures[1];
-                case SurvivalStatHazardLevel.Max:
-                    return orderedTextures[2];
-                case SurvivalStatHazardLevel.Undefined:
-                    Debug.Fail("Undefined value can't be assigned.");
-                    return defaultBackground;
-                default:
-                    Debug.Fail($"Value {level} is unknown.");
-                    return defaultBackground;
-            }
-        }
-
-        private Texture2D GetStatHazardConditionTypeClient(SurvivalStatType type)
-        {
-            if (_survivalStatConditionIcons is null)
-            {
-                throw new InvalidOperationException("Survival condition icons is not loaded.");
-            }
-
-            if (!_survivalStatConditionIcons.TryGetValue(type, out var iconTexture))
-            {
-                Debug.Fail("Every stat must has its own icon.");
-            }
-
-            return iconTexture;
         }
 
         public Texture2D GetHintBackgroundTexture()
