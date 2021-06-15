fa376f211d8e780b332778bb9f81cb749abc2011
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-06-15 20:14:47 +0700

    Switch idle mode back

--- Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/Ui/CombatActPanel.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/Ui/CombatActPanel.cs
@@ -23,6 +23,7 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene.Ui
         private readonly IEquipmentModule _equipmentModule;
         private readonly ISectorUiState _sectorUiState;
         private readonly IUiContentStorage _uiContentStorage;
+        private readonly IconButton _idleModeSwitcherButton;
 
         public CombatActPanel(ICombatActModule combatActModule, IEquipmentModule equipmentModule,
             IUiContentStorage uiContentStorage, ISectorUiState sectorUiState)
@@ -38,18 +39,29 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene.Ui
 
             Initialize(_buttons);
 
+            _idleModeSwitcherButton = new IconButton(uiContentStorage.GetSmallVerticalButtonBackgroundTexture(),
+                new IconData(uiContentStorage.GetSmallVerticalButtonIconsTexture(), new Rectangle(0, 0, 16, 32)),
+                new Rectangle(0, 0, 16, 32));
+            _idleModeSwitcherButton.OnClick += IdleModeSwitcherButton_OnClick;
+
             _equipmentModule.EquipmentChanged += EquipmentModule_EquipmentChanged;
+        }
+
+        private void IdleModeSwitcherButton_OnClick(object? sender, EventArgs e)
+        {
+            _combatActModule.IsCombatMode = !_combatActModule.IsCombatMode;
         }
 
         public void Draw(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice)
         {
+            const int BUTTON_SIZE = 32;
+            const int BOTTOM_MARGIN = 0;
+
             var acts = _combatActModule.CalcCombatActs().Take(MAX_COMBAT_ACT_COUNT);
             var actsOrdered = acts.OrderBy(x => x.Scheme?.Sid).ToArray();
             var actIndex = 0;
             foreach (var button in _buttons)
             {
-                const int BUTTON_SIZE = 32;
-                const int BOTTOM_MARGIN = 0;
                 var buttonRect = new Rectangle(
                     actIndex * BUTTON_SIZE + graphicsDevice.Viewport.Width / 2 - BUTTON_SIZE * MAX_COMBAT_ACT_COUNT / 2,
                     graphicsDevice.Viewport.Bounds.Bottom - BUTTON_SIZE - BOTTOM_MARGIN, BUTTON_SIZE,
@@ -61,6 +73,12 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene.Ui
 
                 actIndex++;
             }
+
+            _idleModeSwitcherButton.Rect = new Rectangle(
+                    actIndex * BUTTON_SIZE + graphicsDevice.Viewport.Width / 2 - BUTTON_SIZE * MAX_COMBAT_ACT_COUNT / 2,
+                    graphicsDevice.Viewport.Bounds.Bottom - BUTTON_SIZE - BOTTOM_MARGIN, 16,
+                    BUTTON_SIZE);
+            _idleModeSwitcherButton.Draw(spriteBatch);
         }
 
         public void UnsubscribeEvents()
@@ -74,6 +92,8 @@ namespace CDT.LAST.MonoGameClient.ViewModels.MainScene.Ui
             {
                 button.Update();
             }
+
+            _idleModeSwitcherButton.Update();
         }
 
         private void EquipmentModule_EquipmentChanged(object? sender, EquipmentChangedEventArgs e)
