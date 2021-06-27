39af11a7b5d77bac1303956b3075efa9f3736149
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-06-28 00:31:50 +0700

    Fix Sonar Issues - [csharpsquid:S3776]

--- Zilon.Core/CDT.LAST.Outliner/Program.cs
+++ Zilon.Core/CDT.LAST.Outliner/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 
@@ -11,24 +10,29 @@ namespace CDT.LAST.Outliner
     {
         private static bool CheckIsBound(Bitmap sourceBmp, int x, int y)
         {
-            for (var x1 = -1; x1 <= 1; x1++)
+            var offsets = new[] {
+                new { x = 1, y = 0 },
+                new { x = -1, y = 0 },
+                new { x = 0, y = 1 },
+                new { x = 0, y = -1 }
+            };
+
+            foreach (var offset in offsets)
             {
-                for (var y1 = -1; y1 <= 1; y1++)
+                var targetX = x + offset.x;
+                var targetY = y + offset.y;
+
+                if (targetX < 0 || targetY < 0 || targetX >= sourceBmp.Width || targetY >= sourceBmp.Height)
                 {
-                    var targetX = x + x1;
-                    var targetY = y + y1;
-
-                    if (targetX < 0 || targetY < 0 || targetX >= sourceBmp.Width || targetY >= sourceBmp.Height)
-                    {
-                        continue;
-                    }
-
-                    var pixel = sourceBmp.GetPixel(targetX, targetY);
-                    if (pixel.A > 0)
-                    {
-                        return true;
-                    }
+                    continue;
                 }
+
+                var pixel = sourceBmp.GetPixel(targetX, targetY);
+                if (pixel.A > 0)
+                {
+                    return true;
+                }
+
             }
 
             return false;
@@ -42,47 +46,36 @@ namespace CDT.LAST.Outliner
             var files = Directory.GetFiles(imagePath, "*.png");
             foreach (var file in files)
             {
-                using var sourceBmp = new Bitmap(file);
-                using var outputBmp = new Bitmap(sourceBmp.Width, sourceBmp.Height);
+                HandleFile(outputPath, file);
+            }
+        }
 
-                for (var x = 0; x < sourceBmp.Width; x++)
+        private static void HandleFile(string outputPath, string file)
+        {
+            using var sourceBmp = new Bitmap(file);
+            using var outputBmp = new Bitmap(sourceBmp.Width, sourceBmp.Height);
+            for (var x = 0; x < sourceBmp.Width; x++)
+            {
+                for (var y = 0; y < sourceBmp.Height; y++)
                 {
-                    for (var y = 0; y < sourceBmp.Height; y++)
+                    var currentPixel = sourceBmp.GetPixel(x, y);
+                    if (currentPixel.A > 0)
                     {
-                        var currentPixel = sourceBmp.GetPixel(x, y);
-                        if (currentPixel.A > 0)
-                        {
-                            outputBmp.SetPixel(x, y, Color.White);
-                            continue;
-                        }
+                        outputBmp.SetPixel(x, y, Color.White);
+                        continue;
+                    }
 
-                        var isBound = CheckIsBound(sourceBmp, x, y);
+                    var isBound = CheckIsBound(sourceBmp, x, y);
 
-                        if (isBound)
-                        {
-                            for (var x1 = -1; x1 <= 1; x1++)
-                            {
-                                for (var y1 = -1; y1 <= 1; y1++)
-                                {
-                                    var targetX = x + x1;
-                                    var targetY = y + y1;
-
-                                    if (targetX < 0 || targetY < 0 || targetX >= sourceBmp.Width ||
-                                        targetY >= sourceBmp.Height)
-                                    {
-                                        continue;
-                                    }
-
-                                    outputBmp.SetPixel(x + x1, y + y1, Color.White);
-                                }
-                            }
-                        }
+                    if (isBound)
+                    {
+                        outputBmp.SetPixel(x, y, Color.White);
                     }
                 }
-
-                var outFile = Path.GetFileNameWithoutExtension(file);
-                outputBmp.Save(Path.Combine(outputPath, outFile + ".png"), ImageFormat.Png);
             }
+
+            var outFile = Path.GetFileNameWithoutExtension(file);
+            outputBmp.Save(Path.Combine(outputPath, outFile + ".png"), ImageFormat.Png);
         }
     }
 }
