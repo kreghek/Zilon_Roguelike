08e91715e0ab90316fdacdbd43ff2bd079ee2a14
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-06-27 18:06:00 +0700

    Introduce console outliner

---
+++ Zilon.Core/CDT.LAST.Outliner/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+using Zilon.CommonUtilities;
+
+namespace CDT.LAST.Outliner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var imagePath = ArgumentHelper.GetProgramArgument(args, "imagePath");
+            var outputPath = ArgumentHelper.GetProgramArgument(args, "outputPath");
+
+            var files = Directory.GetFiles(imagePath, "*.png");
+            foreach (var file in files)
+            {
+                var sourceBmp = new Bitmap(file);
+                var outputBmp = new Bitmap(sourceBmp.Width, sourceBmp.Height);
+
+                for (var x = 0; x < sourceBmp.Width; x++)
+                {
+                    for (var y = 0; y < sourceBmp.Height; y++)
+                    {
+                        var currentPixel = sourceBmp.GetPixel(x, y);
+                        if (currentPixel.A > 0)
+                        {
+                            outputBmp.SetPixel(x, y, Color.White);
+                            continue;
+                        }
+
+                        var isBound = CheckIsBound(sourceBmp, x, y);
+
+                        if (isBound)
+                        {
+                            for (var x1 = -1; x1 <= 1; x1++)
+                            {
+                                for (var y1 = -1; y1 <= 1; y1++)
+                                {
+                                    var targetX = x + x1;
+                                    var targetY = y + y1;
+
+                                    if (targetX < 0 || targetY < 0 || targetX >= sourceBmp.Width || targetY >= sourceBmp.Height)
+                                    {
+                                        continue;
+                                    }
+
+                                    outputBmp.SetPixel(x + x1, y + y1, Color.White);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                var outFile = Path.GetFileNameWithoutExtension(file);
+                outputBmp.Save(Path.Combine(outputPath, outFile + ".png"), ImageFormat.Png);
+            }
+        }
+
+        private static bool CheckIsBound(Bitmap sourceBmp, int x, int y)
+        {
+            for (var x1 = -1; x1 <= 1; x1++)
+            {
+                for (var y1 = -1; y1 <= 1; y1++)
+                {
+                    var targetX = x + x1;
+                    var targetY = y + y1;
+
+                    if (targetX < 0 || targetY < 0 || targetX >= sourceBmp.Width || targetY >= sourceBmp.Height)
+                    {
+                        continue;
+                    }
+
+                    var pixel = sourceBmp.GetPixel(targetX, targetY);
+                    if (pixel.A > 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
