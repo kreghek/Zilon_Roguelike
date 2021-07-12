a72c48cae698decf6b3ed818c2f384f8780e3a36
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-07-12 21:04:01 +0700

    Replace red color

---
+++ Zilon.Core/CDT.LAST.MonoGameClient/Engine/LastColors.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CDT.LAST.MonoGameClient.Engine
+{
+    public static class LastColors
+    {
+        public static Color Red = new Color(0x3140bf);
+    }
+}
