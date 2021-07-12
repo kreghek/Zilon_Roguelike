9a187dc1fb14d91e09b8c1ab9320786c4591adec
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-07-12 08:51:49 +0700

    WIP Person markers panel

---
+++ Zilon.Core/CDT.LAST.MonoGameClient/ViewModels/MainScene/Ui/PersonMarkersPanel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Zilon.Core.Players;
+
+namespace CDT.LAST.MonoGameClient.ViewModels.MainScene.Ui
+{
+    internal class PersonMarkersPanel
+    {
+        private readonly SectorViewModelContext _sectorViewModelContext;
+        private readonly IList<ActorViewModel> _visibleActors;
+
+        public PersonMarkersPanel(SectorViewModelContext sectorViewModelContext, IPlayer player)
+        {
+            _sectorViewModelContext = sectorViewModelContext;
+
+            _visibleActors = new List<ActorViewModel>();
+        }
+
+        public void Update()
+        {
+            _visibleActors.Clear();
+
+            var actorViewModels = _sectorViewModelContext.GameObjects.OfType<ActorViewModel>().ToArray();
+            foreach (var actorViewModel in actorViewModels)
+            { 
+                actorViewModel
+            }
+        }
+    }
+}
