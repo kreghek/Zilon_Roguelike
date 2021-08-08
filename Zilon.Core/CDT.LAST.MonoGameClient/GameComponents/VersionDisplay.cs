71cda39572c905268f671d47f13200620485c4e7
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-08-08 21:00:28 +0700

    Ver 4

--- Zilon.Core/CDT.LAST.MonoGameClient/GameComponents/VersionDisplay.cs
+++ Zilon.Core/CDT.LAST.MonoGameClient/GameComponents/VersionDisplay.cs
@@ -5,7 +5,7 @@ namespace CDT.LAST.MonoGameClient.GameComponents
 {
     internal class VersionDisplay : DrawableGameComponent
     {
-        private readonly string _version = "ver.2.5.8-gdw21.3";
+        private readonly string _version = "ver.2.5.8-gdw21.4";
         private SpriteBatch _spriteBatch;
         private readonly SpriteFont _font;
 
