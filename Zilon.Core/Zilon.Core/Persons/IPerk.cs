a6f34dda8bb89768a758282f1da1724e1cb14d56
Pavel Kurkutov <pkurkutov@ptsecurity.com>
2022-05-13 12:56:34 +0700

    Refactor

--- Zilon.Core/Zilon.Core/Persons/IPerk.cs
+++ Zilon.Core/Zilon.Core/Persons/IPerk.cs
@@ -3,12 +3,12 @@
 namespace Zilon.Core.Persons
 {
     /// <summary>
-    /// Information about perk - archieved or potentially.
+    /// Information about perk - achieved or potentially.
     /// </summary>
     public interface IPerk : IJobExecutable
     {
         /// <summary>
-        /// Current perk level. Null current level means perk is not archieved.
+        /// Current perk level. Null current level means perk is not achieved.
         /// </summary>
         PerkLevel? CurrentLevel { get; set; }
 
